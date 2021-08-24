d525e8905db25aa46213909c0b6be288cc03610e
Oleksandr Morozov <oleksandr.morozov@protonmail.com>
2021-08-24 17:43:55 +0300

    some changes have been implemented

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -14,69 +14,68 @@ export interface Figure {
 }
 
 export class Triangle implements Figure {
+  public shape = Shape.triangle;
+
   constructor(
     public color: Color,
     public a: number,
     public b: number,
     public c: number,
-    public shape = Shape.triangle,
   ) {
-    if (
-      (this.a <= 0 || this.b <= 0 || this.c <= 0)
-      || (this.a + this.b <= this.c
-        || this.a + this.c <= this.b || this.b + this.c <= this.a)
-    ) {
+    if (this.a <= 0 || this.b <= 0 || this.c <= 0) {
+      throw new Error('All sides should be positive integers');
+    }
+
+    if (this.a + this.b <= this.c
+      || this.a + this.c <= this.b
+      || this.b + this.c <= this.a) {
       throw new Error('Those sides cannot form a triangle');
     }
   }
 
   getArea(): number {
-    const s = (this.a + this.b + this.c) / 2;
-    const area = Math.sqrt(s * ((s - this.a) * (s - this.b) * (s - this.c)));
+    const p = (this.a + this.b + this.c) / 2;
+    const area = Math.sqrt(p * ((p - this.a) * (p - this.b) * (p - this.c)));
 
     return Math.floor(area * 100) / 100;
   }
 }
 
 export class Circle implements Figure {
+  public shape = Shape.circle;
+
   constructor(
     public color: Color,
     public radius: number,
-    public shape = Shape.circle,
   ) {
     if (this.radius <= 0) {
-      throw new Error('You cannot form a circle');
+      throw new Error('The radius should be positive integer');
     }
   }
 
   getArea(): number {
-    const area = 3.14 * (this.radius ** 2);
-
-    return Math.floor(area * 100) / 100;
+    return Math.floor((3.14 * (this.radius ** 2)) * 100) / 100;
   }
 }
 
 export class Rectangle implements Figure {
+  public shape = Shape.rectangle;
+
   constructor(
     public color: Color,
     public width: number,
     public height: number,
-    public shape = Shape.rectangle,
   ) {
     if (this.height <= 0 || this.width <= 0) {
-      throw new Error('You cannot form a rectangle');
+      throw new Error('Both sides should be positive integers');
     }
   }
 
   getArea(): number {
-    const area = this.width * this.height;
-
-    return Math.floor(area * 100) / 100;
+    return Math.floor((this.width * this.height) * 100) / 100;
   }
 }
 
 export function getInfo(figure: Figure): string {
-  const area = figure.getArea();
-
-  return `A ${figure.color} ${figure.shape} - ${area}`;
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
