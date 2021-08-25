ed8298e427fb6374ded4ba07f86f259b964e1879
Alex Hlotov <alexhlotov67@gmail.com>
2021-08-25 14:47:41 +0300

    Solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,85 @@
+enum Shape {
+  Triangle = 'triangle',
+  Rectangle = 'rectangle',
+  Circle = 'circle'
+}
+
+enum Color {
+  Red = 'red',
+  Green = 'green',
+  Blue = 'blue'
+}
+
 export interface Figure {
-
+  shape: Shape,
+  color: Color,
+  getArea(): number
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  shape = Shape.Triangle;
 
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (this.a + this.b <= this.c
+      || this.a + this.c <= this.b
+      || this.b + this.c <= this.a) {
+      throw new Error('Sides 1, 2 and 3 cant form a triangle');
+    }
+  }
+
+  getArea(): number {
+    const p = (this.a + this.b + this.c) / 2;
+
+    return Math
+      .round(Math
+        .sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)) * 100) / 100;
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  shape = Shape.Circle;
 
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (this.radius < 1) {
+      throw new Error('Radius cant be less then 1');
+    }
+  }
+
+  getArea(): number {
+    return Math.round(3.14 * (this.radius ** 2) * 100) / 100;
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  shape = Shape.Rectangle;
 
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
+    if (this.width < 1) {
+      throw new Error('Width cant be less then 1');
+    }
+
+    if (this.height < 1) {
+      throw new Error('Height cant be less then 1');
+    }
+  }
+
+  getArea(): number {
+    return Math.round(this.height * this.width * 100) / 100;
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure) :string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
