cbce599a3407e251075b450b7ef4be34bbd4bcd2
Andriy Nikorych <andrei,nikorich@gmail.com>
2021-08-29 14:28:20 +0300

    Solution Figures Classes

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,80 @@
 export interface Figure {
-
+  shape: string;
+  color: string;
+  getArea(): number;
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  public shape = 'triangle';
 
+  constructor(
+    public color: string,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('Side length has to be grater than 0');
+    }
+
+    const arrayOfTriangle: number[] = [a, b, c]
+      .sort((x: number, y: number) => y - x);
+
+    if (arrayOfTriangle[0] === arrayOfTriangle[1] + arrayOfTriangle[2]) {
+      throw new Error('These three sides cannot make a triangle');
+    }
+  }
+
+  getArea(): number {
+    const p: number = (this.a + this.b + this.c) / 2; // half of perimeter
+    const area: number = (p
+      * (p - this.a)
+      * (p - this.b)
+      * (p - this.c)) ** (1 / 2); // Heron's formula
+
+    return Number(area.toFixed(2));
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  public shape = 'circle';
 
+  constructor(
+    public color: string,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('Side length has to be grater than 0');
+    }
+  }
+
+  getArea(): number {
+    const area: number = 3.14 * this.radius ** 2;
+
+    return Number(area.toFixed(2));
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  public shape = 'rectangle';
 
+  constructor(
+    public color: string,
+    public width: number,
+    public height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('Side length has to be grater than 0');
+    }
+  }
+
+  getArea(): number {
+    const area: number = this.width * this.height;
+
+    return Number(area.toFixed(2));
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
