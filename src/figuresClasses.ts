170fbaea63fc7a9232d90258a66f4c5b02052c74
Artem <violence044@gmail.com>
2021-08-30 14:37:33 +0300

    solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,77 @@
-export interface Figure {
+type Color = 'red' | 'green' | 'blue';
 
+enum Shape {
+  triangle = 'triangle',
+  circle = 'circle',
+  rectangle = 'rectangle',
 }
 
-export class Triangle {
+export interface Figure {
+  color: Color,
+}
 
+export class Triangle implements Figure {
+  public shape = Shape.triangle;
+
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if ((this.a <= 0 || this.b <= 0 || this.c <= 0) || (
+      this.a + this.b <= this.c
+      || this.b + this.c <= this.a
+      || this.a + this.c <= this.b)) {
+      throw new Error('Error');
+    }
+  }
+
+  getArea(): number {
+    const semiPerimeter = (this.a + this.b + this.c) / 2;
+
+    return +Math.sqrt(semiPerimeter
+      * (semiPerimeter - this.a)
+      * (semiPerimeter - this.b)
+      * (semiPerimeter - this.c)).toFixed(2);
+  }
 }
 
 export class Circle {
+  public shape = Shape.circle;
 
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (this.radius <= 0) {
+      throw new Error('Error');
+    }
+  }
+
+  getArea(): number {
+    return +(Number(Math.PI.toFixed(2)) * this.radius ** 2).toFixed(2);
+  }
 }
 
 export class Rectangle {
+  public shape = Shape.rectangle;
 
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
+    if (height <= 0 || width <= 0) {
+      throw new Error('Error');
+    }
+  }
+
+  getArea(): number {
+    return +(this.width * this.height).toFixed(2);
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
