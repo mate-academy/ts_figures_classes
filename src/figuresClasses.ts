67560476cfb287f84f7fa16a425cdc3d01060e8f
Oleksandr Zhitenko <geetonio28@gmail.com>
2021-08-30 15:32:39 +0300

    ver 2

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -18,7 +18,7 @@ export interface Figure {
 }
 
 export class Triangle implements Figure {
-  public shape;
+  public shape = Shape.triangle;
 
   constructor(
     public color: Color,
@@ -26,11 +26,12 @@ export class Triangle implements Figure {
     public b: number,
     public c: number,
   ) {
-    this.shape = Shape.triangle;
-
     if (a + b <= c
       || a + c <= b
       || b + c <= a
+      || a <= 0
+      || b <= 0
+      || c <= 0
     ) {
       throw new Error('sides 1, 2 and 3 can\'t form a triangle');
     }
@@ -39,48 +40,46 @@ export class Triangle implements Figure {
   getArea(): number {
     const s: number = (this.a + this.b + this.c) / 2;
 
-    return +(Math.sqrt(s * (s - this.a)
-    * (s - this.b) * (s - this.c)).toFixed(2));
+    return Math.sqrt(s * (s - this.a)
+      * (s - this.b) * (s - this.c));
   }
 }
 
 export class Circle implements Figure {
+  public shape = Shape.circle;
+
   constructor(
     public color: Color,
     public radius: number,
   ) {
-    this.shape = Shape.circle;
-
     if (this.radius <= 0) {
       throw new Error('Radius value must be positive');
     }
   }
 
   getArea(): number {
-    return +(3.14 * (this.radius ** 2)).toFixed(2);
+    return 3.14 * (this.radius ** 2);
   }
 }
 
 export class Rectangle implements Figure {
-  public shape;
+  public shape = Shape.rectangle;
 
   constructor(
     public color: Color,
     public width: number,
     public height: number,
   ) {
-    this.shape = Shape.rectangle;
-
     if (this.width <= 0 || this.height <= 0) {
       throw new Error('Width and height can\'t form a rectangle');
     }
   }
 
   getArea(): number {
-    return +(this.width * this.height).toFixed(2);
+    return this.width * this.height;
   }
 }
 
 export function getInfo(figure: Figure): string {
-  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
+  return `A ${figure.color} ${figure.shape} - ${+figure.getArea().toFixed(2)}`;
 }
