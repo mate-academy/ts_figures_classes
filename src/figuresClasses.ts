0bdb5a49e978e3de033a209c8e82569225014dd2
Oleksandr Krasnoshchokov <krasnoshchokovoleksandr@gmail.com>
2021-10-06 07:37:25 +0300

    fixed

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,7 +1,6 @@
 export interface Figure {
   shape: 'triangle' | 'circle' | 'rectangle',
   color: 'red' | 'green' | 'blue',
-
   getArea(): number;
 }
 
@@ -13,11 +12,13 @@ export class Triangle implements Figure {
     public c: number,
     public shape: Figure['shape'],
   ) {
-    if (a <= 0 || b <= 0 || c <= 0 || (a + b) === c) {
+    if (a <= 0 || b <= 0 || c <= 0
+      || (a + b) === c
+      || (a + c) === b
+      || (c + b) === a) {
       throw Error();
     }
     this.shape = 'triangle';
-    this.getArea();
   }
 
   getArea(): number {
@@ -33,37 +34,35 @@ export class Triangle implements Figure {
 export class Circle implements Figure {
   constructor(
     public color: Figure['color'],
-    public a: number,
+    public r: number,
     public shape: Figure['shape'],
   ) {
-    if (a <= 0) {
+    if (r <= 0) {
       throw Error();
     }
     this.shape = 'circle';
-    this.getArea();
   }
 
   getArea(): number {
-    return Number((3.14 * this.a * this.a).toFixed(2));
+    return Number((3.14 * this.r * this.r).toFixed(2));
   }
 }
 
 export class Rectangle implements Figure {
   constructor(
     public color: Figure['color'],
-    public a: number,
-    public b: number,
+    public h: number,
+    public l: number,
     public shape: Figure['shape'],
   ) {
-    if (a <= 0 || b <= 0) {
+    if (h <= 0 || l <= 0) {
       throw Error();
     }
     this.shape = 'rectangle';
-    this.getArea();
   }
 
   getArea(): number {
-    return Number((this.a * this.b).toFixed(2));
+    return Number((this.h * this.l).toFixed(2));
   }
 }
 
