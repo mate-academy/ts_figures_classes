b5e60123f3bbdfbf32f0618b59b22c764a155c41
Ivarovskiy <iva7art@gmail.com>
2021-10-29 18:49:45 +0300

    solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,80 @@
-export interface Figure {
-
+enum Shape {
+  Triangle = 'triangle',
+  Circle = 'circle',
+  Rectangle = 'rectangle',
 }
 
-export class Triangle {
+enum Color {
+  Red = 'red',
+  Green = 'green',
+  Blue = 'blue'
+}
 
+export interface Figure {
+  shape: Shape,
+  color: Color,
+  getArea(): number,
+}
+
+export class Triangle implements Figure {
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+    public shape = Shape.Triangle,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('One of your sides is <= 0');
+    }
+
+    if (a >= b + c || b >= a + c || c >= a + b) {
+      throw new Error('This is not a triangle');
+    }
+  }
+
+  getArea(): number {
+    // used Heron's formula
+    const s = (this.a + this.b + this.c) / 2;
+    const area = Math.sqrt(s * ((s - this.a) * (s - this.b) * (s - this.c)));
+
+    return Math.floor(area * 100) / 100;
+  }
 }
 
 export class Circle {
+  constructor(
+    public color: Color,
+    public radius: number,
+    public shape = Shape.Circle,
+  ) {
+    if (radius <= 0) {
+      throw new Error('Radius cannot be less than 0');
+    }
+  }
 
+  getArea(): number {
+    return Math.floor(Math.PI * this.radius ** 2 * 100) / 100;
+  }
 }
 
 export class Rectangle {
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+    public shape = Shape.Rectangle,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('One of your sides is <= 0');
+    }
+  }
 
+  getArea(): number {
+    return this.width * this.height;
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
