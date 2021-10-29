317d1612fc5658237b9f74c75962d7a7b40d8f5f
cyb1ove <cyb1ove.ua@gmail.com>
2021-10-29 18:09:14 +0300

    Some fixes

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -12,46 +12,46 @@ function truncate(number: number): number {
 }
 
 function checkInvalidForm(...sides: number[]): boolean {
+  const [a, b, c] = sides;
+
   if (sides.length === 3) {
-    return !(sides[0] < (sides[1] + sides[2])
-          && sides[1] < (sides[0] + sides[2])
-          && sides[2] < (sides[0] + sides[1]));
+    return !(a < (b + c) && b < (a + c) && c < (a + b));
   }
 
   return sides.some((side: number) => side <= 0);
 }
 
 export class Triangle implements Figure {
-  public shape;
+  public shape: Shape = 'triangle';
 
-  constructor(public color: Color,
-    private a: number, private b: number, private c: number) {
+  constructor(
+    public color: Color,
+    private a: number,
+    private b: number,
+    private c: number,
+  ) {
     if (checkInvalidForm(a, b, c)) {
       throw new Error('Wrong data');
     }
-
-    this.shape = 'triangle';
   }
 
   getArea(): number {
-    const semiperimeter = (this.a + this.b + this.c) / 2;
+    const semiPerimeter = (this.a + this.b + this.c) / 2;
 
     return truncate(
-      Math.sqrt(semiperimeter * (semiperimeter - this.a)
-      * (semiperimeter - this.b) * (semiperimeter - this.c)),
+      Math.sqrt(semiPerimeter * (semiPerimeter - this.a)
+      * (semiPerimeter - this.b) * (semiPerimeter - this.c)),
     );
   }
 }
 
 export class Circle implements Figure {
-  public shape;
+  public shape: Shape = 'circle';
 
   constructor(public color: Color, private radius: number) {
     if (checkInvalidForm(radius)) {
       throw new Error('Wrong data');
     }
-
-    this.shape = 'circle';
   }
 
   getArea(): number {
@@ -60,15 +60,13 @@ export class Circle implements Figure {
 }
 
 export class Rectangle implements Figure {
-  public shape;
+  public shape: Shape = 'rectangle';
 
   constructor(public color: Color,
     private width: number, private height: number) {
     if (checkInvalidForm(height, width)) {
       throw new Error('Wrong data');
     }
-
-    this.shape = 'rectangle';
   }
 
   getArea(): number {
