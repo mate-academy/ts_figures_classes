5fc13ae68d7b4366726044e9e6c5d87eb8c2b7f3
Igor Kovalev <bizzzy0@gmail.com>
2021-11-01 19:03:19 +0200

    Issues was fixed x2

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -3,21 +3,22 @@ type Color = 'red' | 'green' | 'blue';
 export interface Figure {
   shape: string;
   color: string;
-  sizes: number[];
 
   getArea(): number;
 }
 
 export class Triangle implements Figure {
-  public shape = 'triangle';
+  shape = 'triangle';
 
   sizes: number[];
 
   constructor(
     public color: Color,
-    ...args: number[]
+    public a: number,
+    public b: number,
+    public c: number,
   ) {
-    this.sizes = args.sort((a, b) => a - b);
+    this.sizes = [a, b, c].sort((first, second) => first - second);
 
     const maxValue = this.sizes[2];
     const restValuesSummary = this.sizes[0] + this.sizes[1];
@@ -30,54 +31,49 @@ export class Triangle implements Figure {
   }
 
   getArea(): number {
-    const a = this.sizes[0];
-    const b = this.sizes[1];
-    const c = this.sizes[2];
-    const semiPerimetr = (a + b + c) / 2;
+    const semiPerimetr = (this.a + this.b + this.c) / 2;
     const area = Math.sqrt(semiPerimetr
-      * (semiPerimetr - a)
-      * (semiPerimetr - b)
-      * (semiPerimetr - c));
+      * (semiPerimetr - this.a)
+      * (semiPerimetr - this.b)
+      * (semiPerimetr - this.c));
 
     return Math.floor(area * 100) / 100;
   }
 }
 
 export class Circle implements Figure {
-  public shape = 'circle';
-
-  sizes: number[];
+  shape = 'circle';
 
   constructor(
     public color: Color,
-    ...args: number[]
+    public radius: number,
   ) {
-    this.sizes = args;
+    this.radius = radius;
 
-    if (!(this.sizes[0] > 0)) {
+    if (!(radius > 0)) {
       throw new Error('Wrong radius value');
     }
   }
 
   getArea(): number {
-    const area = Math.PI * (this.sizes[0] ** 2);
+    const area = Math.PI * (this.radius ** 2);
 
     return Math.floor(area * 100) / 100;
   }
 }
 
 export class Rectangle implements Figure {
-  public shape = 'rectangle';
-
-  sizes: number[];
+  shape = 'rectangle';
 
   constructor(
     public color: Color,
-    ...args: number[]
+    public width: number,
+    public height: number,
   ) {
-    this.sizes = args;
+    this.width = width;
+    this.height = height;
 
-    const isCorrectLengthOfSides = this.sizes.every((side) => side > 0);
+    const isCorrectLengthOfSides = width > 0 && height > 0;
 
     if (!isCorrectLengthOfSides) {
       throw new Error('Wrong sides value');
@@ -85,7 +81,7 @@ export class Rectangle implements Figure {
   }
 
   getArea(): number {
-    const area = this.sizes[0] * this.sizes[1];
+    const area = this.width * this.height;
 
     return Math.floor(area * 100) / 100;
   }
