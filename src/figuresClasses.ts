31bd2bbfc7fbe942162e83a125c441b3ec8f5721
Ivan Stanko <ivan_stanko@ukr.net>
2021-12-01 14:49:02 +0200

    completed figures

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,75 @@
+type Color = 'red' | 'green' | 'blue';
+
 export interface Figure {
-
+  shape: string;
+  color: string;
+  getArea(): number;
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  shape = 'triangle';
 
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('Wrong sides value');
+    }
+
+    if (a + b <= c || a + c <= b || b + c <= a) {
+      throw new Error('Wrong sides value');
+    }
+  }
+
+  getArea(): number {
+    const semiPerimetr = (this.a + this.b + this.c) / 2;
+    const area = Math.sqrt(semiPerimetr
+      * (semiPerimetr - this.a)
+      * (semiPerimetr - this.b)
+      * (semiPerimetr - this.c));
+
+    return Math.floor(area * 100) / 100;
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  shape = 'circle';
 
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('Wrong radius value');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor(100 * Math.PI * this.radius ** 2) / 100;
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  shape = 'rectangle';
 
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('Wrong sides value');
+    }
+  }
+
+  getArea(): number {
+    return this.width * this.height;
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
