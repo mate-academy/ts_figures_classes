37188fa38b431abf7c043afc3f8dc535f598b61c
Dima Permiakov <dimapermyakov2532@gmail.com>
2021-12-17 23:19:44 +0200

    solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,78 @@
+type Color = 'red' | 'green' | 'blue';
+type Shape = 'triangle' | 'circle' | 'rectangle';
+
 export interface Figure {
-
+  shape: Shape,
+  color: Color,
+  getArea(): number,
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  shape: Shape = 'triangle';
 
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('side <= 0');
+    }
+
+    const longestSide: number[] = [a, b, c].sort((prev, curr) => prev - curr);
+
+    const [side1, side2, side3] = longestSide;
+
+    if (side1 + side2 <= side3) {
+      throw new Error(`the longest side of a triangle is
+      >= than a sum of two others`);
+    }
+  }
+
+  getArea(): number {
+    const s: number = (this.a + this.b + this.c) / 2;
+
+    return Math.floor(Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c))
+      * 100) / 100;
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  shape: Shape = 'circle';
 
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('radius <= 0');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor((Math.PI * (this.radius ** 2)) * 100) / 100;
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  shape: Shape = 'rectangle';
 
+  constructor(
+    public color: Color,
+    public sideA: number,
+    public sideB: number,
+  ) {
+    if (sideA <= 0 || sideB <= 0) {
+      throw new Error('side <= 0');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor(this.sideA * this.sideB * 100) / 100;
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
