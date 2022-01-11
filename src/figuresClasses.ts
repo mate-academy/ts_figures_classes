c41c8cfe604d09a1560c31b2204a56a17717bcdf
Taras Zherebetskiy <tarasj76@gmail.com>
2022-01-11 18:54:16 +0200

    figures

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,95 @@
+enum Shape {
+  Triangle = 'triangle',
+  Circle = 'circle',
+  Rectangle = 'rectangle',
+}
+
+enum Color {
+  Red = 'red',
+  Green = 'green',
+  Blue = 'blue',
+}
+
 export interface Figure {
-
+  shape: Shape;
+  color: Color;
+  getArea(): number;
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  shape = Shape.Triangle;
 
+  constructor(
+    public color: Color,
+
+    private a: number,
+    private b: number,
+    private c: number,
+  ) {
+    this.color = color;
+
+    if (this.a <= 0
+      || this.b <= 0
+      || this.c <= 0
+      || this.a + this.b <= this.c
+      || this.a + this.c <= this.b
+      || this.c + this.b <= this.a
+    ) {
+      throw new Error('Enter correct data');
+    }
+  }
+
+  getArea(): number {
+    const p = (this.a + this.b + this.c) / 2;
+
+    return Math.floor(Math.sqrt(p * (p - this.a)
+    * (p - this.b) * (p - this.c)) * 100) / 100;
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  shape = Shape.Circle;
 
+  constructor(
+    public color: Color,
+
+    private radius: number,
+  ) {
+    this.color = color;
+
+    if (this.radius <= 0) {
+      throw new Error('Enter correct data');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor(Math.PI * this.radius * this.radius * 100) / 100;
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  shape = Shape.Rectangle;
 
+  constructor(
+    public color: Color,
+
+    private width: number,
+    private height: number,
+  ) {
+    this.color = color;
+
+    if (this.width <= 0
+      || this.height <= 0
+    ) {
+      throw new Error('Enter correct data');
+    }
+  }
+
+  getArea(): number {
+    return +(this.height * this.width).toFixed(2);
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
