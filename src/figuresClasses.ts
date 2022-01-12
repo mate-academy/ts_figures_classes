666a80c68ef80dcadf65f17d77772182ce78bc4b
Oleksandr Ostrovskyi <o.ostrovskiy62@gmail.com>
2022-01-12 10:59:42 +0200

    figures solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,90 @@
+enum Color {
+  Red = 'red',
+  Green = 'green',
+  Blue = 'blue',
+}
+
+enum Shape {
+  Triangle = 'triangle',
+  Circle = 'circle',
+  Rectangle = 'rectangle',
+}
+
 export interface Figure {
+  shape: Shape;
+  color: Color;
 
+  getArea(): number;
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  shape = Shape.Triangle;
 
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (this.a <= 0
+      || this.b <= 0
+      || this.c <= 0
+      || Math.max(this.a, this.b, this.c)
+        >= (this.a + this.b + this.c - Math.max(this.a, this.b, this.c))
+    ) {
+      throw new Error('Your parametrs are not so good');
+    }
+  }
+
+  getArea(): number {
+    const s = (this.a + this.b + this.c) / 2;
+
+    const area = Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+
+    return Math.floor(area * 100) / 100;
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  shape = Shape.Circle;
 
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (this.radius <= 0) {
+      throw new Error('Your parametrs are not so good');
+    }
+  }
+
+  getArea(): number {
+    const area = Math.PI * (this.radius ** 2);
+
+    return Math.floor(area * 100) / 100;
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  shape = Shape.Rectangle;
 
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
+    if (
+      this.width <= 0
+      || this.height <= 0
+    ) {
+      throw new Error('Your parametrs are not so good');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor(this.width * this.height);
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Triangle | Circle | Rectangle): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
