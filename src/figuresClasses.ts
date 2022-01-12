89664d742abd23e6850f6ca44a7aa3d38eb47b8f
Taras Zherebetskiy <tarasj76@gmail.com>
2022-01-12 11:19:01 +0200

    add other errors

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -25,13 +25,18 @@ export class Triangle implements Figure {
     private b: number,
     private c: number,
   ) {
-    const incorrectLength: boolean = this.a <= 0 || this.b <= 0 || this.c <= 0;
-    const incorrectSides: boolean = (this.a + this.b) <= this.c
+    const isValidLength: boolean = this.a <= 0 || this.b <= 0 || this.c <= 0;
+    const isValidSides: boolean = (this.a + this.b) <= this.c
     || (this.a + this.c) <= this.b
     || (this.c + this.b) <= this.a;
 
-    if (incorrectLength || incorrectSides) {
-      throw new Error('Enter correct data');
+    if (isValidLength) {
+      throw new Error('Sides must be bigger than 0');
+    }
+
+    if (isValidSides) {
+      throw new Error('The longest side of a triangle must be '
+      + 'bigger than a sum of two others');
     }
   }
 
@@ -51,7 +56,7 @@ export class Circle implements Figure {
     private radius: number,
   ) {
     if (this.radius <= 0) {
-      throw new Error('Enter correct data');
+      throw new Error('Radius must be bigger than 0');
     }
   }
 
@@ -71,7 +76,7 @@ export class Rectangle implements Figure {
     if (this.width <= 0
       || this.height <= 0
     ) {
-      throw new Error('Enter correct data');
+      throw new Error('Sides must be bigger than 0');
     }
   }
 
