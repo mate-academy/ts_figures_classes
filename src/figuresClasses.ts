bb7e7e8ae62841fe8c76531d7cf427d15f3c64e2
Mikedacheka <mikedacheka@gmail.com>
2022-01-13 14:45:19 +0200

    Naming

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,3 +1,11 @@
+enum Shape {
+  Triangle = 'triangle',
+  Circle = 'circle',
+  Rectangle = 'rectangle',
+}
+
+type Color = 'red' | 'blue' | 'green';
+
 export interface Figure {
   shape: string,
   color: string,
@@ -6,75 +14,80 @@ export interface Figure {
 
 export class Triangle implements Figure {
   constructor(
-    public color: string,
+    public color: Color,
     public a: number,
     public b: number,
     public c: number,
   ) {
-    const MaxValue = Math.max(a, b, c);
+    const MaxSizeOfSide = Math.max(a, b, c);
 
-    if (a <= 0 || b <= 0 || c <= 0 || MaxValue >= (a + b + c) - MaxValue) {
-      throw new Error('Invalid Values');
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('Invalid value/s for sides(values > 0) ');
+    }
+
+    if (MaxSizeOfSide >= (a + b + c) - MaxSizeOfSide) {
+      throw new Error(`The sum of the lengths of
+      any two sides of a triangle
+      has to be greater than the length of the third side.`);
     }
     this.color = color;
   }
 
-  public shape = 'triangle';
+  public shape = Shape.Triangle;
 
   getArea(): number {
-    const s = (this.a + this.b + this.c) / 2;
-    const area = Math.sqrt(s
-      * (s - this.a)
-      * (s - this.b)
-      * (s - this.c));
+    const halfThePerimeter = (this.a + this.b + this.c) / 2;
+    const areaOfTriangle = Math.sqrt(halfThePerimeter
+      * (halfThePerimeter - this.a)
+      * (halfThePerimeter - this.b)
+      * (halfThePerimeter - this.c));
 
-    return Number(area.toFixed(2));
+    return Number(areaOfTriangle.toFixed(2));
   }
 }
 
 export class Circle {
   constructor(
-    public color: string,
-    public r: number,
+    public color: Color,
+    public radius: number,
   ) {
-    if (r <= 0) {
-      throw new Error('Invalid value');
+    if (radius <= 0) {
+      throw new Error('Invalid value for radius size(value > 0)');
     }
-    this.r = r;
     this.color = color;
   }
 
-  public shape = 'circle';
+  public shape = Shape.Circle;
 
   getArea(): number {
-    let area = Math.PI * (this.r * this.r);
+    let circleArea = Math.PI * (this.radius * this.radius);
 
-    area = Math.trunc(area * 100) / 100;
+    circleArea = Math.trunc(circleArea * 100) / 100;
 
-    return area;
+    return circleArea;
   }
 }
 
 export class Rectangle {
   constructor(
-    public color: string,
+    public color: Color,
     public a: number,
     public b: number,
   ) {
     if (a < 0 || b < 0) {
-      throw new Error('Invalid Values');
+      throw new Error('Invalid Value/s for sides(values > 0)');
     }
     this.color = color;
   }
 
-  public shape = 'rectangle';
+  public shape = Shape.Rectangle;
 
   getArea(): number {
-    let area = this.a * this.b;
+    let rectangleArea = this.a * this.b;
 
-    area = Math.trunc(area * 100) / 100;
+    rectangleArea = Math.trunc(rectangleArea * 100) / 100;
 
-    return area;
+    return rectangleArea;
   }
 }
 
