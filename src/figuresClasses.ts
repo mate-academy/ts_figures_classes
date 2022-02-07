8e1ea0e50641948e9b7371116c0b773099f8bac9
Osypiuk Dmytro <dimich578ua@gmail.com>
2022-02-07 16:59:48 +0200

    solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,77 @@
 export interface Figure {
-
+  shape: string;
+  color: string;
+  getArea(): number;
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  public shape: string;
 
+  constructor(
+    public color: string,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    this.shape = this.constructor.name.toLowerCase();
+
+    if (this.a + this.b <= this.c
+        || this.a + this.c <= this.b
+        || this.b + this.c <= this.a
+        || [this.a, this.b, this.c].some((side: number) => side <= 0)) {
+      throw new Error('Incorrect value!');
+    }
+  }
+
+  getArea(): number {
+    const s = (this.a + this.b + this.c) / 2;
+
+    return Math
+      .round(Math
+        .sqrt(s * (s - this.a) * (s - this.b) * (s - this.c)) * 100) / 100;
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  public shape: string;
 
+  constructor(
+    public color: string,
+    public radius: number,
+  ) {
+    this.shape = this.constructor.name.toLowerCase();
+
+    if (this.radius <= 0) {
+      throw new Error('Incorrect value!');
+    }
+  }
+
+  getArea(): number {
+    return Math.trunc((Math.PI * this.radius ** 2) * 100) / 100;
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  public shape: string;
 
+  constructor(
+    public color: string,
+    public width: number,
+    public hight: number,
+  ) {
+    this.shape = this.constructor.name.toLowerCase();
+
+    if ([this.width, this.hight].some((side: number) => side <= 0)) {
+      throw new Error('Incorrect value!');
+    }
+  }
+
+  getArea(): number {
+    return Math.round(this.width * this.hight * 100) / 100;
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape}`
+  + ` - ${figure.getArea()}`;
 }
