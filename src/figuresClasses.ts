5eb18ae48f6571db25e9b1c3c6312b1c7f4e63a9
Vadym Mishchenko <m.i.c.h.m.a.n.94@gmail.com>
2022-02-09 17:19:55 +0200

    figures classes were created

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,83 @@
-export interface Figure {
+type Color = 'red' | 'green' | 'blue';
+type Shape = 'triangle' | 'circle' | 'rectangle';
 
+export interface Figure {
+  shape: Shape,
+  color: Color,
+  getArea : () => number;
 }
 
 export class Triangle {
+  public shape: Shape;
 
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    this.shape = 'triangle';
+
+    if (this.a >= this.b + this.c
+      || this.b >= this.a + this.c
+      || this.c >= this.a + this.b
+    ) {
+      throw new Error('invalid data: Input correct values');
+    }
+
+    if (this.a === 0 || this.b === 0 || this.c === 0) {
+      throw new Error('invalid data: Side can not be 0');
+    }
+  }
+
+  getArea(): number {
+    const p: number = (this.a + this.b + this.c) / 2;
+
+    return Math.round(
+      Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)) * 100,
+    ) / 100;
+  }
 }
 
 export class Circle {
+  public shape: Shape;
 
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    this.shape = 'circle';
+
+    if (this.radius <= 0) {
+      throw new Error('invalid data: Radius can not be 0');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor((Math.PI * (this.radius ** 2) * 100)) / 100;
+  }
 }
 
 export class Rectangle {
+  public shape: Shape;
 
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+  ) {
+    this.shape = 'rectangle';
+
+    if (a <= 0 || b <= 0) {
+      throw new Error('invalid data: Side can not be 0');
+    }
+  }
+
+  getArea(): number {
+    return this.a * this.b;
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
