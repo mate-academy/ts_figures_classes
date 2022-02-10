b5e17916291102be2d09ddf9044dbc6e72bc0c3f
Roma Boreiko <roma.boreiko@gmail.com>
2022-02-10 17:49:05 +0200

    Solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,80 @@
+type Shape = 'triangle' | 'circle' | 'rectangle';
+type Color = 'red' | 'green' | 'blue';
+
+export function round(num: number): number {
+  return Math.floor(num * 100) / 100;
+}
+
 export interface Figure {
-
+  shape: Shape
+  color: Color
+  getArea: () => number
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  shape: Shape = 'triangle';
 
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (a >= (b + c) || b >= (a + c) || c >= (b + a)) {
+      throw new Error('Wrong value one of sides!');
+    }
+
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('Sides should be positive!');
+    }
+  }
+
+  getArea(): number {
+    const { a, b, c } = this;
+
+    const halfSum = (a + b + c) / 2;
+
+    return round(Math.sqrt(
+      halfSum * (halfSum - a) * (halfSum - b) * (halfSum - c),
+    ));
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  shape: Shape = 'circle';
 
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (this.radius <= 0) {
+      throw new Error('Radius should be positive!');
+    }
+  }
+
+  getArea(): number {
+    return round(Math.PI * this.radius * this.radius);
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  shape: Shape = 'rectangle';
 
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('Sides should be positive!');
+    }
+  }
+
+  getArea(): number {
+    return round(this.width * this.height);
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
