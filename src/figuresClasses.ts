caa96fa1082186b21ea4df10b3ee1c68e9beb9d7
Roma Afanasiev <romafanasiev123@gmail.com>
2022-02-15 21:16:49 +0200

    add function

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,89 @@
 export interface Figure {
-
+  shape : string,
+  color : string,
+  getArea() : number | Error,
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  public shape: string;
 
+  constructor(
+    public color: string,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    this.shape = 'triangle';
+    this.color = color;
+    this.a = a;
+    this.b = b;
+    this.c = c;
+
+    if ((a <= 0 || b <= 0 || c <= 0)
+      || Math.max(a, b, c) >= a + b + c - Math.max(a, b, c)) {
+      throw new Error('Not valid data');
+    }
+  }
+
+  getArea(): number | Error {
+    const p = (this.a + this.b + this.c) / 2;
+    const area = Math.sqrt(p * (p - this.a)
+    * (p - this.b) * (p - this.c));
+
+    return (Math.floor(area * 100) / 100);
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  public shape: string;
 
+  constructor(
+    public color: string,
+    public radius: number,
+  ) {
+    this.shape = 'circle';
+    this.color = color;
+    this.radius = radius;
+
+    if (radius <= 0) {
+      throw new Error('Not valid data');
+    }
+  }
+
+  getArea(): number | Error {
+    const area = Math.PI * (this.radius ** 2);
+
+    return (Math.floor(area * 100) / 100);
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  public shape: string;
 
+  constructor(
+    public color: string,
+    public width: number,
+    public height: number,
+  ) {
+    this.shape = 'rectangle';
+    this.color = color;
+    this.width = width;
+    this.height = height;
+
+    if (height <= 0 || width <= 0) {
+      throw new Error('Not valid data');
+    }
+  }
+
+  getArea(): number | Error {
+    const area = this.height * this.width;
+
+    return area;
+  }
 }
 
-export function getInfo(figure) {
+export function getInfo(figure) : string {
+  const info = `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 
+  return info;
 }
