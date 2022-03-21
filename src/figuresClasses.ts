d6fcccbd9f365ce747e1b755b192270290e24c9c
Max Shlikhta <maxok.vibe@gmail.com>
2022-03-21 21:04:20 +0200

    Figures area

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,85 @@
+type Shape = 'triangle' | 'circle' | 'rectangle';
+type Color = 'red' | 'green' | 'blue';
+
 export interface Figure {
+  shape: Shape;
+  color: Color;
 
+  getArea(): number;
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  shape: Shape = 'triangle';
 
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (this.a <= 0 || this.b <= 0 || this.c <= 0) {
+      throw new Error('Invalid one or more properties value!');
+    }
+
+    const sides = [this.a, this.b, this.c]
+      .sort((side1, side2) => side2 - side1);
+
+    if (sides[0] >= sides[1] + sides[2]) {
+      throw new Error(`Sides ${this.a}, ${this.b}`
+        + `and ${this.c} can't form a triangle!`);
+    }
+  }
+
+  getArea(): number {
+    const semiPerimeter: number = (this.a + this.b + this.c) / 2;
+    const square: number = Math.sqrt(semiPerimeter
+      * (semiPerimeter - this.a)
+      * (semiPerimeter - this.b)
+      * (semiPerimeter - this.c));
+
+    return Math.floor(square * 100) / 100;
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  shape: Shape = 'circle';
 
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (this.radius <= 0) {
+      throw new Error(`Invalid property value: ${this.radius}!`);
+    }
+  }
+
+  getArea(): number {
+    const square: number = this.radius ** 2 * Math.PI;
+
+    return Math.floor(square * 100) / 100;
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  shape: Shape = 'rectangle';
 
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
+    if (this.width <= 0 || this.height <= 0) {
+      throw new Error('Invalid one or more properties value!');
+    }
+  }
+
+  getArea(): number {
+    const square: number = this.width * this.height;
+
+    return Math.floor(square * 100) / 100;
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
