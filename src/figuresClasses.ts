2ff5703de3a58c4194ebea57b959edf4dae8eed0
Max Nikiforov <maxxnikiforov@gmail.com>
2022-04-23 15:51:56 +0700

    solution Figures

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,79 @@
+enum Shape {
+  Triangle = 'triangle',
+  Circle = 'circle',
+  Rectangle = 'rectangle',
+}
+
+enum Color {
+  Red = 'red',
+  Green = 'green',
+  Blue = 'blue',
+}
+
 export interface Figure {
-
+  shape: Shape,
+  color: Color,
+  getArea(): number,
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  public shape = Shape.Triangle;
 
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('Error!');
+    }
+
+    if (Math.max(a, b, c) >= (a + b + c - Math.max(a, b, c))) {
+      throw new Error('not a sides of triangle');
+    }
+  }
+
+  getArea(): number {
+    const s = (this.a + this.b + this.c) / 2;
+
+    return +Math.sqrt(s * (s - this.a)
+      * (s - this.b) * (s - this.c)).toFixed(2);
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  public shape = Shape.Circle;
 
+  constructor(public color: Color, public radius: number) {
+    if (radius <= 0) {
+      throw new Error('Error!');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor((Math.PI * this.radius ** 2) * 100) / 100;
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  public shape = Shape.Rectangle;
 
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('Error!');
+    }
+  }
+
+  getArea(): number {
+    return this.width * this.height;
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
