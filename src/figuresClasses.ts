38b71fd02d3c4f2130e0df15744f1d01184a0c77
Nikita Prepelytsia <prep.nikita@gmail.com>
2022-04-29 08:04:49 +0300

    fix

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -4,9 +4,15 @@ enum Shape {
   Rectangle = 'rectangle'
 }
 
+enum Color {
+  Red = 'red',
+  Green = 'green',
+  Blue = 'blue',
+}
+
 export interface Figure {
   shape: Shape;
-  color: string;
+  color: Color;
   getArea(): number;
 }
 
@@ -14,17 +20,17 @@ export class Triangle implements Figure {
   public shape = Shape.Triangle;
 
   constructor(
-    public color: string,
+    public color: Color,
     public a: number,
     public b: number,
     public c: number,
   ) {
     if (a <= 0 || b <= 0 || c <= 0) {
-      throw new Error('Unreal');
+      throw new Error('All params have to be more than 0');
     }
 
     if (a + b <= c || a + c <= b || b + c <= a) {
-      throw new Error('Unreal');
+      throw new Error('Every side shouldnt be more than other two');
     }
   }
 
@@ -41,11 +47,11 @@ export class Circle implements Figure {
   public shape = Shape.Circle;
 
   constructor(
-    public color: string,
+    public color: Color,
     public radius: number,
   ) {
     if (radius <= 0) {
-      throw new Error('Unreal');
+      throw new Error('Radius have to be more than 0');
     }
   }
 
@@ -58,12 +64,12 @@ export class Rectangle implements Figure {
   public shape = Shape.Rectangle;
 
   constructor(
-    public color: string,
+    public color: Color,
     public height: number,
     public width: number,
   ) {
     if (height <= 0 || width <= 0) {
-      throw new Error('Unreal');
+      throw new Error('All sides have to be more than 0');
     }
   }
 
@@ -72,6 +78,6 @@ export class Rectangle implements Figure {
   }
 }
 
-export function getInfo(figure): string {
+export function getInfo(figure: Figure): string {
   return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
