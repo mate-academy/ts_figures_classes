542ced4f6d247f750022f4514811eef832ae34c9
Daria Liubymova <dellachaise@gmail.com>
2022-05-23 15:12:28 +0300

    Added changes accoding to comments

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,5 +1,10 @@
-type Shapes = 'triangle' | 'circle' | 'rectangle';
 type Color = 'red' | 'green' | 'blue';
+
+enum Shapes {
+  Triangle = 'triangle',
+  Circle = 'circle',
+  Rectangle = 'rectangle',
+}
 
 export interface Figure {
   shape: Shapes,
@@ -13,22 +18,18 @@ function validator(key: string, value: number):void {
   }
 }
 
+function rounder(square: number): number {
+  return Math.floor(square * 100) / 100;
+}
+
 export class Triangle implements Figure {
-  shape: Shapes = 'triangle';
-
-  color: Color;
-
-  a: number;
-
-  b: number;
-
-  c: number;
+  shape = Shapes.Triangle;
 
   constructor(
-    color: Color,
-    a: number,
-    b: number,
-    c: number,
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
   ) {
     validator('a', a);
     validator('b', b);
@@ -40,10 +41,6 @@ export class Triangle implements Figure {
     if (arraySides[2] >= (arraySides[1] + arraySides[0])) {
       throw new Error('Please enter correct side valies!');
     }
-    this.color = color;
-    this.a = a;
-    this.b = b;
-    this.c = c;
   }
 
   getArea(): number {
@@ -52,59 +49,43 @@ export class Triangle implements Figure {
     const square = Math.sqrt(semiPerimetr * (semiPerimetr - this.a)
       * (semiPerimetr - this.b) * (semiPerimetr - this.c));
 
-    return Math.round(square * 100) / 100;
+    return rounder(square);
   }
 }
 
 export class Circle {
-  shape: Shapes = 'circle';
-
-  color: Color;
-
-  radius: number;
+  shape = Shapes.Circle;
 
   constructor(
-    color: Color,
-    radius: number,
+    public color: Color,
+    public radius: number,
   ) {
     validator('radius', radius);
-
-    this.color = color;
-    this.radius = radius;
   }
 
   getArea(): number {
     const square = Math.PI * this.radius * this.radius;
 
-    return Math.floor(square * 100) / 100;
+    return rounder(square);
   }
 }
 
 export class Rectangle {
-  shape: Shapes = 'rectangle';
-
-  color: Color;
-
-  width: number;
-
-  height: number;
+  shape = Shapes.Rectangle;
 
   constructor(
-    color: Color,
-    width: number,
-    height: number,
+    public color: Color,
+    public width: number,
+    public height: number,
   ) {
     validator('width', width);
     validator('heigth', height);
-    this.color = color;
-    this.width = width;
-    this.height = height;
   }
 
   getArea(): number {
     const square = this.width * this.height;
 
-    return Math.round(square * 100) / 100;
+    return rounder(square);
   }
 }
 
