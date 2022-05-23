b083ab7627443bb92b54a63786aced6438b07cc8
efdalyalcin <efdalyalcin@hotmail.com>
2022-05-23 15:26:53 +0300

    solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,84 @@
+enum Shapes {
+  Circle = 'circle',
+  Rectangle = 'rectangle',
+  Triangle = 'triangle',
+}
+
+type Color = 'red' | 'green' | 'blue';
+
+function roundNumbers(num: number): number {
+  return Math.floor(num * 100) / 100;
+}
+
 export interface Figure {
-
+  shape: Shapes;
+  color: Color;
+  getArea(): number;
 }
 
-export class Triangle {
+export class Circle implements Figure {
+  public shape = Shapes.Circle;
 
+  constructor(
+    public color: Color,
+    public a: number,
+  ) {
+    if (a <= 0) {
+      throw new Error('Incorrect radius');
+    }
+  }
+
+  getArea(): number {
+    return roundNumbers((this.a ** 2) * Math.PI);
+  }
 }
 
-export class Circle {
+export class Rectangle extends Circle {
+  public shape = Shapes.Rectangle;
 
+  constructor(
+    color: Color,
+    a: number,
+    public b: number,
+  ) {
+    super(color, a);
+
+    if (a <= 0 || b <= 0) {
+      throw new Error('This is not a rectangle');
+    }
+  }
+
+  getArea(): number {
+    return roundNumbers(this.a * this.b);
+  }
 }
 
-export class Rectangle {
+export class Triangle extends Rectangle {
+  public shape = Shapes.Triangle;
 
+  constructor(
+    color: Color,
+    a: number,
+    b: number,
+    public c: number,
+  ) {
+    super(color, a, b);
+
+    if (a + b <= c || b + c <= a || a + c <= b) {
+      throw new Error('This sides doesn`t create a valid triangle');
+    }
+  }
+
+  getArea(): number {
+    const firstPartOfHeron = 4 * (this.a ** 2) * (this.b ** 2);
+    const secondPartOfHeron = ((this.a ** 2) + (this.b ** 2)
+      - (this.c ** 2)) ** 2;
+
+    return roundNumbers((1 / 4)
+      * ((firstPartOfHeron - secondPartOfHeron) ** (1 / 2)));
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
