6bd529aef4925e01c69608fc0d21f731bd1f8b57
Uktam19980416 <rakhmatullayev2019@mail.ru>
2022-05-25 18:24:58 +0200

    solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,85 @@
+enum Shape {
+  Triangle = 'triangle',
+  Rectangle = 'rectangle',
+  Circle = 'circle',
+}
+
+enum Color {
+  red = 'red',
+  green = 'green',
+  blue = 'blue',
+}
+
 export interface Figure {
-
+  shape: Shape,
+  color: Color,
+  getArea(): number,
 }
 
-export class Triangle {
-
+function areValidDimensions(...sides: number[]): void {
+  if (sides.some((side: number) => side <= 0)) {
+    throw new Error('Dimensions are not valid for triangle');
+  }
 }
 
-export class Circle {
-
+function rounded(number: number): number {
+  return Math.floor(number * 100) / 100;
 }
 
-export class Rectangle {
+export class Triangle implements Figure {
+  public shape: Shape = Shape.Triangle;
 
+  constructor(
+    public color: Color,
+    private a: number,
+    private b: number,
+    private c: number,
+  ) {
+    areValidDimensions(a, b, c);
+
+    if (a >= b + c || b >= a + c || c >= a + b) {
+      throw new Error('One side is longer than other side!');
+    }
+  }
+
+  getArea(): number {
+    const s = (this.a + this.b + this.c) / 2;
+
+    return rounded(Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c)));
+  }
 }
 
-export function getInfo(figure) {
+export class Circle implements Figure {
+  public shape: Shape = Shape.Circle;
 
+  constructor(
+    public color: Color,
+    private radius: number,
+  ) {
+    areValidDimensions(radius);
+  }
+
+  getArea(): number {
+    return rounded(Math.PI * (this.radius ** 2));
+  }
+}
+
+export class Rectangle implements Figure {
+  public shape: Shape = Shape.Rectangle;
+
+  constructor(
+    public color: Color,
+    private width: number,
+    private height: number,
+  ) {
+    areValidDimensions(width, height);
+  }
+
+  getArea(): number {
+    return rounded(this.width * this.height);
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
