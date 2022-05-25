4f85c9564341c4d0789f075ca69a819dbfe47bc6
Hubert Wolak <29hihub@gmail.com>
2022-05-25 12:45:38 +0200

    Solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,100 @@
+export enum Shape {
+  Triangle = 'triangle',
+  Circle = 'circle',
+  Rectangle = 'rectangle',
+}
+
+type Color = 'red'|'green'|'blue';
+
 export interface Figure {
-
+  shape: Shape,
+  color: Color,
+  getArea(): number,
 }
 
-export class Triangle {
+export function heronsFormula(sides: number[]): boolean {
+  const sortedSides = sides.sort((a, b) => a - b);
 
+  if (sortedSides[0] + sortedSides[1] <= sortedSides[2]) {
+    return false;
+  }
+
+  return true;
 }
 
-export class Circle {
+export function greaterThanZero(args: number[]): boolean {
+  if (args.every((x) => x > 0)) {
+    return true;
+  }
 
+  return false;
 }
 
-export class Rectangle {
-
+export function roundToHund(x: number): number {
+  return Math.floor(x * 100) / 100;
 }
 
-export function getInfo(figure) {
+export class Triangle implements Figure {
+  shape: Shape = Shape.Triangle;
 
+  constructor(
+    public color: Color,
+    private a: number,
+    private b: number,
+    private c: number,
+  ) {
+    if (!greaterThanZero([a, b, c])) {
+      throw new Error('Side/Sides lesser than 0');
+    }
+
+    if (!heronsFormula([a, b, c])) {
+      throw new Error('Triangle sides doesnt match Herons Formula');
+    }
+  }
+
+  getArea(): number {
+    const s: number = (this.a + this.b + this.c) / 2;
+    const area = Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+
+    return roundToHund(area);
+  }
+}
+
+export class Circle implements Figure {
+  shape: Shape = Shape.Circle;
+
+  constructor(
+    public color: Color,
+    private radius: number,
+  ) {
+    if (!greaterThanZero([radius])) {
+      throw new Error('Invalid radius');
+    }
+  }
+
+  getArea(): number {
+    return roundToHund(Math.PI * (this.radius ** 2));
+  }
+}
+
+export class Rectangle implements Figure {
+  shape: Shape = Shape.Rectangle;
+
+  constructor(
+    public color: Color,
+    private width: number,
+    private height: number,
+  ) {
+    if (!greaterThanZero([width, height])) {
+      throw new Error('Invalid arguments');
+    }
+  }
+
+  getArea(): number {
+    return roundToHund(this.width * this.height);
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
