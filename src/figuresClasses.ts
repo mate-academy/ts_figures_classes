8a7c7b4256c85a317c9f71afcff2797d87788107
Anastasiia Sydorchuk <nastia.sydorchuk@gmail.com>
2022-06-03 22:44:31 +0200

    solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,83 @@
+// import { Literal } from '@babel/types';
+// import { LiteralType, UnionType } from 'typescript';
+
+type Shape = 'triangle' | 'circle' | 'rectangle';
+type Color = 'red' | 'green' | 'blue';
+
+function getRounded(area: number): number {
+  return Math.floor(area * 100) / 100;
+}
+
 export interface Figure {
-
+  shape: Shape;
+  color: Color;
+  getArea(): number;
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  public shape: Shape = 'triangle';
 
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (this.a <= 0 || this.b <= 0 || this.c <= 0) {
+      throw new Error('Not a triangle');
+    }
+
+    if (this.a + this.b <= this.c
+        || this.a + this.c <= this.b
+        || this.b + this.c <= this.a) {
+      throw new Error('Not a triangle');
+    }
+  }
+
+  getArea(): number {
+    const s: number = (this.a + this.b + this.c) / 2;
+    const area: number = Math.sqrt(s * (s - this.a) * (s - this.b)
+    * (s - this.c));
+
+    return getRounded(area);
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  public shape: Shape = 'circle';
 
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (this.radius <= 0) {
+      throw new Error('Not a circle');
+    }
+  }
+
+  getArea(): number {
+    return getRounded(Math.PI * this.radius ** 2);
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  public shape: Shape = 'rectangle';
 
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
+    if (this.width <= 0 || this.height <= 0) {
+      throw new Error('Not a rectangle');
+    }
+  }
+
+  getArea(): number {
+    return getRounded(this.height * this.width);
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Triangle | Circle | Rectangle): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
