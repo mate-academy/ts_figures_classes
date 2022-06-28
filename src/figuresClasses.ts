74d8671b3a404f9655dd7cdd476840992f86c646
SAMAEV <andreysamaev@gmail.com>
2022-06-28 11:21:27 +0300

    figureout1

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,75 @@
+type Shape = 'triangle' | 'circle' | 'rectangle';
+type Color = 'red' | 'green' | 'blue';
+
 export interface Figure {
-
+  shape: Shape;
+  color: Color;
+  getArea(): number;
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  shape: Shape = 'triangle';
 
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (a <= 0
+        || b <= 0
+        || c <= 0
+        || (Math.max(a, b, c) >= (a + b + c) - Math.max(a, b, c))
+    ) {
+      throw new Error('Triangle doesn`t exsist');
+    }
+  }
+
+  getArea(): number {
+    const halfPerimetr = (this.a + this.b + this.c) / 2;
+
+    return Math.floor(Math.sqrt(halfPerimetr
+      * (halfPerimetr - this.a)
+      * (halfPerimetr - this.b)
+      * (halfPerimetr - this.c)) * 100) / 100;
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  shape: Shape = 'circle';
 
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('Circle doesn`t exsist with R < 0');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor((Math.PI * this.radius ** 2) * 100) / 100;
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  shape: Shape = 'rectangle';
 
+  constructor(
+    public color: Color,
+    public height: number,
+    public width: number,
+  ) {
+    if (height * width <= 0) {
+      throw new Error('Rectangle doesn`t exsist with negative side');
+    }
+  }
+
+  getArea(): number {
+    return this.height * this.width;
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
