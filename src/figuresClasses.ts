5b8faa76052f6518ed52bf6255d08a6c40496d2c
Alina Melentieva <melenteva2906@gmail.com>
2022-07-01 21:07:22 +0300

    Solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,79 @@
+type Shape = 'triangle' | 'circle' | 'rectangle';
+type Color = 'red' | 'green' | 'blue';
+
 export interface Figure {
-
+  shape?: Shape;
+  color?: Color;
+  getArea(): number;
 }
 
-export class Triangle {
-
+export enum ShapeName {
+  TRIANGLE = 'triangle',
+  CIRCLE = 'circle',
+  RECTANGLE = 'rectangle',
 }
 
-export class Circle {
+export class Triangle implements Figure {
+  shape: Shape = ShapeName.TRIANGLE;
 
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (a < 0 || b < 0 || c < 0) {
+      throw new Error("can't be less than 0");
+    }
+
+    if (a >= b + c || b >= a + c || c >= a + b) {
+      throw new Error('One of the sides bigger than two others');
+    }
+  }
+
+  getArea(): number {
+    const s = (this.a + this.b + this.c) / 2;
+    const area = Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+
+    return Number(area.toFixed(2));
+  }
 }
 
-export class Rectangle {
+export class Circle implements Figure {
+  shape: Shape = ShapeName.CIRCLE;
 
+  constructor(
+    public color: Color,
+    public r: number,
+  ) {
+    if (r <= 0) {
+      throw new Error("can't be less than 0");
+    }
+  }
+
+  getArea(): number {
+    return Math.floor((Math.PI * this.r ** 2) * 100) / 100;
+  }
 }
 
-export function getInfo(figure) {
+export class Rectangle implements Figure {
+  shape: Shape = ShapeName.RECTANGLE;
 
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+  ) {
+    if (a < 0 || b < 0) {
+      throw new Error("can't be less than 0");
+    }
+  }
+
+  getArea(): number {
+    return this.a * this.b;
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
