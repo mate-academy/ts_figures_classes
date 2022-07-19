b0e71bc7702dbf29a228a86ce35299bde4fc024d
PaulRodriguez2049 <paul.rodriguez.2049@gmail.com>
2022-07-20 00:55:19 +0300

    sec com

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,20 +1,30 @@
 export interface Figure {
-  shape:string;
-  color:string;
-  getArea():number;
+  shape: string;
+  color: string;
+  getArea(): number;
 }
 
+enum Shapes {
+  triangle = 'triangle',
+  circle = 'circle',
+  rectangle = 'rectangle',
+}
+
+const roundFoo = (variable:number): number => {
+  return Math.floor(variable * 100) / 100;
+};
+
 export class Triangle implements Figure {
-  public shape = 'triangle';
+  public shape = Shapes.triangle;
 
   constructor(
-    public color:string,
-    public a:number,
-    public b:number,
-    public c:number,
+    public color: string,
+    public a: number,
+    public b: number,
+    public c: number,
   ) {
     if (this.a <= 0 || this.b <= 0 || this.c <= 0) {
-      throw new Error('one of the side is less or equil = 0');
+      throw new Error('one of the side is less or equal = 0');
     }
 
     if (this.a >= this.b + this.c
@@ -24,49 +34,49 @@ export class Triangle implements Figure {
     }
   }
 
-  getArea():number {
+  getArea(): number {
     const base = 4 * (this.a ** 2) * (this.b ** 2)
      - ((this.a ** 2) + (this.b ** 2) - (this.c ** 2)) ** 2;
 
-    return Math.floor(1 / 4 * base ** (1 / 2) * 100) / 100;
+    return roundFoo(1 / 4 * base ** (1 / 2));
   }
 }
 
 export class Circle implements Figure {
-  public shape = 'circle';
+  public shape = Shapes.circle;
 
   constructor(
-    public color:string,
-    public a:number,
+    public color: string,
+    public a: number,
   ) {
     if (this.a <= 0) {
-      throw new Error('radius is less or equil 0');
+      throw new Error('radius is less or equal 0');
     }
   }
 
   getArea(): number {
-    const result = Math.floor((Math.PI * (this.a ** 2)) * 100) / 100;
+    const result = roundFoo((Math.PI * (this.a ** 2)));
 
     return result;
   }
 }
 
 export class Rectangle implements Figure {
-  public shape = 'rectangle';
+  public shape = Shapes.rectangle;
 
   constructor(
-    public color:string,
-    public a:number,
-    public b:number,
+    public color: string,
+    public a: number,
+    public b: number,
   ) {
     if (this.a <= 0
         || this.b <= 0) {
-      throw new Error('one of the sides is less or equil 0');
+      throw new Error('one of the sides is less or equal 0');
     }
   }
 
   getArea(): number {
-    return this.a * this.b;
+    return roundFoo(this.a * this.b);
   }
 }
 
