f0103ae5bf439135f67fa549e9840c5c8fe860e6
silchyn <ivan.silchin@gmail.com>
2022-07-20 15:53:57 +0300

    added figures

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,63 @@
 export interface Figure {
-
+  shape: 'triangle' | 'circle' | 'rectangle';
+  color: 'red' | 'green' | 'blue';
+  getArea: () => number;
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public a: number,
+    public b: number,
+    public c: number,
+    public shape: 'triangle' | 'circle' | 'rectangle' = 'triangle',
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0 || a >= b + c || b >= a + c || c >= a + b) {
+      throw new Error('Invalid sides');
+    }
+  }
 
+  getArea(): number {
+    const s = (this.a + this.b + this.c) / 2;
+
+    return Math.round(Math.sqrt(s * (s - this.a)
+      * (s - this.b) * (s - this.c)) * 100) / 100;
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public radius: number,
+    public shape: 'triangle' | 'circle' | 'rectangle' = 'circle',
+  ) {
+    if (radius <= 0) {
+      throw new Error('Invalid sides');
+    }
+  }
 
+  getArea(): number {
+    return Math.trunc(this.radius ** 2 * Math.PI * 100) / 100;
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public width: number,
+    public height: number,
+    public shape: 'triangle' | 'circle' | 'rectangle' = 'rectangle',
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('Invalid sides');
+    }
+  }
 
+  getArea(): number {
+    return Math.round(this.width * this.height * 100) / 100;
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
