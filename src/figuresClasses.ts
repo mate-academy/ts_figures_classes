a5bceb4dfc278e7da61bd2263426c71c7649355b
dasha <dashasv76@gmail.com>
2022-07-26 17:11:50 +0300

    figure classes

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,68 @@
-export interface Figure {
+type ShapeType = 'triangle' | 'circle' | 'rectangle';
 
+export interface Figure {
+  shape: ShapeType;
+  color: 'red' | 'green' | 'blue';
+  getArea: () => number;
 }
 
 export class Triangle {
+  shape = 'triangle';
 
+  constructor(
+    public color: ShapeType,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0 || a >= b + c || b >= a + c || c >= a + b) {
+      throw new Error('Invalid sides');
+    }
+  }
+
+  getArea(): number {
+    const p = (this.a + this.b + this.c) / 2;
+
+    return Math.floor(Math.sqrt(p * (p - this.a) * (p - this.b)
+        * (p - this.c)) * 100) / 100;
+  }
 }
 
 export class Circle {
+  shape = 'circle';
 
+  constructor(
+    public color: ShapeType,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('Invalid sides');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor(Math.PI * (this.radius ** 2) * 100) / 100;
+  }
 }
 
 export class Rectangle {
+  shape = 'rectangle';
 
+  constructor(
+    public color: ShapeType,
+    public a: number,
+    public b: number,
+  ) {
+    if (a <= 0 || b <= 0) {
+      throw new Error('Invalid sides');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor(this.a * this.b * 100) / 100;
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
