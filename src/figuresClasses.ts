e49ec77c0b0e6804c4c0b56dfba33f6fae0957da
Aleksandr Yuschenko <105875778+AleksandrYuschenko@users.noreply.github.com>
2022-07-27 19:21:08 +0300

    solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,76 @@
+
+type Shapes = 'triangle' | 'circle' | 'rectangle';
+type Color = 'red' | 'green' | 'blue';
 export interface Figure {
-
+  shape: Shapes,
+  color: Color,
+  getArea(): number;
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  public shape: Shapes = 'triangle';
 
+  constructor(
+    public color: Color,
+    public sideA: number,
+    public sideB: number,
+    public sideC: number,
+  ) {
+    const maxSide = Math.max(sideA, sideB, sideC);
+
+    if (sideA <= 0 || sideB <= 0 || sideC <= 0
+      || maxSide >= sideA + sideB + sideC - maxSide) {
+      throw new Error('Error: entered data is invalid!');
+    }
+  }
+
+  getArea(): number {
+    const semiPerimeter = (this.sideA + this.sideB + this.sideC) / 2;
+
+    return Math.floor(100 * Math.sqrt(semiPerimeter
+      * (semiPerimeter - this.sideA)
+      * (semiPerimeter - this.sideB)
+      * (semiPerimeter - this.sideC))) / 100;
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  public shape: Shapes = 'circle';
 
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('Error: entered data is invalid!');
+    }
+  }
+
+  getArea(): number {
+    const circleArea: number = Math.PI * (this.radius * this.radius);
+
+    return (Math.floor(circleArea * 100)) / 100;
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  public shape: Shapes = 'rectangle';
 
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
+    if (this.width < 0 || this.height < 0) {
+      throw new Error('Error: entered data is invalid!');
+    }
+  }
+
+  getArea(): number {
+    return this.width * this.height;
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
