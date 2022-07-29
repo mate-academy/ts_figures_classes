a69b4ec93e8d348c79594992512a927c073f3fd0
Misha Dzhemula <medvedmisha23@gmail.com>
2022-07-29 17:01:18 +0300

    solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,80 @@
+type Shape = 'triangle' | 'circle' | 'rectangle';
+type Color = 'red' | 'green' | 'blue';
+
 export interface Figure {
-
+  shape: Shape,
+  color: Color,
+  getArea(): number,
 }
 
-export class Triangle {
-
+function round(num: number): number {
+  return Math.floor(num * 100) / 100;
 }
 
-export class Circle {
+export class Triangle implements Figure {
+  shape: Shape = 'triangle';
 
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    const maxSide = Math.max(a, b, c);
+    const sumOfLowSides = [a, b, c]
+      .filter((side) => side !== maxSide)
+      .reduce((sum, side) => sum + side);
+
+    if (a <= 0 || b <= 0 || c <= 0 || maxSide >= sumOfLowSides) {
+      throw new Error('error');
+    }
+  }
+
+  getArea(): number {
+    return round(1 / 4 * Math.sqrt(
+      (this.a + this.b + this.c)
+      * (this.b + this.c - this.a)
+      * (this.a - this.b + this.c)
+      * (this.a + this.b - this.c),
+    ));
+  }
 }
 
-export class Rectangle {
+export class Circle implements Figure {
+  shape: Shape = 'circle';
 
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('error');
+    }
+  }
+
+  getArea(): number {
+    return round(Math.PI * this.radius * this.radius);
+  }
 }
 
-export function getInfo(figure) {
+export class Rectangle implements Figure {
+  shape: Shape = 'rectangle';
 
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('error');
+    }
+  }
+
+  getArea(): number {
+    return round(this.width * this.height);
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
