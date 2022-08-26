d95036f4da7fd9121a1ab84bc7a5a775c018f4c5
sergii-nosachenko <s.nosachenko@gmail.com>
2022-08-26 14:03:04 +0300

    Implemented Figure interface, BasicFigure class, Triangle class

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,61 @@
+type Shapes = 'triangle' | 'circle' | 'rectangle';
+type Colors = 'red' | 'green' | 'blue';
 export interface Figure {
+  shape: Shapes;
+  color: Colors;
+}
+class BaseFigure {
+  protected isOk: boolean = true;
 
+  constructor(
+    readonly shape: Shapes,
+    public color: Colors,
+  ) {
+    this.shape = shape;
+    this.color = color;
+  }
+
+  public checkLengths(...values: number[]): void {
+    this.isOk = values.every((value) => value > 0);
+
+    if (!this.isOk) {
+      throw new Error('Every figure length must be > 0!');
+    }
+  }
 }
 
-export class Triangle {
+export class Triangle extends BaseFigure {
+  readonly shape: Shapes = 'triangle';
 
-}
+  constructor(
+    public color: Colors,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    super('triangle', color);
+    this.checkLengths(a, b, c);
+    this.checkPossible();
+  }
 
-export class Circle {
+  private checkPossible(): void {
+    const longest: number = Math.max(this.a, this.b, this.c);
 
-}
+    this.isOk = this.a + this.b + this.c - longest > longest;
 
-export class Rectangle {
+    if (!this.isOk) {
+      throw new Error('Impossible triangle!');
+    }
+  }
 
-}
+  public getArea(): number {
+    const semiPerimeter: number = (this.a + this.b + this.c) / 2;
 
-export function getInfo(figure) {
-
+    return Math.sqrt(
+      semiPerimeter
+      * (semiPerimeter - this.a)
+      * (semiPerimeter - this.b)
+      * (semiPerimeter - this.c),
+    );
+  }
 }
