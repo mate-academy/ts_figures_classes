5e82f873bff3169b9244a0b01b9e2e7ebd89a436
Zinchenko Anastasiia <zincenkonasta6@gmail.com>
2022-08-31 16:02:24 +0300

    figures classes solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,88 @@
+type Color = 'red' | 'green' | 'blue';
+type Shape = 'triangle' | 'circle' | 'rectangle';
+
 export interface Figure {
-
+  shape: Shape;
+  color: Color;
+  getArea(): number;
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  shape: Shape = 'triangle';
 
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (this.a <= 0 || this.b <= 0 || this.c <= 0) {
+      throw new Error('length cant be 0 ');
+    }
+
+    if (this.a + this.b <= this.c
+      || this.c + this.a <= this.b
+      || this.b + this.c <= this.a) {
+      throw new Error(
+        'the longest side of a triangle is >= than a sum of two other',
+      );
+    }
+  }
+
+  getArea(): number {
+    const semiperimeter: number = (this.a + this.b + this.c) / 2;
+    const area: number = (
+      Math.sqrt(
+        semiperimeter
+        * (semiperimeter - this.a)
+        * (semiperimeter - this.b)
+        * (semiperimeter - this.c),
+      )
+    );
+
+    return Math.floor(area * 100) / 100;
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  shape: Shape = 'circle';
 
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (this.radius <= 0) {
+      throw new Error('length cant be 0 ');
+    }
+  }
+
+  getArea(): number {
+    const area: number = Math.PI * (this.radius ** 2);
+
+    return Math.floor(area * 100) / 100;
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  shape: Shape = 'rectangle';
 
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
+    if (this.width <= 0 || this.height <= 0) {
+      throw new Error('length cant be 0 ');
+    }
+  }
+
+  getArea(): number {
+    const area = this.width * this.height;
+
+    return area;
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
