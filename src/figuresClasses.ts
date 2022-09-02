6a9c253cd50ca44d036ebd015eadcc93ab590a96
Vladyslav Gusakovskiy <vladyslav.gusakovskiy@gmail.com>
2022-09-02 23:18:15 +0300

    Solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,85 @@
-export interface Figure {
 
+enum Shapes {
+  Triangle = 'triangle',
+  Circle = 'circle',
+  Rectangle = 'rectangle',
 }
 
-export class Triangle {
+enum Colors {
+  Red = 'red',
+  Green = 'green',
+  Blue = 'blue',
+}
 
+export interface Figure {
+  shape: Shapes;
+  color: Colors;
+  getArea(): number;
+}
+
+export class Triangle implements Figure {
+  public shape = Shapes.Triangle;
+
+  constructor(
+    public color: Colors,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (
+      this.a >= this.b + this.c
+      || this.b >= this.a + this.c
+      || this.c >= this.a + this.b
+    ) {
+      throw new Error('Error! Invalid result');
+    }
+  }
+
+  getArea(): number {
+    const perimeter = (this.a + this.b + this.c) / 2;
+
+    const sqrtValue = Math.sqrt(perimeter * (perimeter - this.a)
+    * (perimeter - this.b) * (perimeter - this.c));
+
+    return Number(sqrtValue.toFixed(2));
+  }
 }
 
 export class Circle {
+  public shape = Shapes.Circle;
 
+  constructor(
+    public color: Colors,
+    public radius: number,
+  ) {
+    if (this.radius <= 0) {
+      throw new Error('Error! Invalid result');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor(Math.PI * (this.radius ** 2) * 100) / 100;
+  }
 }
 
 export class Rectangle {
+  public shape = Shapes.Rectangle;
 
+  constructor(
+    public color: Colors,
+    public width: number,
+    public height: number,
+  ) {
+    if (this.width <= 0 || this.height <= 0) {
+      throw new Error('Error! Invalid result');
+    }
+  }
+
+  getArea(): number {
+    return this.width * this.height;
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
