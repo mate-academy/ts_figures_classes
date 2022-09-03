e18b4c9c9af3b0b8f9a1f62f00518258c73a82f3
Anton Liada <liadaantom@gmail.com>
2022-09-03 21:40:38 +0300

    "Figures" classes were implemented

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,96 @@
+const enum Shape {
+  Triangle = 'triangle',
+  Circle = 'circle',
+  Rectangle = 'rectangle',
+}
+
 export interface Figure {
-
+  shape: Shape;
+  color: string;
+  getArea(): number;
 }
 
-export class Triangle {
+type Color = 'red' | 'green' | 'blue';
 
+abstract class BaseFigure implements Figure {
+  constructor(
+    readonly shape: Shape,
+    public color: Color,
+  ) {
+    this.shape = shape;
+    this.color = color;
+  }
+
+  abstract getArea():number;
 }
 
-export class Circle {
-
+function checkValue(...args: number[]): void {
+  if (args.some((side) => side <= 0)) {
+    throw new Error('ERROR, value should be > 0');
+  }
 }
 
-export class Rectangle {
+export class Triangle extends BaseFigure {
+  constructor(
+    public color: Color,
+    public a = 0,
+    public b = 0,
+    public c = 0,
+  ) {
+    super(Shape.Triangle, color);
+    this.color = color;
+    checkValue(a, b, c);
 
+    if (a + b <= c || a + c <= b || b + c <= a) {
+      throw new Error('sides 1, 2 and 3 can\'t form a triangle');
+    }
+  }
+
+  getArea():number {
+    const semiPerimetr = (this.a + this.b + this.c) / 2;
+
+    const area = Math.sqrt(
+      semiPerimetr
+      * (semiPerimetr - this.a)
+      * (semiPerimetr - this.b)
+      * (semiPerimetr - this.c),
+    );
+
+    return Math.floor(area * 100) / 100;
+  }
 }
 
-export function getInfo(figure) {
+export class Circle extends BaseFigure {
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    super(Shape.Circle, color);
+    checkValue(this.radius);
+  }
 
+  getArea():number {
+    const area = Math.floor((Math.PI * this.radius ** 2) * 100) / 100;
+
+    return area;
+  }
+}
+
+export class Rectangle extends BaseFigure {
+  constructor(
+    public color: Color,
+    public witdh: number,
+    public hight: number,
+  ) {
+    super(Shape.Rectangle, color);
+    checkValue(this.witdh, this.hight);
+  }
+
+  getArea():number {
+    return this.witdh * this.hight;
+  }
+}
+
+export function getInfo(figure: Figure):string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
