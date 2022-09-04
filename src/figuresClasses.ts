a0e04a778fd615fa67b8c0fcbe98207e71907445
Adrian Faryniuk <forever0754@gmail.com>
2022-09-04 22:24:59 +0300

    solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,72 @@
 export interface Figure {
-
+  shape: 'triangle' | 'circle' | 'rectangle';
+  color: 'red' | 'green' | 'blue';
+  getArea: () => number;
 }
 
-export class Triangle {
-
+enum Shape {
+  Triangle = 'triangle',
+  Circle = 'circle',
+  Rectangle = 'rectangle',
 }
 
-export class Circle {
+export class Triangle implements Figure {
+  shape: Shape = Shape.Triangle;
 
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public a:number,
+    public b:number,
+    public c:number,
+  ) {
+    if (a >= b + c || b >= a + c || c >= a + b || a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('Error: can not create triangle with such data!!!');
+    }
+  }
+
+  getArea(): number {
+    const p: number = (this.a + this.b + this.c) / 2;
+
+    return +Math.sqrt(p * (p - this.a)
+     * (p - this.b) * (p - this.c)).toFixed(2);
+  }
 }
 
-export class Rectangle {
+export class Circle implements Figure {
+  shape: Shape = Shape.Circle;
 
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('Error: radius must be greater than 0!!!');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor(Math.PI * this.radius ** 2 * 100) / 100;
+  }
 }
 
-export function getInfo(figure) {
+export class Rectangle implements Figure {
+  shape: Shape = Shape.Rectangle;
 
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public width:number,
+    public height:number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('Error: width && height must be greater then 0!!!');
+    }
+  }
+
+  getArea(): number {
+    return (this.width * this.height);
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
