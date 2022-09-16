ba79dca6f0ce821c15203a7e3fbfcf6c5c940091
Serhii Vovk <gear.fq@gmail.com>
2022-09-16 10:45:00 +0300

    Solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,82 @@
+type Color = 'red' | 'green' | 'blue';
+type Shape = 'triangle' | 'circle' | 'rectangle';
+
 export interface Figure {
-
+  shape: Shape;
+  color: Color;
+  getArea: () => number,
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  shape: Shape = 'triangle';
 
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    const sides = [this.a, this.b, this.c];
+    const sortedSide = sides.sort((x, y) => y - x);
+
+    if (sides.some((x) => x < 0)) {
+      throw new Error('Side of the triangle must be > 0');
+    }
+
+    if (sortedSide[0] >= sortedSide[1] + sortedSide[2]) {
+      throw new Error('A side cannot be greater than the sum of the other two');
+    }
+  }
+
+  getArea(): number {
+    const semiPerimeter: number = (this.a + this.b + this.c) / 2;
+
+    const areaTriangle = Math.sqrt(semiPerimeter * (semiPerimeter - this.a)
+      * (semiPerimeter - this.b) * (semiPerimeter - this.c));
+
+    return Math.floor(areaTriangle * 100) / 100;
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  shape: Shape = 'circle';
 
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (this.radius <= 0) {
+      throw new Error('Radius must be > 0');
+    }
+  }
+
+  getArea(): number {
+    const areaCircle = Math.PI * (this.radius * this.radius);
+
+    return Math.floor(areaCircle * 100) / 100;
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  shape: Shape = 'rectangle';
 
+  constructor(
+    public color: Color,
+    public height: number,
+    public width: number,
+  ) {
+    if (this.height <= 0 || this.width <= 0) {
+      throw new Error('Height or width must be > 0');
+    }
+  }
+
+  getArea(): number {
+    const areaRectangle = this.height * this.width;
+
+    return Math.floor(areaRectangle * 100) / 100;
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
