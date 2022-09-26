0ef1cfdb80ee7c6371b7cb4837a27395fe4e43fe
Tilak Kumarclear <mailtotilakkumar@gmail.com>
2022-09-26 07:07:53 +0530

    solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,81 @@
+type Color = 'red' | 'green' | 'blue';
+
+function roundArea(area: number): number {
+  return Math.floor(area * 100) / 100;
+}
+
 export interface Figure {
-
+  shape: string;
+  color: Color;
+  getArea(): number;
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  shape = 'triangle';
 
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    const max = Math.max(a, b, c);
+    const sum = a + b + c;
+
+    if (a <= 0 || b <= 0 || c <= 0 || max >= sum - max) {
+      throw new Error('Invalid data');
+    }
+  }
+
+  getArea(): number {
+    const halfSum = (this.a + this.b + this.c) / 2;
+    const area = Math.sqrt(
+      halfSum * (halfSum - this.a) * (halfSum - this.b) * (halfSum - this.c),
+    );
+
+    return roundArea(area);
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  shape = 'circle';
 
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('Invalid data');
+    }
+  }
+
+  getArea(): number {
+    const area = Math.PI * (this.radius ** 2);
+
+    return roundArea(area);
+  }
 }
 
 export class Rectangle {
+  shape = 'rectangle';
 
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('Invalid data');
+    }
+  }
+
+  getArea(): number {
+    const area = this.width * this.height;
+
+    return roundArea(area);
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
