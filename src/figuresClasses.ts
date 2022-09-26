a680175e433e21d5ef098a48c38f209c6cec482d
Anastasiia Mahalias <anastasiabatychenko@gmail.com>
2022-09-26 20:47:49 +0300

    Solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,76 @@
+type Color = 'red' | 'green' | 'blue';
+
 export interface Figure {
-
+  shape: string;
+  color: Color;
+  getArea(): number;
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  shape = 'triangle';
 
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    const max = Math.max(a, b, c);
+    const sum = a + b + c;
+
+    if ((a <= 0 || b <= 0 || c <= 0)
+    || max >= sum - max) {
+      throw new Error('It can\'t be triangle');
+    }
+  }
+
+  getArea(): number {
+    const p = (this.a + this.b + this.c) / 2;
+    const s = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+
+    return Math.floor(s * 100) / 100;
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  shape = 'circle';
 
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('It can\'t be circle');
+    }
+  }
+
+  getArea(): number {
+    const s = Math.floor((Math.PI * this.radius ** 2) * 100) / 100;
+
+    return s;
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  shape = 'rectangle';
 
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('It can\'t be rectangle');
+    }
+  }
+
+  getArea(): number {
+    const s = this.width * this.height;
+
+    return Math.round(s * 100) / 100;
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
