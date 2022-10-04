f2e7512db517ab9c1912470c5c1b5075ee2d5c12
Artem Musii <musiy.artem@gmail.com>
2022-10-04 21:04:51 +0300

    solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,80 @@
-export interface Figure {
+type Color = 'red' | 'green' | 'blue';
+type Shape = 'triangle' | 'circle' | 'rectangle';
 
+export interface Figure {
+  shape: Shape;
+  color: Color;
+  getArea(): number;
 }
 
-export class Triangle {
+function roundArea(area: number): number {
+  return (Math.trunc(area * 100) / 100);
+}
 
+export class Triangle implements Figure {
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+    public shape: Shape = 'triangle',
+  ) {
+    if (a + b <= c || b + c <= a || c + a <= b) {
+      throw Error('Triangle does not exist');
+    }
+
+    if (a < 0 || b < 0 || c < 0) {
+      throw Error('Input data is not valid');
+    }
+  }
+
+  getArea(): number {
+    const { a, b, c } = this;
+
+    const p = (a + b + c) / 2;
+    const s = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+
+    return roundArea(s);
+  }
 }
 
 export class Circle {
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public radius: number,
+    public shape: Shape = 'circle',
+  ) {
+    if (radius < 0) {
+      throw Error('Input data is not valid');
+    }
+  }
 
+  getArea(): number {
+    const s = Math.PI * (this.radius ** 2);
+
+    return roundArea(s);
+  }
 }
 
 export class Rectangle {
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public a: number,
+    public b: number,
+    public shape: Shape = 'rectangle',
+  ) {
+    if (a < 0 || b < 0) {
+      throw Error('Input data is not valid');
+    }
+  }
 
+  getArea(): number {
+    const s = this.a * this.b;
+
+    return roundArea(s);
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
