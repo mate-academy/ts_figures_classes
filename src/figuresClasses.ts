075ae9768d892c802ab1b9c8159332bb81930e78
Myroslav Diiak <diiakm23819@gmail.com>
2022-10-04 22:37:41 +0300

    solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,80 @@
+type Shape = 'triangle' | 'circle' | 'rectangle';
 export interface Figure {
-
+  shape: Shape;
+  color: string;
+  a: number;
+  b?: number;
+  c?: number;
+  getArea() : number;
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  shape: Shape = 'triangle';
 
+  constructor(
+    public color: string,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw Error('nope');
+    }
+
+    if (a + b <= c || a + c <= b || c + b <= a) {
+      throw Error('nope');
+    }
+  }
+
+  getArea(): number {
+    const perimetr: number = (this.a + this.b + this.c) / 2;
+
+    const area: number = Math.sqrt(perimetr
+      * (perimetr - this.a)
+      * (perimetr - this.b)
+      * (perimetr - this.c));
+
+    return Math.floor(area * 100) / 100;
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  shape: Shape = 'circle';
 
+  constructor(
+    public color: string,
+    public a: number,
+  ) {
+    if (a <= 0) {
+      throw Error('nope');
+    }
+  }
+
+  getArea(): number {
+    const area: number = Math.PI * this.a * this.a;
+
+    return Math.floor(area * 100) / 100;
+  }
 }
 
 export class Rectangle {
+  shape: Shape = 'rectangle';
 
+  constructor(
+    public color: string,
+    public a: number,
+    public b: number,
+  ) {
+    if (a <= 0 || b <= 0) {
+      throw Error('nope');
+    }
+  }
+
+  getArea(): number {
+    return this.a * this.b;
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
