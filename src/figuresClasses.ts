e05c663c819767c7a821b475ec5704027bb569fe
Oleh Bazyla <olegbazila@gmail.com>
2022-10-07 16:14:47 +0300

    fix

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,5 +1,10 @@
-type Shape = 'triangle' | 'circle' | 'rectangle';
 type Color = 'red' | 'green' | 'blue';
+
+enum Shape {
+  Triangle = 'triangle',
+  Circle = 'circle',
+  Rectangle = 'rectangle',
+}
 
 export interface Figure {
   shape: Shape;
@@ -7,8 +12,18 @@ export interface Figure {
   getArea(): number
 }
 
+const isValid = (...items: number[]): void => {
+  if (items.some((num) => num <= 0)) {
+    throw new Error('Params should be more than 0');
+  }
+};
+
+const rounder = (num: number): number => {
+  return Math.floor(num * 100) / 100;
+};
+
 export class Triangle implements Figure {
-  shape: Shape = 'triangle';
+  shape = Shape.Triangle;
 
   constructor(
     public color: Color,
@@ -16,65 +31,55 @@ export class Triangle implements Figure {
     public b: number,
     public c: number,
   ) {
-    if (this.a <= 0 || this.b <= 0 || this.c <= 0) {
-      throw new Error('Triangle sides should be more than 0');
-    }
+    isValid(a, b, c);
 
     const maxSide: number = Math.max(a, b, c);
-    const perimeter: number = a + b + c;
 
-    if (maxSide >= perimeter - maxSide) {
+    if (maxSide >= a + b + c - maxSide) {
       throw new Error('Wrong sides for a triangle');
     }
   }
 
   getArea(): number {
-    const semiPerimeter: number = (this.a + this.b + this.c) / 2;
+    const { a, b, c } = this;
+    const semiPerimeter: number = (a + b + c) / 2;
 
-    const area = Math.sqrt(
-      semiPerimeter * (semiPerimeter - this.a)
-      * (semiPerimeter - this.b)
-      * (semiPerimeter - this.c),
-    );
-
-    return Math.round(area * 100) / 100;
+    return rounder(Math.sqrt(
+      semiPerimeter * (semiPerimeter - a)
+      * (semiPerimeter - b)
+      * (semiPerimeter - c),
+    ));
   }
 }
 
 export class Circle implements Figure {
-  shape: Shape = 'circle';
+  shape = Shape.Circle;
 
   constructor(
     public color: Color,
     public radius: number,
   ) {
-    if (radius <= 0) {
-      throw new Error('Triangle radius should be more than 0');
-    }
+    isValid(radius);
   }
 
   getArea(): number {
-    const area = Math.PI * this.radius ** 2;
-
-    return Math.floor(area * 100) / 100;
+    return rounder(Math.PI * this.radius ** 2);
   }
 }
 
 export class Rectangle implements Figure {
-  shape: Shape = 'rectangle';
+  shape = Shape.Rectangle;
 
   constructor(
     public color: Color,
     public width: number,
     public height: number,
   ) {
-    if (width <= 0 || height <= 0) {
-      throw new Error('Triangle radius should be more than 0');
-    }
+    isValid(width, height);
   }
 
   getArea(): number {
-    return this.width * this.height;
+    return rounder(this.width * this.height);
   }
 }
 
