e101611a74c614151f28927cf35caa3cf429a9d6
dmitriyn3679 <dmitriyn3679@gmail.com>
2022-10-14 09:59:06 +0300

    fixed

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,17 +1,29 @@
+enum FigureShape {
+  triangle,
+  circle,
+  rectangle
+}
+
+enum FigureColor {
+  red,
+  green,
+  blue,
+}
+
 export interface Figure {
-  shape: string;
-  color: string;
+  shape: FigureShape;
+  color: FigureColor;
   getArea(): void;
 }
 
-function checkFigure(figureType: string, ...sides: number[]): void {
+function checkFigure(figureType: FigureShape, ...sides: number[]): void {
   sides.forEach((side: number) => {
     if (side <= 0) {
       throw new Error('Any side cant be less 0');
     }
   });
 
-  if (figureType === 'triangle') {
+  if (figureType === FigureShape.triangle) {
     const longestSide = Math.max.apply(null, sides);
     const longestSideIndex = sides.indexOf(longestSide);
     const sumTwoOthersSides = sides
@@ -36,10 +48,10 @@ function roundedArea(area: number): number {
 }
 
 export class Triangle implements Figure {
-  shape = 'triangle';
+  shape = FigureShape.triangle;
 
   constructor(
-    public color: string,
+    public color: FigureColor,
     public a: number,
     public b: number,
     public c: number,
@@ -57,11 +69,11 @@ export class Triangle implements Figure {
   }
 }
 
-export class Circle {
-  shape = 'circle';
+export class Circle implements Figure {
+  shape = FigureShape.circle;
 
   constructor(
-    public color: string,
+    public color: FigureColor,
     public radius: number,
   ) {
     checkFigure(this.shape, radius);
@@ -74,11 +86,11 @@ export class Circle {
   }
 }
 
-export class Rectangle {
-  shape = 'rectangle';
+export class Rectangle implements Figure {
+  shape = FigureShape.rectangle;
 
   constructor(
-    public color: string,
+    public color: FigureColor,
     public width: number,
     public height: number,
   ) {
