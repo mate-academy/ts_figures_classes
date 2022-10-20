962ab063c40ef0396cce22f57e0a1e9a45084832
Kostya Berehoviy <kostyaber3@gmail.com>
2022-10-20 22:49:40 +0300

    Figures classes solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,72 @@
 export interface Figure {
-
+  shape: 'triangle' | 'rectangle' | 'circle',
+  color: 'red' | 'green' | 'blue',
+  getArea(): number,
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public a: number,
+    public b: number,
+    public c: number,
+    public shape: 'triangle' | 'rectangle' | 'circle'
+    = 'triangle',
+  ) {
+    if (!a || !b || !c || a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('Please, enter valid data');
+    }
 
+    if (c >= a + b) {
+      throw new Error('Please, enter valid data');
+    }
+  }
+
+  getArea(): number {
+    const s = (this.a + this.b + this.c) / 2;
+    const area = s * (s - this.a) * (s - this.b) * (s - this.c);
+
+    return Math.trunc(Math.sqrt(area) * 100) / 100;
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public radius: number,
+    public shape: 'triangle' | 'rectangle' | 'circle'
+    = 'circle',
+  ) {
+    if (!radius || radius <= 0) {
+      throw new Error('Please, enter valid data');
+    }
+  }
 
+  getArea(): number {
+    const area = Math.PI * this.radius * this.radius;
+
+    return Math.trunc(area * 100) / 100;
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public width: number,
+    public height: number,
+    public shape: 'triangle' | 'rectangle' | 'circle'
+    = 'rectangle',
+  ) {
+    if (!width || !height || width <= 0 || height <= 0) {
+      throw new Error('Please, enter valid data');
+    }
+  }
 
+  getArea(): number {
+    return this.width * this.height;
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Triangle | Circle | Rectangle): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
