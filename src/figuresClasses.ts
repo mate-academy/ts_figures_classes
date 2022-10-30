61df2efa324a1b243cac7aca693038bd54d69a12
Bibicutu <pauleugen@gmail.com>
2022-10-31 01:05:54 +0200

    Requested changes made

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -10,25 +10,13 @@ export interface Figure {
 export class Triangle implements Figure {
   shape: Shape;
 
-  color: Color;
-
-  sideA: number;
-
-  sideB: number;
-
-  sideC: number;
-
   constructor(
-    color: Color,
-    sideA: number,
-    sideB: number,
-    sideC: number,
+    public color: Color,
+    public sideA: number,
+    public sideB: number,
+    public sideC: number,
   ) {
     this.shape = 'triangle';
-    this.color = color;
-    this.sideA = sideA;
-    this.sideB = sideB;
-    this.sideC = sideC;
 
     if (!(this.sideA > 0 && this.sideB > 0 && this.sideC > 0)) {
       throw new Error('Sides of a triangle must be positive numbers!');
@@ -57,17 +45,11 @@ export class Triangle implements Figure {
 export class Circle implements Figure {
   shape: Shape;
 
-  color: Color;
-
-  radius: number;
-
   constructor(
-    color: Color,
-    radius: number,
+    public color: Color,
+    public radius: number,
   ) {
     this.shape = 'circle';
-    this.color = color;
-    this.radius = radius;
 
     if (this.radius <= 0) {
       throw new Error('The radius must be a positive number!');
@@ -82,21 +64,12 @@ export class Circle implements Figure {
 export class Rectangle implements Figure {
   shape: Shape;
 
-  color: Color;
-
-  width: number;
-
-  height: number;
-
   constructor(
-    color: Color,
-    width: number,
-    height: number,
+    public color: Color,
+    public width: number,
+    public height: number,
   ) {
     this.shape = 'rectangle';
-    this.color = color;
-    this.width = width;
-    this.height = height;
 
     if (this.width <= 0 || this.height <= 0) {
       throw new Error('The both sides must be positive numbers!');
