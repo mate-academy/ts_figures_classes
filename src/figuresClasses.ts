c2e49070c1958de326caff34e570351b51b00ab7
metin <metinbicaksiz@gmail.com>
2022-11-08 08:25:10 +0300

    solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -19,12 +19,12 @@ export class Triangle implements Figure {
 
   public shape: string;
 
-  constructor(color: string, a: number, b: number, c: number, shape: string) {
+  constructor(color: string, a: number, b: number, c: number) {
     this.color = color;
     this.a = a;
     this.b = b;
     this.c = c;
-    this.shape = shape;
+    this.shape = this.constructor.name.toLowerCase();
 
     if (this.a <= 0 || this.b <= 0 || this.c <= 0) {
       throw new Error('Negative sides!');
@@ -36,10 +36,11 @@ export class Triangle implements Figure {
   }
 
   getArea(): number {
-    const size = (this.a + this.b + this.c) / 2;
+    const { a, b, c } = this;
+    const size = (a + b + c) / 2;
 
     return Math.floor((Math.sqrt(size
-      * (size - this.a) * (size - this.b) * (size - this.c))) * 100) / 100;
+      * (size - a) * (size - b) * (size - c))) * 100) / 100;
   }
 }
 
@@ -50,8 +51,8 @@ export class Circle implements Figure {
 
   radius: number;
 
-  constructor(color: string, radius: number, shape: string) {
-    this.shape = shape;
+  constructor(color: string, radius: number) {
+    this.shape = this.constructor.name.toLowerCase();
     this.color = color;
     this.radius = radius;
 
@@ -76,11 +77,11 @@ export class Rectangle implements Figure {
 
   height: number;
 
-  constructor(color: string, width: number, height: number, shape: string) {
+  constructor(color: string, width: number, height: number) {
     this.color = color;
     this.width = width;
     this.height = height;
-    this.shape = shape;
+    this.shape = this.constructor.name.toLowerCase();
 
     if (this.height <= 0 || this.width <= 0) {
       throw new Error('Negative Sides!');
@@ -95,17 +96,5 @@ export class Rectangle implements Figure {
 }
 
 export function getInfo(figure: Figure): string {
-  if (figure instanceof Circle) {
-    return `A ${figure.color} circle - ${figure.getArea()}`;
-  }
-
-  if (figure instanceof Rectangle) {
-    return `A ${figure.color} rectangle - ${figure.getArea()}`;
-  }
-
-  if (figure instanceof Triangle) {
-    return `A ${figure.color} triangle - ${figure.getArea()}`;
-  }
-
-  return `A ${figure.color} triangle - ${figure.getArea()}`;
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
