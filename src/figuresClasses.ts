bc7a599841c1e4e73901a6c0cce1b50d16d9feb9
Andriy Shymkiv <winamondezik@gmail.com>
2022-11-11 15:41:44 +0200

    Solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,81 @@
-export interface Figure {
 
+type FigureShape = 'triangle' | 'circle' | 'rectangle';
+type FigureColor = 'red' | 'green' | 'blue';
+
+export interface Figure {
+  shape: FigureShape;
+  color: FigureColor;
+  getArea(): number;
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  public shape: FigureShape = 'triangle';
 
+  constructor(
+    public color: FigureColor,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('wrong input for Triangle');
+    }
+
+    if (a + b <= c || a + c <= b || b + c <= a) {
+      throw new Error('wrong input for Triangle');
+    }
+  }
+
+  getArea(): number {
+    const perimeter: number = (this.a + this.b + this.c) / 2;
+    const triangleSquare = Math.sqrt(
+      perimeter * (perimeter - this.a)
+      * (perimeter - this.b) * (perimeter - this.c),
+    );
+
+    return Math.floor(triangleSquare * 100) / 100;
+  }
 }
 
 export class Circle {
+  public shape: FigureShape = 'circle';
 
+  constructor(
+    public color: FigureColor,
+    public radius: number,
+  ) {
+    if (this.radius < 0) {
+      throw new Error('wrong inputfor Circle');
+    }
+  }
+
+  getArea(): number {
+    const circleSquare = Math.PI * this.radius ** 2;
+
+    return Math.floor(100 * circleSquare) / 100;
+  }
 }
 
 export class Rectangle {
+  public shape: FigureShape = 'rectangle';
 
+  constructor(
+    public color: FigureColor,
+    public width: number,
+    public height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('wrong input for Rectangle');
+    }
+  }
+
+  getArea(): number {
+    const rectangleArea: number = this.width * this.height;
+
+    return Math.floor(rectangleArea * 100) / 100;
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
