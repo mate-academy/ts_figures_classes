daf24f40fd572aca28478c9a606e6685cf05a4a9
Andrii Bassarab <andrijbassarab@gmail.com>
2022-11-14 16:58:45 +0200

    fix solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -7,7 +7,13 @@ export interface Figure {
   getArea: Function,
 }
 
+function roundToHundreths(number: number): number {
+  return Math.floor(number * 100) / 100;
+}
+
 export class Triangle implements Figure {
+  shape: Shape;
+
   constructor(
     public color: Color,
     public a: number,
@@ -16,8 +22,8 @@ export class Triangle implements Figure {
   ) {
     this.shape = 'triangle';
 
-    const biggestSide = Math.max(this.a, this.b, this.c);
-    const sumOfSmallerSides = [this.a, this.b, this.c]
+    const biggestSide = Math.max(a, b, c);
+    const sumOfSmallerSides = [a, b, c]
       .reduce((sum, curr) => sum + curr) - biggestSide;
 
     if (biggestSide >= sumOfSmallerSides) {
@@ -25,44 +31,48 @@ export class Triangle implements Figure {
       + 'than the sum of the other two');
     }
 
-    if (this.a < 0 || this.b < 0 || this.c < 0
-      || biggestSide >= sumOfSmallerSides) {
+    if (a < 0 || b < 0 || c < 0) {
       throw new Error('Sides of the triangle must be bigger than 0');
     }
   }
 
-  shape: Shape;
-
   getArea(): number {
-    const avergeSide = (this.a + this.b + this.c) / 2;
+    const { a, b, c } = this;
 
-    return Math.floor(Math.sqrt(
-      avergeSide * (avergeSide - this.a)
-      * (avergeSide - this.b) * (avergeSide - this.c),
-    ) * 100) / 100;
+    const avergeSide = (a + b + c) / 2;
+
+    const area = Math.sqrt(
+      avergeSide * (avergeSide - a) * (avergeSide - b) * (avergeSide - c),
+    );
+
+    return roundToHundreths(area);
   }
 }
 
 export class Circle implements Figure {
+  shape: Shape;
+
   constructor(
     public color: Color,
     public radius: number,
   ) {
     this.shape = 'circle';
 
-    if (this.radius < 0) {
+    if (radius < 0) {
       throw new Error('Radius of the Circle must be bigger than 0');
     }
   }
 
-  shape: Shape;
-
   getArea(): number {
-    return Math.floor(Math.PI * this.radius ** 2 * 100) / 100;
+    const area: number = Math.PI * this.radius ** 2;
+
+    return roundToHundreths(area);
   }
 }
 
 export class Rectangle implements Figure {
+  shape: Shape;
+
   constructor(
     public color: Color,
     public width: number,
@@ -70,15 +80,15 @@ export class Rectangle implements Figure {
   ) {
     this.shape = 'rectangle';
 
-    if (this.width < 0 || this.height < 0) {
+    if (width < 0 || height < 0) {
       throw new Error('Sides of the rectangle must be bigger than 0');
     }
   }
 
-  shape: Shape;
-
   getArea(): number {
-    return Math.floor(this.width * this.height * 100) / 100;
+    const area: number = this.width * this.height;
+
+    return roundToHundreths(area);
   }
 }
 
