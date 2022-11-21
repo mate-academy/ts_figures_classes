db87bbf8690db8728882159b075dcc38b5f48ea7
Vadym Baranov <vadym.baranov@hotmail.com>
2022-11-21 13:32:53 +0200

    Added destructuring to triangle calculations

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -36,12 +36,13 @@ export class Triangle implements Figure {
   }
 
   getArea(): number {
-    const s: number = (this.a + this.b + this.c) / 2;
+    const { a, b, c } = this;
+    const s: number = (a + b + c) / 2;
     const area: number = Math.sqrt(
       s * (
-        (s - this.a)
-      * (s - this.b)
-      * (s - this.c)
+        (s - a)
+      * (s - b)
+      * (s - c)
       ),
     );
 
