9b396030617d14011d9e66602190ec13dc91950c
Oleksandr Lytvynenko <olek.lytvynenko@gmail.com>
2022-11-28 14:17:23 +0200

    add function

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -7,6 +7,18 @@ export interface Figure {
   getArea(): number,
 }
 
+function rounding(expr: number): number {
+  return Math.floor(expr * 100) / 100;
+}
+
+function validation(check: boolean, message: string): Error | void {
+  if (check) {
+    throw new Error(
+      message,
+    );
+  }
+}
+
 export class Triangle implements Figure {
   shape: Shape = 'triangle';
 
@@ -16,30 +28,26 @@ export class Triangle implements Figure {
     public b: number,
     public c: number,
   ) {
-    if (a <= 0 || b <= 0 || c <= 0) {
-      throw new Error(
-        'A side of a triangle cannot be equal to 0 or be negative.',
-      );
-    }
-
+    const checkNegative:boolean = a <= 0 || b <= 0 || c <= 0;
     const maxSide: number = Math.max(a, b, c);
+    const checkLongestSide: boolean = maxSide >= (a + b + c) - maxSide;
+    const messageNegative: string = 'A side of a triangle cannot be equal '
+    + 'to 0 or be negative.';
+    const messageLongest: string = 'the longest side of the triangle must '
+    + 'be greater than the sum of the other two';
 
-    if (maxSide >= (a + b + c) - maxSide) {
-      throw new Error(
-        'the longest side of the triangle must'
-         + 'be greater than the sum of the other two',
-      );
-    }
+    validation(checkNegative, messageNegative);
+    validation(checkLongestSide, messageLongest);
   }
 
   getArea(): number {
     const semiP: number = (this.a + this.b + this.c) / 2;
 
-    return Math.floor(
+    return rounding(
       Math.sqrt(
         semiP * (semiP - this.a) * (semiP - this.b) * (semiP - this.c),
-      ) * 100,
-    ) / 100;
+      ),
+    );
   }
 }
 
@@ -50,15 +58,14 @@ export class Circle implements Figure {
     public color: Color,
     public radius: number,
   ) {
-    if (radius <= 0) {
-      throw new Error(
-        'The radius of the circle must not be zero or negative',
-      );
-    }
+    const checkNegative: boolean = radius <= 0;
+    const message = 'The radius of the circle must not be zero or negative';
+
+    validation(checkNegative, message);
   }
 
   getArea(): number {
-    return Math.floor(Math.PI * (this.radius ** 2) * 100) / 100;
+    return rounding(Math.PI * (this.radius ** 2));
   }
 }
 
@@ -70,15 +77,15 @@ export class Rectangle implements Figure {
     public width: number,
     public height: number,
   ) {
-    if (width <= 0 || height <= 0) {
-      throw new Error(
-        'A side of a rectangle cannot be equal to 0 or be negative.',
-      );
-    }
+    const checkNegative:boolean = width <= 0 || height <= 0;
+    const message = 'A side of a rectangle cannot be equal'
+    + 'to 0 or be negative.';
+
+    validation(checkNegative, message);
   }
 
   getArea(): number {
-    return Math.floor(this.width * this.height * 100) / 100;
+    return rounding(this.width * this.height);
   }
 }
 
