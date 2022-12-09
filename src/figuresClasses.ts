c424df9a654d58fb08f4ec5076e846282f7c6893
ysrckr <dyt.yasarcakir@gmail.com>
2022-12-09 20:16:41 +0300

    constructer are created

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,55 @@
 export interface Figure {
-
+  shape: 'triangle' | 'circle' | 'rectangle'
+  color: 'red' | 'green' | 'blue'
+  getArea(): number
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  shape = 'triangle';
 
+  constructor(
+    public color,
+    public a: number,
+    public b: number,
+    public c: number,
+
+  ) {
+    if (arguments.length <= 0) {
+      throw new Error('argument(s) missing');
+    }
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  shape = 'circle';
 
+  constructor(
+    public color,
+    public radius: number,
+
+  ) {
+    if (arguments.length <= 0) {
+      throw new Error('argument(s) missing');
+    }
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  shape = 'rectangle';
 
+  constructor(
+    public color,
+    public width: number,
+    public height: number,
+
+  ) {
+    if (arguments.length <= 0) {
+      throw new Error('argument(s) missing');
+    }
+  }
 }
 
-export function getInfo(figure) {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function getInfo(figure): string {
 
 }
