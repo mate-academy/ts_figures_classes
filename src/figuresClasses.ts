3047f5a6e6409ee824b192f4ffc2631f7d174648
Kalmykov Mikhail <kalmykov.mikhail2@gmail.com>
2022-12-15 18:26:15 +0200

    task was completed

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,73 @@
+type Shape = 'triangle' | 'circle' | 'rectangle';
+type Color = 'red' | 'green' | 'blue';
+
 export interface Figure {
-
+  shape: Shape;
+  color: Color;
+  getArea(): number;
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  shape: Shape = 'triangle';
 
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0 || a + b <= c || a + c <= b || c + b <= a) {
+      throw new Error('your error message');
+    }
+  }
+
+  getArea():number {
+    const { a, b, c } = this;
+    const p = 0.5 * (a + b + c);
+
+    return +Math.sqrt(p * (p - a) * (p - b) * (p - c)).toFixed(2);
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  shape: Shape = 'circle';
 
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('your error message');
+    }
+  }
+
+  getArea(): number {
+    const { radius } = this;
+
+    return Math.floor(Math.PI * radius * radius * 100) / 100;
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  shape: Shape = 'rectangle';
 
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('your error message');
+    }
+  }
+
+  getArea():number {
+    const { width, height } = this;
+
+    return width * height;
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
