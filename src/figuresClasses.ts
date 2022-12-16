b6bd14b225b1b182b1325579e96989da4e61929d
Lefteris Loizides <loizides.lef@gmail.com>
2022-12-16 23:40:08 +0200

    solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,74 @@
+export type Shape = 'circle' | 'triangle' | 'rectangle';
+export type Color = 'red' | 'green' | 'blue';
+
 export interface Figure {
-
+  shape: Shape;
+  color: Color;
+  getArea: () => number;
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  shape: Shape = 'triangle';
 
+  getArea: () => number;
+
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    const hasInvalidSide: boolean = !((a && b && c) > 0);
+    const isNotTriangle: boolean = a >= b + c || b >= a + c || c >= a + b;
+
+    if (hasInvalidSide) {
+      throw new Error('All sides must be positive numbers');
+    }
+
+    if (isNotTriangle) {
+      throw new Error(`Sides ${a}, ${b} and ${c} can't form a triangle`);
+    }
+
+    this.getArea = (): number => {
+      const s = (a + b + c) / 2;
+      const heronsFormula = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+
+      return Math.floor(heronsFormula * 100) / 100;
+    };
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  shape: Shape = 'circle';
 
+  getArea = (): number => Math.floor(Math.PI * this.radius ** 2 * 100) / 100;
+
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('Radius must be a positive number');
+    }
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  shape: Shape = 'rectangle';
 
+  getArea = (): number => Math.floor(this.width * this.height * 100) / 100;
+
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('Width and height must be positive numbers');
+    }
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure):string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
