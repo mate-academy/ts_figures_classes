c7d72bb68b3bb71574d5aeffbb62e47d8140fad7
JzSol <111354281+JzSol@users.noreply.github.com>
2022-12-21 18:32:21 +0200

    Finished

---
+++ src/figuresClasses.js
@@ -0,0 +1,24 @@
+"use strict";
+exports.__esModule = true;
+exports.getInfo = exports.Rectangle = exports.Circle = exports.Triangle = void 0;
+var Triangle = /** @class */ (function () {
+    function Triangle() {
+    }
+    return Triangle;
+}());
+exports.Triangle = Triangle;
+var Circle = /** @class */ (function () {
+    function Circle() {
+    }
+    return Circle;
+}());
+exports.Circle = Circle;
+var Rectangle = /** @class */ (function () {
+    function Rectangle() {
+    }
+    return Rectangle;
+}());
+exports.Rectangle = Rectangle;
+function getInfo(figure) {
+}
+exports.getInfo = getInfo;
