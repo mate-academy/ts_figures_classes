3f6d301dfedd88fb5f861003492892ceacae4735
Elias Montauti <egsm147@gmail.com>
2022-12-21 17:18:48 +0000

    Solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,80 @@
+type Color = 'red' | 'green' | 'blue';
+type Shape = 'triangle' | 'circle' | 'rectangle';
+
 export interface Figure {
-
+  shape: Shape;
+  color: Color;
+  getArea():number;
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  shape: Shape;
 
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    this.shape = 'triangle';
+
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('it is a trap!!');
+    }
+
+    if ((a >= b + c) || (c >= a + b) || (b >= a + c)) {
+      throw new Error('it is a trap!!');
+    }
+  }
+
+  getArea():number {
+    const spm = (this.a + this.b + this.c) / 2;
+
+    return Math.floor(Math.sqrt(
+      spm * (spm - this.a) * (spm - this.b) * (spm - this.c),
+    ) * 100) / 100;
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  shape: Shape;
 
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    this.shape = 'circle';
+
+    if (radius <= 0) {
+      throw new Error('it is a trap!!');
+    }
+  }
+
+  getArea():number {
+    return Math.floor(Math.PI * this.radius ** 2 * 100) / 100;
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  shape: Shape;
 
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
+    this.shape = 'rectangle';
+
+    if (width <= 0 || height <= 0) {
+      throw new Error('your error message');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor(this.width * this.height * 100) / 100;
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure:Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
