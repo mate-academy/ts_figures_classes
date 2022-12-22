ac531905c0628f33f5648834d4d6a738618d2e99
Ilija Pejanovic <ikadvm96@gmail.com>
2022-12-22 11:23:18 +0100

    added suggested changes

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -13,12 +13,13 @@ export enum Color {
 export interface Figure {
   shape: Shape,
   color: Color,
-
-  getArea(): number,
+  getArea: () => number,
 }
 
 export class Triangle implements Figure {
-  shape: Shape;
+  shape = Shape.Triangle;
+
+  getArea: () => number;
 
   constructor(
     public color: Color,
@@ -34,19 +35,19 @@ export class Triangle implements Figure {
       throw new Error('It\'s not a triangle');
     }
 
-    this.shape = Shape.Triangle;
-  }
+    this.getArea = (): number => {
+      const semiPerimeter = (this.a + this.b + this.c) / 2;
 
-  getArea(): number {
-    const semiPerimeter = (this.a + this.b + this.c) / 2;
-
-    return Math.round(Math.sqrt(semiPerimeter * (semiPerimeter - this.a)
+      return Math.round(Math.sqrt(semiPerimeter * (semiPerimeter - this.a)
       * (semiPerimeter - this.b) * (semiPerimeter - this.c)) * 100) / 100;
+    };
   }
 }
 
 export class Circle implements Figure {
-  shape: Shape;
+  shape = Shape.Circle;
+
+  getArea = (): number => Math.floor(Math.PI * this.radius ** 2 * 100) / 100;
 
   constructor(
     public color: Color,
@@ -55,17 +56,13 @@ export class Circle implements Figure {
     if (radius <= 0) {
       throw new Error('Radius is less or equal 0!');
     }
-
-    this.shape = Shape.Circle;
-  }
-
-  getArea(): number {
-    return Math.floor(Math.PI * this.radius * this.radius * 100) / 100;
   }
 }
 
 export class Rectangle implements Figure {
-  shape: Shape;
+  shape = Shape.Rectangle;
+
+  getArea = (): number => Math.floor(this.width * this.height * 100) / 100;
 
   constructor(
     public color: Color,
@@ -75,12 +72,6 @@ export class Rectangle implements Figure {
     if (width <= 0 || height <= 0) {
       throw new Error('One of the sides is less or equal 0!');
     }
-
-    this.shape = Shape.Rectangle;
-  }
-
-  getArea(): number {
-    return +Math.floor(this.width * this.height * 100) / 100;
   }
 }
 
