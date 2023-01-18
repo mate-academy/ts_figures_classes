1abbc50b91f45d9ee133e8dd5fcad2f44ed8a78a
oleh <shankar.oleh@gmail.com>
2023-01-18 18:02:35 +0200

    added Shape type, moved method getArea to class

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,15 +1,14 @@
 // import { NumericLiteral } from '@babel/types';
+export type Shape = 'triangle' | 'circle' | 'rectangle';
 
 export interface Figure {
-  shape: string;
+  shape: Shape;
   color: string;
   getArea: () => number;
 }
 
 export class Triangle implements Figure {
-  getArea: () => number;
-
-  shape = 'triangle';
+  shape: Shape = 'triangle';
 
   constructor(
     public color: string,
@@ -26,20 +25,18 @@ export class Triangle implements Figure {
     if (a >= perimeter - a || b >= perimeter - b || c >= perimeter - c) {
       throw new Error(`sides ${a}, ${b} and ${c} can't form a triangle`);
     }
-
-    this.getArea = (): number => {
-      const s = perimeter / 2;
-      const area = Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
-
-      return Math.round(area * 100) / 100;
-    };
   }
+
+  getArea = (): number => {
+    const s = (this.a + this.b + this.c) / 2;
+    const area = Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+
+    return Math.round(area * 100) / 100;
+  };
 }
 
 export class Circle implements Figure {
-  getArea: () => number;
-
-  shape = 'circle';
+  shape : Shape = 'circle';
 
   constructor(
     public color: string,
@@ -48,19 +45,17 @@ export class Circle implements Figure {
     if (radius <= 0) {
       throw new Error('length cannot be less than or equal to zero');
     }
-
-    this.getArea = (): number => {
-      const area = Math.PI * (this.radius ** 2);
-
-      return Math.floor(area * 100) / 100;
-    };
   }
+
+  getArea = (): number => {
+    const area = Math.PI * (this.radius ** 2);
+
+    return Math.floor(area * 100) / 100;
+  };
 }
 
 export class Rectangle implements Figure {
-  getArea: () => number;
-
-  shape = 'rectangle';
+  shape: Shape = 'rectangle';
 
   constructor(
     public color: string,
@@ -70,13 +65,13 @@ export class Rectangle implements Figure {
     if (width <= 0 || height <= 0) {
       throw new Error('length cannot be less than or equal to zero');
     }
-
-    this.getArea = (): number => {
-      const area = this.width * this.height;
-
-      return Math.round(area * 100) / 100;
-    };
   }
+
+  getArea = (): number => {
+    const area = this.width * this.height;
+
+    return Math.round(area * 100) / 100;
+  };
 }
 
 export function getInfo(figure: Figure): string {
