11ce5a534a4a4aa576c14ca8e4131ddb8b54eeab
maasiimka <mrozisalovec@gmail.com>
2023-01-18 14:41:02 +0200

    Solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,75 @@
 export interface Figure {
-
+  shape: string;
+  color: string;
+  getArea():number
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  shape = 'triangle';
 
+  constructor(
+    public color: string,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('One of sides is 0 or less. Such figure is impossible');
+    }
+
+    if (a >= b + c || b >= a + c || c >= b + a) {
+      throw new Error('One side is bigger than sum of others.It is impossible');
+    }
+  }
+
+  getArea(): number {
+    const { a, b, c } = this;
+
+    const p = (a + b + c) / 2;
+
+    return Math.floor(Math.sqrt(p * (p - a) * (p - b) * (p - c)) * 100) / 100;
+  }
 }
 
 export class Circle {
+  shape = 'circle';
 
+  constructor(
+    public color: string,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('Radius is 0 or less. Such figure is impossible');
+    }
+  }
+
+  getArea(): number {
+    const { radius } = this;
+
+    return Math.floor(Math.PI * radius * radius * 100) / 100;
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  shape = 'rectangle';
 
+  constructor(
+    public color: string,
+    public width: number,
+    public height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('One of sides is 0 or less. Such figure is impossible');
+    }
+  }
+
+  getArea(): number {
+    const { width, height } = this;
+
+    return width * height;
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
