90336e7b8137c31e70e96b215dec6a0f2e9f96ed
Yaroslvna Horban <domkomp2709@gmail.com>
2023-01-19 18:02:14 +0100

    solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,80 @@
+type Shape = ('triangle' | 'circle' | 'rectangle');
+type Color = ('red' | 'green' | 'blue');
 export interface Figure {
+  shape: Shape;
+  color: Color;
 
+  getArea(): number;
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  shape: Shape = 'triangle';
 
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (this.a <= 0 && this.b <= 0 && this.c <= 0) {
+      throw new Error('Size must be greater than 0');
+    }
+
+    if (
+      this.a + this.b <= this.c
+      || this.a + this.c <= this.b
+      || this.c + this.b <= this.a
+    ) {
+      throw new Error('Incorrect size!');
+    }
+  }
+
+  getArea(): number {
+    const half: number = (this.a + this.b + this.c) / 2;
+    const squareArea: number = half
+    * (half - this.a)
+    * (half - this.b)
+    * (half - this.c);
+
+    return Math.floor(Math.sqrt(squareArea) * 100) / 100;
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  shape: Shape = 'circle';
 
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (this.radius <= 0) {
+      throw new Error('Circle radius must be greater than 0');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor((Math.PI * (this.radius ** 2)) * 100) / 100;
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  shape: Shape = 'rectangle';
 
+  constructor(
+    public color: Color,
+    public weight: number,
+    public height: number,
+  ) {
+    if (this.weight <= 0 || this.height <= 0) {
+      throw new Error('Parameters must be greater than 0');
+    }
+  }
+
+  getArea(): number {
+    return this.weight * this.height;
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
