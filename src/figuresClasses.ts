3ba3d64a5d80b2128b92000d5bef3d1fdbbcb26c
Denis Bokov <denisbokov1703@gmail.com>
2023-01-21 17:55:57 +0100

    Solution (not quite done)

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,92 @@
+export enum Shapes {
+    Triangle = 'triangle',
+    Circle = 'circle',
+    Rectangle = 'rectangle',
+}
+
+export enum Colors {
+    Red = 'red',
+    Green = 'green',
+    Blue = 'blue', 
+}
+
 export interface Figure {
-
+    shape: Shapes;
+    color: Colors;
+    getArea(): number;
 }
 
-export class Triangle {
 
+export class Triangle implements Figure {
+    shape = Shapes.Triangle;
+
+    constructor(
+        public color = Colors,
+        public a: number,
+        public b: number,
+        public c: number,
+    ) {
+        let sides: number[] = [a, b, c];
+
+        sides.sort((a, b) => b - a);
+
+        if (sides.some((side) => side <= 0)) {
+            throw Error('One side of the sides less then 0')
+        }
+
+        if (sides[0] >= sides[1] + sides[2]) {
+            throw Error('This is not Triangle');
+        }
+    }
+
+    getArea(): number {
+        const sum = (this.a + this.b + this.c) / 2;
+        const area = Math.sqrt(sum * (sum - this.a) * (sum - this.b) * (sum - this.c));
+
+        return Math.floor(area * 100) / 100;
+    }
 }
 
-export class Circle {
+export class Circle implements Figure {
+    shape = Shapes.Circle;
 
+    constructor(
+        public radius: number, 
+        public color: Colors
+    ) {
+        if (radius <= 0) {
+            throw Error('This is not a circle');
+        }
+    }
+
+    getArea(): number {
+        const area = Math.PI * this.radius ** 2;
+        return Math.floor(area * 100) / 100;
+    }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+    // public shape = Shapes.Rectangle;
 
+    constructor(
+        public shape = Shapes.Rectangle,
+        public color: Colors,
+        public width: number,
+        public height: number,
+    ) {
+        if (width <= 0 || height <= 0) {
+            throw Error('This is not rectangle');
+        }
+    }
+
+    getArea(): number {
+        const {width, height} = this;
+       const area = width * height;
+       
+       return Math.floor(area * 100) / 100;
+    } 
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+    return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`
 }
