60f8758858aed92e8d55ea6cbdb5d172adbc6943
Denis Bokov <denisbokov1703@gmail.com>
2023-01-22 16:41:46 +0100

    Solution (1)

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,92 +1,97 @@
 export enum Shapes {
-    Triangle = 'triangle',
-    Circle = 'circle',
-    Rectangle = 'rectangle',
+  Triangle = 'triangle',
+  Circle = 'circle',
+  Rectangle = 'rectangle',
 }
 
 export enum Colors {
-    Red = 'red',
-    Green = 'green',
-    Blue = 'blue', 
+  Red = 'red',
+  Green = 'green',
+  Blue = 'blue'
 }
 
 export interface Figure {
-    shape: Shapes;
-    color: Colors;
-    getArea(): number;
+  shape: Shapes;
+  color: Colors;
+  getArea(): number;
 }
 
+function toFixed(area: number): number {
+  return Math.floor(area * 100) / 100;
+}
 
 export class Triangle implements Figure {
-    shape = Shapes.Triangle;
+  shape = Shapes.Triangle;
 
-    constructor(
-        public color = Colors,
-        public a: number,
-        public b: number,
-        public c: number,
-    ) {
-        let sides: number[] = [a, b, c];
+  constructor(
+    public color: Colors,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    const sides: number[] = [a, b, c];
 
-        sides.sort((a, b) => b - a);
+    sides.sort((x, y) => x - y);
 
-        if (sides.some((side) => side <= 0)) {
-            throw Error('One side of the sides less then 0')
-        }
-
-        if (sides[0] >= sides[1] + sides[2]) {
-            throw Error('This is not Triangle');
-        }
+    if (sides.some((side) => side <= 0)) {
+      throw Error('One side of the sides less then 0');
     }
 
-    getArea(): number {
-        const sum = (this.a + this.b + this.c) / 2;
-        const area = Math.sqrt(sum * (sum - this.a) * (sum - this.b) * (sum - this.c));
-
-        return Math.floor(area * 100) / 100;
+    if (sides[0] >= sides[1] + sides[2]) {
+      throw Error('This is not Triangle');
     }
+  }
+
+  getArea(): number {
+    const sum = (this.a + this.b + this.c) / 2;
+    const area = Math.sqrt(
+      sum * (sum - this.a) * (sum - this.b) * (sum - this.c),
+    );
+
+    return toFixed(area);
+  }
 }
 
 export class Circle implements Figure {
-    shape = Shapes.Circle;
+  shape = Shapes.Circle;
 
-    constructor(
-        public radius: number, 
-        public color: Colors
-    ) {
-        if (radius <= 0) {
-            throw Error('This is not a circle');
-        }
+  constructor(
+    public color: Colors,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw Error('This is not a circle');
     }
+  }
 
-    getArea(): number {
-        const area = Math.PI * this.radius ** 2;
-        return Math.floor(area * 100) / 100;
-    }
+  getArea(): number {
+    const area = Math.PI * this.radius ** 2;
+
+    return toFixed(area);
+  }
 }
 
 export class Rectangle implements Figure {
-    // public shape = Shapes.Rectangle;
+  public shape = Shapes.Rectangle;
 
-    constructor(
-        public shape = Shapes.Rectangle,
-        public color: Colors,
-        public width: number,
-        public height: number,
-    ) {
-        if (width <= 0 || height <= 0) {
-            throw Error('This is not rectangle');
-        }
+  constructor(
+    public color: Colors,
+    public width: number,
+    public height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw Error('This is not rectangle');
     }
+  }
 
-    getArea(): number {
-        const {width, height} = this;
-       const area = width * height;
-       
-       return Math.floor(area * 100) / 100;
-    } 
+  getArea(): number {
+    const { width, height } = this;
+    const area = width * height;
+
+    return toFixed(area);
+  }
 }
 
 export function getInfo(figure: Figure): string {
-    return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
