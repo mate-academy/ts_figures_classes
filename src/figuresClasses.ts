813c6a8d90867b97957c82863d57b526eee933d6
Kate <ekaterina2002aleks@gmail.com>
2023-02-02 21:04:59 +0200

    fix bugs

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -7,90 +7,66 @@ export interface Figure {
 }
 
 export class Triangle implements Figure {
-  shape: Shape;
+  shape: Shape = 'triangle';
 
-  color: Color;
-
-  a: number;
-
-  b: number;
-
-  c: number;
-
-  constructor(color: Color, a: number, b: number, c: number) {
+  constructor(public color: Color,
+    public a: number,
+    public b: number,
+    public c: number) {
     const max = Math.max(a, b, c);
 
     if ((max >= (a + b + c - max)) || (a <= 0) || (b <= 0) || (c <= 0)) {
       throw new Error(`sides ${a}, ${b} and ${c} can't form a triangle`);
     }
-
-    this.shape = 'triangle';
-    this.color = color;
-    this.a = a;
-    this.b = b;
-    this.c = c;
   }
 
   getArea(): number {
-    const s: number = (this.a + this.b + this.c) / 2;
-    const underSqrt: number = s * (s - this.a) * (s - this.b) * (s - this.c);
-    const AreaLong: number = Math.sqrt(underSqrt);
-    const Area: number = +((AreaLong).toFixed(2));
+    const semiperimeter = (this.a + this.b + this.c) / 2;
+    const underSqrt = semiperimeter
+      * (semiperimeter - this.a)
+      * (semiperimeter - this.b) * (semiperimeter - this.c);
+    const AreaLong = Math.sqrt(underSqrt);
+    const Area = +((AreaLong).toFixed(2));
 
     return Area;
   }
 }
 
 export class Circle implements Figure {
-  shape: Shape;
+  shape: Shape = 'circle';
 
-  color: Color;
-
-  radius: number;
-
-  constructor(color: Color, radius: number) {
+  constructor(public color: Color, public radius: number) {
     if (radius <= 0) {
       throw new Error(`this radius ${radius} can't form a triangle`);
     }
-    this.shape = 'circle';
-    this.color = color;
-    this.radius = radius;
   }
 
   getArea(): number {
-    const AreaLong: number = Math.PI * this.radius * this.radius;
-    const Area: number = (Math.floor(AreaLong * 100)) / 100;
+    const AreaLong = Math.PI * this.radius * this.radius;
+    const Area = (Math.floor(AreaLong * 100)) / 100;
 
     return Area;
   }
 }
 
 export class Rectangle {
-  shape: Shape;
+  shape: Shape = 'rectangle';
 
-  color: Color;
-
-  width: number;
-
-  height: number;
-
-  constructor(color: Color, width: number, height: number) {
+  constructor(public color: Color,
+    public width: number,
+    public height: number) {
     if (width <= 0 || height <= 0) {
       throw new Error(`sides ${width} and ${height} can't form a rectangle`);
     }
-    this.shape = 'rectangle';
-    this.color = color;
-    this.width = width;
-    this.height = height;
   }
 
   getArea(): number {
-    const Area: number = +((this.width * this.height).toFixed(2));
+    const Area = +((this.width * this.height).toFixed(2));
 
     return Area;
   }
 }
 
-export function getInfo(figure: Circle | Triangle | Rectangle): string {
+export function getInfo(figure: Figure): string {
   return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
