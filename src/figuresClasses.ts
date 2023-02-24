92b5d0afa63acf33897b523eab0dc36886a01015
christian m wolf <108151633+devWizHub@users.noreply.github.com>
2023-02-24 12:11:47 +0100

    commit message

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,78 @@
-export interface Figure {
+// import { getTypeParameterOwner } from 'typescript';
 
+export interface Figure {
+  shape: string;
+  color: string;
+  getArea(): number;
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  shape = 'triangle';
 
+  constructor(
+    public color: string,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    const sides = [a, b, c];
+
+    if (sides.some((side) => side <= 0)) {
+      throw new Error('All sides must have positive length');
+    }
+
+    if (sides[2] >= (sides[0] + sides[1])) {
+      throw new Error(
+        'The longest side must be less than the sum of the other two sides',
+      );
+    }
+  }
+
+  getArea(): number {
+    const { a, b, c } = this;
+    const s = (a + b + c) / 2;
+
+    return Math.floor(Math.sqrt(s * (s - a) * (s - b) * (s - c)) * 100) / 100;
+  }
 }
 
 export class Circle {
+  shape = 'circle';
 
+  constructor(
+    public color: string,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('Radius must have a positive length');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor(Math.PI * this.radius ** 2 * 100) / 100;
+  }
 }
 
 export class Rectangle {
+  shape = 'rectangle';
 
+  constructor(
+    public color: string,
+    public width: number,
+    public height: number,
+  ) {
+    const dim = [width, height];
+
+    if (dim.some((el) => el <= 0)) {
+      throw new Error('All sides must have a positive length');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor(this.width * this.height);
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
