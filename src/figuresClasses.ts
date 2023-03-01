7eef0892df39cca3e810efcbc76e99f9f85ceeed
Rodion Pushkarskiy <pushkarskiyrodion@gmail.com>
2023-03-01 19:21:33 +0200

    figuresClasses was done

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,73 @@
 export interface Figure {
-
+  shape: string;
+  color: string;
+  getArea(): number;
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  shape = 'triangle';
 
+  constructor(
+    public color: string,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('The digit cannot be less than 0 or equal to 0');
+    }
+
+    const maxSide = Math.max(a, b, c);
+
+    if (maxSide >= (a + b + c) - maxSide) {
+      throw new Error('The max side should be less than a sum of two other');
+    }
+  }
+
+  getArea(): number {
+    const sum = 0.5 * (this.a + this.b + this.c);
+    const area = Math.floor(
+      Math.sqrt(
+        sum * (sum - this.a) * (sum - this.b) * (sum - this.c),
+      ) * 100,
+    ) / 100;
+
+    return area;
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  shape = 'circle';
 
+  constructor(public color: string, public radius: number) {
+    if (this.radius <= 0) {
+      throw new Error('The digit cannot be less than 0 or equal to 0');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor((Math.PI * this.radius ** 2) * 100) / 100;
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  shape = 'rectangle';
 
+  constructor(
+    public color: string,
+    public width: number,
+    public height: number,
+  ) {
+    if (this.width <= 0 || this.height <= 0) {
+      throw new Error('The digit cannot be less than 0 or equal to 0');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor((this.width * this.height) * 100) / 100;
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
