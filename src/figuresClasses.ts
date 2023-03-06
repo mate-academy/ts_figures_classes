b281d9c7cbdc728ca02a519f620cad467d82e3b2
Peti51 <galpeter31@gmail.com>
2023-03-06 19:54:46 +0100

    add task solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,62 @@
-export interface Figure {
+type Shape = 'triangle' | 'circle' | 'rectangle';
 
+type Color = 'red' |'green' |'blue';
+
+export interface Figure {
+  shape: Shape[],
+  color: Color[],
 }
 
 export class Triangle {
-
+  constructor(
+    public shape: string[],
+    public area: number,
+    public color: string[],
+  ) {
+    this.area = this.getArea;
+    this.shape = shape;
+    this.color = color;
+  }
 }
 
 export class Circle {
-
+  constructor(
+    public shape: string[],
+    public color: string[],
+    public area: number,
+  ) {
+    this.shape = shape;
+    this.color = color;
+  }
 }
 
 export class Rectangle {
-
+  constructor(
+    public shape: string[],
+    public color: string[],
+    public area: number,
+  ) {
+    this.shape = shape;
+    this.color = color;
+  }
 }
 
-export function getInfo(figure) {
+export function getInfo(figure: any): {} {
+  getArea() {
+    if (this.shape === 'rectangle') {
+      return (a * b).toFixed(2);
+    }
+    if (this.shape === 'triangle') {
+      if(a + b > c || a + c > b || b + c > a) {
+        return (a * b).toFixed(2);
+      }
+      return 'your error message';
+    }
+    if (this.shape === 'circle') {
+      return (Math.PI * r**2).toFixed(2);
+    }
+  }
+
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea}`
 
 }
