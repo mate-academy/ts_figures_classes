8a1e02b860b59433087162194cc001f36219e161
Peti51 <galpeter31@gmail.com>
2023-03-06 20:24:34 +0100

    add task solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,62 +1,59 @@
-type Shape = 'triangle' | 'circle' | 'rectangle';
-
-type Color = 'red' |'green' |'blue';
 
 export interface Figure {
-  shape: Shape[],
-  color: Color[],
+  shape: string,
+  color: string,
+  area: number,
 }
 
 export class Triangle {
   constructor(
-    public shape: string[],
+    public shape: string,
+    public color: string,
     public area: number,
-    public color: string[],
   ) {
-    this.area = this.getArea;
     this.shape = shape;
     this.color = color;
+  };
+
+  getArea(a: number, b: number, c: number) {
+    const s: number = + ((a + b + c) / 2).toFixed(2);
+    const area: number = + (Math.sqrt(s * (s - a) * (s - b) * (s - c))).toFixed(2);
+
+    this.area = area;
   }
 }
 
 export class Circle {
   constructor(
-    public shape: string[],
-    public color: string[],
+    public shape: string,
+    public color: string,
     public area: number,
   ) {
     this.shape = shape;
     this.color = color;
+  }
+
+  getArea(r: number) {
+    this.area = + (Math.PI * r**2).toFixed(2);
   }
 }
 
 export class Rectangle {
   constructor(
-    public shape: string[],
-    public color: string[],
+    public shape: string,
+    public color: string,
     public area: number,
   ) {
     this.shape = shape;
     this.color = color;
   }
+
+  getArea(a: number, b: number) {
+    this.area = + (a * b).toFixed(2);
+  }
 }
 
 export function getInfo(figure: any): {} {
-  getArea() {
-    if (this.shape === 'rectangle') {
-      return (a * b).toFixed(2);
-    }
-    if (this.shape === 'triangle') {
-      if(a + b > c || a + c > b || b + c > a) {
-        return (a * b).toFixed(2);
-      }
-      return 'your error message';
-    }
-    if (this.shape === 'circle') {
-      return (Math.PI * r**2).toFixed(2);
-    }
-  }
-
-  return `A ${figure.color} ${figure.shape} - ${figure.getArea}`
+  return `A ${figure.color} ${figure.shape} - ${figure.area}`
 
 }
