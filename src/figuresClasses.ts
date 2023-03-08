77b7cc3ffc2611e459c42725933f3b960020fa4a
vadosik15 <vadim.bratus@gmail.com>
2023-03-08 18:29:51 +0200

    created program

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,108 @@
+enum Shape {
+  Triangle = 'triangle',
+  Circle = 'circle',
+  Rectangle = 'rectangle',
+}
+
+enum Color {
+  Red = 'red',
+  Green = 'green',
+  Blue = 'blue',
+}
+
 export interface Figure {
-
+  shape : Shape;
+  color : Color;
+  getArea():number;
 }
 
-export class Triangle {
+abstract class BasedForm implements Figure {
+  abstract shape : Shape;
 
+  abstract color: Color;
+
+  abstract getArea(): number;
 }
 
-export class Circle {
+export class Triangle extends BasedForm {
+  readonly shape = Shape.Triangle;
 
+  constructor(
+    public color: Color,
+    public a:number,
+    public b:number,
+    public c:number,
+  ) {
+    super();
+
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('every side must be bigger than 0');
+    }
+
+    const sortedSides: number[] = [a, b, c]
+      .sort((aSide: number, bSide: number) => bSide - aSide);
+
+    if (sortedSides[0] >= sortedSides[1] + sortedSides[2]) {
+      throw new Error('every side must be bigger than 0');
+    }
+  }
+
+  getArea(): number {
+    const semiperimetr = (this.a + this.b + this.c) / 2;
+    const area = Math.sqrt(
+      semiperimetr
+      * (semiperimetr - this.a)
+      * (semiperimetr - this.b)
+      * (semiperimetr - this.c),
+    );
+
+    return +(area.toFixed(2));
+  }
 }
 
-export class Rectangle {
+export class Circle extends BasedForm {
+  readonly shape = Shape.Circle;
 
+  constructor(
+    public color:Color,
+    public radius: number,
+  ) {
+    super();
+
+    if (this.radius <= 0) {
+      throw new Error('chel minyai radius');
+    }
+  }
+
+  getArea(): number {
+    const area = Math.PI * this.radius ** 2;
+
+    return Math.floor(area * 100) / 100;
+  }
 }
 
-export function getInfo(figure) {
+export class Rectangle extends BasedForm {
+  public shape = Shape.Rectangle;
 
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
+    super();
+
+    if (this.width <= 0 || this.height <= 0) {
+      throw new Error('chel minyai radius');
+    }
+  }
+
+  getArea(): number {
+    const area = this.width * this.height;
+
+    return +(area.toFixed(2));
+  }
+}
+
+export function getInfo(figure: BasedForm): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
