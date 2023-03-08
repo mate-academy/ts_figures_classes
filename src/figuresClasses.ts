67ab6cea048f4367ffcd0ad23e7329d2d37efa28
Sasha Dranuta <dranuta97@gmail.com>
2023-03-08 21:59:32 +0200

    Solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,94 @@
+enum Shape {
+  Triangle = 'triangle',
+  Circle = 'circle',
+  Rectangle = 'rectangle'
+}
+
+enum Color {
+  Red = 'red',
+  Green = 'green',
+  Blue = 'blue',
+}
+
 export interface Figure {
-
+  shape: Shape,
+  color: Color,
+  getArea(): number,
 }
 
-export class Triangle {
-
+function сheckSidesLength(...sides: number[]): void {
+  if (Math.min(...sides) <= 0) {
+    throw new Error('Side lengths error: some sides <= 0');
+  }
 }
 
-export class Circle {
-
+function round(area: number): number {
+  return Math.floor((area) * 100) / 100;
 }
 
-export class Rectangle {
+export class Triangle implements Figure {
+  readonly shape = Shape.Triangle;
 
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    сheckSidesLength(a, b, c);
+
+    if (a + b <= c || a + c <= b || b + c <= a) {
+      throw new Error('Impossible to build a triangle with these sides');
+    }
+  }
+
+  getArea(): number {
+    const semiPerimeter = (this.a + this.b + this.c) / 2;
+
+    const area = Math.sqrt(semiPerimeter
+      * (semiPerimeter - this.a)
+      * (semiPerimeter - this.b)
+      * (semiPerimeter - this.c));
+
+    return round(area);
+  }
 }
 
-export function getInfo(figure) {
+export class Circle implements Figure {
+  readonly shape = Shape.Circle;
 
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (this.radius <= 0) {
+      throw new Error('Radius can not be 0 or less');
+    }
+  }
+
+  getArea(): number {
+    return round(Math.PI * this.radius ** 2);
+  }
+}
+
+export class Rectangle implements Figure {
+  readonly shape = Shape.Rectangle;
+
+  constructor(
+    public color: Color,
+    private width: number,
+    private height: number,
+  ) {
+    сheckSidesLength(height, width);
+  }
+
+  getArea(): number {
+    return round(this.width * this.height);
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  const { color, shape } = figure;
+
+  return `A ${color} ${shape} - ${figure.getArea()}`;
 }
