bcb9bcfe50104ce6f35c3a35775cd56860320163
Yevhen Petrovskyi <petrovskyi.yevhen@gmail.com>
2023-03-10 00:11:52 +0200

    Solution for ts_figures_classes

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,75 @@
-export interface Figure {
+type Shape = 'triangle' | 'circle' | 'rectangle';
+type Color = 'red' | 'green' | 'blue';
 
+export interface Figure {
+  shape: Shape,
+  color: Color,
+  getArea(): number,
 }
 
 export class Triangle {
+  shape: Shape = 'triangle';
 
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error(`Any length can't be <= 0`);
+    }
+
+    const longestSide = Math.max(a, b, c);
+
+    if (longestSide >= ((a + b + c) - longestSide)) {
+      throw new Error('The longest side of a triangle should be >= than a sum of two others');
+    }
+  }
+
+  getArea(): number {
+    const sum = 0.5 * (this.a + this.b + this.c);
+
+    return Math.floor(Math.sqrt(sum * (sum - this.a) * (sum - this.b)
+    * (sum - this.c)) * 100) / 100;
+  }
 }
 
 export class Circle {
+  shape: Shape = 'circle';
 
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error(`Radius can't be 0 or less`);
+    }
+  }
+
+  getArea(): number {
+    return Math.floor((Math.PI * this.radius ** 2) * 100) / 100;
+  }
 }
 
 export class Rectangle {
+  shape: Shape = 'rectangle';
 
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error(`Any length can't be <= 0`);
+    }
+  }
+
+  getArea(): number {
+    return Math.floor((this.width * this.height) * 100) / 100;
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure):string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
