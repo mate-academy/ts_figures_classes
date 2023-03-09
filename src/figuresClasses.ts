6ebec53ba3fb0942a82491d2ac21e982fcd7fbf2
Yevhenii Hura <yevheniihura@gmail.com>
2023-03-09 15:11:27 +0200

    the issues fot test were solved

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -14,26 +14,28 @@ export class Triangle implements Figure {
     public c: number,
   ) {
     if (a <= 0 || b <= 0 || c <= 0) {
-      throw new Error (
+      throw new Error(
         'The length of the sides of the triangle'
         + 'cannot be less or equal to zero'
       );
     }
 
     if (a + b <= c || a + c <= b || b + c <= a) {
-      throw new Error (
+      throw new Error(
         'The sum of two sides of a triangle cannot be'
         + 'less than the length of the third side'
-      )
+      );
     }
   }
 
   getArea(): number {
     const s: number = (this.a + this.b + this.c) / 2;
-    const area: number = Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+    const area: number = Math.sqrt(
+      s * (s - this.a) * (s - this.b) * (s - this.c)
+    );
+
     return +area.toFixed(2);
   }
-
 }
 
 export class Circle implements Figure {
@@ -44,7 +46,7 @@ export class Circle implements Figure {
     public radius: number,
   ) {
     if (radius <= 0) {
-      throw new Error (
+      throw new Error(
         'The radius of the circle'
         + 'cannot be less or equal to zero'
       );
@@ -53,9 +55,9 @@ export class Circle implements Figure {
 
   getArea(): number {
     const area: number = Math.PI * (this.radius ** 2);
+
     return +area.toFixed(2);
   }
-
 }
 
 export class Rectangle implements Figure {
@@ -67,7 +69,7 @@ export class Rectangle implements Figure {
     public height: number,
   ) {
     if (width <= 0 || height <= 0) {
-      throw new Error (
+      throw new Error(
         'The width or height of the rectangle'
         + 'cannot be less or equal to zero'
       );
@@ -76,6 +78,7 @@ export class Rectangle implements Figure {
 
   getArea(): number {
     const area = this.width * this.height;
+
     return +area.toFixed(2);
   }
 }
