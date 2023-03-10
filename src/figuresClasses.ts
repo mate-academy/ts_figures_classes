68211644027dcc9704ad07d7c7f42ccfebe10ade
Peti51 <galpeter31@gmail.com>
2023-03-10 15:41:37 +0100

    add task solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -20,20 +20,23 @@ export class Triangle {
     this.a = a;
     this.b = b;
     this.c = c;
-  }
 
-  getArea(): number {
     const bigest: number = Math.max(this.a, this.b, this.c);
 
     const sum: number = this.a + this.b + this.c;
 
-    if (this.a <= 0
-      || this.b <= 0
-      || this.c <= 0
-      || (sum - bigest) <= bigest) {
-      throw new Error('The params should be bigger than zero.');
+    if ((sum - bigest) <= bigest) {
+      throw new Error(
+        'The sum of the two shorter side should be bigger than the third side.',
+      );
     }
 
+    if (this.a <= 0 || this.b <= 0 || this.c <= 0) {
+      throw new Error('The params should be bigger than zero.');
+    }
+  }
+
+  getArea(): number {
     const s: number = +((this.a + this.b + this.c) / 2).toFixed(2);
     const area: number
     = +Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
@@ -51,12 +54,16 @@ export class Circle {
   ) {
     this.color = color;
     this.radius = radius;
+
+    if (this.radius <= 0) {
+      throw new Error('The radius of the circle must be a positive number.');
+    }
   }
 
   getArea(): number {
-    const areaCircle = +(Math.PI * this.radius ** 2).toFixed(2);
+    const areaCircle = Math.PI * this.radius ** 2;
 
-    return areaCircle;
+    return +areaCircle.toString().slice(0, 6);
   }
 }
 
@@ -69,6 +76,12 @@ export class Rectangle {
     public height: number,
   ) {
     this.color = color;
+
+    if (this.width <= 0 || this.height <= 0) {
+      throw new Error(
+        'The height and the width of the circle must be a positive number.',
+      );
+    }
   }
 
   getArea(): number {
