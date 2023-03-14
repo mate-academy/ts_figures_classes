d6fc6b4dbe26b4f01e6c55f2f1a5fbba98a86418
Olha Kissa <olhakissa121r@gmail.com>
2023-03-14 12:42:57 +0200

    Solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,78 @@
+type Shape = 'triangle' | 'circle' | 'rectangle';
+type Color = 'red' | 'green' | 'blue';
+
 export interface Figure {
+  shape: Shape;
+  color: Color;
+  getArea(): number;
+}
+
+export function checkTriangle(sides: number[]): boolean {
+  if (sides instanceof Array) {
+
+    return true;
+  }
+}
+
+export class Triangle implements Figure {
+  shape: Shape = 'triangle';
+
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0 || checkTriangle([a, b, c])) {
+      throw new Error('Incorrect data');
+    }
+  }
+
+  getArea(): number {
+    const sq: number = (this.a + this.b + this.c) / 2;
+    sq.toFixed(2);
+
+    return sq;
+  }
+}
+
+export class Circle implements Figure {
+  shape: Shape = 'circle';
+
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (this.radius <= 0) {
+      throw new Error('Parameter is not valid');
+    }
+  }
+
+  getArea(): number {
+    const area: number = Math.PI * this.radius ** 2;
+
+    return Math.floor(area * 100) / 100;
+  }
 
 }
 
-export class Triangle {
+export class Rectangle implements Figure {
+  shape: Shape = 'rectangle';
 
-}
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
+  }
 
-export class Circle {
+  getArea(): number {
+    const square = (this.width * this.height);
 
-}
-
-export class Rectangle {
-
+    return square;
+  }
 }
 
 export function getInfo(figure) {
-
+  return `A ${this.Color} ${this.Shape} - ${this.getArea()}`;
 }
