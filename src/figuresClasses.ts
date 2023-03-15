a4de595641c1e392c326a3a88de05d52be8a55a7
Yevhen-Ivanchuk <e.ivanchyk@gmail.com>
2023-03-15 13:16:03 +0200

    Figures classes

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,77 @@
+type Shape = 'triangle' | 'circle' | 'rectangle';
+type Color = 'red' | 'green' | 'blue';
+
 export interface Figure {
-
+  shape: Shape;
+  color: Color;
+  getArea(): number;
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  readonly shape = 'triangle';
 
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    const sides = [a, b, c].sort(() => a - b);
+
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('Not is triangle');
+    } else if (sides[0] + sides[1] <= sides[2]
+      || sides[0] + sides[2] <= sides[1]
+      || sides[1] + sides[2] <= sides[0]) {
+      throw new Error('Not is triangle');
+    }
+  }
+
+  getArea(): number {
+    const s = (this.a + this.b + this.c) / 2;
+    const area = Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+
+    return +area.toFixed(2);
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  readonly shape = 'circle';
 
+  constructor(public color: Color, public radius: number) {
+    if (radius <= 0) {
+      throw new Error(`Radius ${radius} must be positive`);
+    }
+  }
+
+  getArea(): number {
+    const area = Math.PI * this.radius * this.radius;
+    const areaMath = (Math.floor(area * 100)) / 100;
+
+    return areaMath;
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  readonly shape = 'rectangle';
 
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('Width and height must be positive');
+    }
+  }
+
+  getArea(): number {
+    const area = this.width * this.height;
+
+    return +area.toFixed(2);
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
