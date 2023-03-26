ae52248120f7b4918149d2d629788a048d357480
Serhii Nikolin <sergeynikolin@gmail.com>
2023-03-26 17:28:04 +0300

    Ready

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,73 @@
+/* eslint-disable no-useless-constructor */
 export interface Figure {
-
+  shape: string;
+  color: string;
+  getArea(): number | never;
 }
 
-export class Triangle {
-
+function numberRound(x: number): number {
+  return Math.floor(x * 100) / 100;
 }
 
-export class Circle {
+export class Triangle implements Figure {
+  public shape: string = 'triangle';
 
+  constructor(
+    public color: string,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0
+      || Math.max(a, b, c) >= (a + b + c - Math.max(a, b, c))) {
+      throw new Error('the entered data is incorrect');
+    }
+  }
+
+  getArea(): number | never {
+    const s: number = (this.a + this.b + this.c) / 2;
+
+    return numberRound(
+      Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c)),
+    );
+  }
 }
 
-export class Rectangle {
+export class Circle implements Figure {
+  public shape: string = 'circle';
 
+  constructor(
+    public color: string,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('the entered data is incorrect');
+    }
+  }
+
+  getArea(): number | never {
+    return numberRound(Math.PI * this.radius ** 2);
+  }
 }
 
-export function getInfo(figure) {
+export class Rectangle implements Figure {
+  public shape: string = 'rectangle';
 
+  constructor(
+    public color: string,
+    public width: number,
+    public height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('the entered data is incorrect');
+    }
+  }
+
+  getArea(): number | never {
+    return numberRound(this.width * this.height);
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
