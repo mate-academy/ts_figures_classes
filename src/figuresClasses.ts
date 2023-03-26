01d60143f38c9d587fad1ddb23dd2a1a46d70f71
Olexandr Istomin <gn.53@ukr.net>
2023-03-27 00:56:23 +0300

    Solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,78 @@
 export interface Figure {
+  shape: 'triangle' | 'circle' | 'rectangle';
+  color: 'red' | 'green' | 'blue';
+  getArea: () => number;
+}
 
+function error(): never {
+  throw new Error('Sides cannot be zero');
 }
 
 export class Triangle {
+  shape: 'triangle';
 
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    this.shape = 'triangle';
+
+    if (a >= b + c || b >= a + c || c >= a + b) {
+      throw new Error(`sides ${a}, ${b} and ${c} can't form a triangle`);
+    } else if (a <= 0 || b <= 0 || c <= 0) {
+      error();
+    }
+  }
+
+  getArea(): number {
+    const p = (this.a + this.b + this.c) / 2;
+
+    return +Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c))
+      .toFixed(2);
+  }
 }
 
 export class Circle {
+  shape: 'circle';
 
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public radius: number,
+  ) {
+    this.shape = 'circle';
+
+    if (radius <= 0) {
+      error();
+    }
+  }
+
+  getArea(): number {
+    return Math.floor((this.radius * this.radius * Math.PI) * 100) / 100;
+  }
 }
 
 export class Rectangle {
+  shape: 'rectangle';
 
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public width: number,
+    public height: number,
+  ) {
+    this.shape = 'rectangle';
+
+    if (width <= 0 || height <= 0) {
+      error();
+    }
+  }
+
+  getArea(): number {
+    return +(this.width * this.height);
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
