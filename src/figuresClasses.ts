6d7c71ff093383f74bb89ac3f2970a72b870af39
Vlad <bojko.vladislav16@gmail.com>
2023-03-31 12:09:37 +0300

    code has been mofidied

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,6 +1,10 @@
+type Shapes = "triangle" | "circle" | "rectangle";
+type Colors = "red" | "green" | "blue";
+type colorOrNumber = Colors | number;
+
 export interface Figure {
-  shape: "triangle" | "circle" | "rectangle";
-  color: "red" | "green" | "blue";
+  shape: Shapes;
+  color: Colors;
   getArea(): number;
 }
 
@@ -9,8 +13,6 @@ const isNotATriangle = (a: number, b: number, c: number): boolean => {
 
   return temp[2] >= temp[0] + temp[1] ? true : false;
 };
-
-type colorOrNumber = "red" | "green" | "blue" | number;
 
 const lengthIsLessThanZero = (...params: colorOrNumber[]): boolean => {
   return params.some((param) => {
@@ -27,16 +29,12 @@ const round = (formula: number): number => {
 };
 
 export class Triangle implements Figure {
-  shape: "triangle";
-
   constructor(
-    public color: "red" | "green" | "blue",
+    public color: Colors,
     public a: number,
     public b: number,
     public c: number
   ) {
-    this.shape = "triangle";
-
     if (lengthIsLessThanZero(color, a, b, c)) {
       throw new Error("The data you entered is not valid");
     }
@@ -46,6 +44,8 @@ export class Triangle implements Figure {
     }
   }
 
+  shape: Shapes = "triangle";
+
   getArea(): number {
     const s = (this.a + this.b + this.c) / 2;
 
@@ -54,15 +54,13 @@ export class Triangle implements Figure {
 }
 
 export class Circle implements Figure {
-  shape: "circle";
-
-  constructor(public color: "red" | "green" | "blue", public radius: number) {
-    this.shape = "circle";
-
+  constructor(public color: Colors, public radius: number) {
     if (lengthIsLessThanZero(color, radius)) {
       throw new Error("The data you entered is not valid");
     }
   }
+
+  shape: Shapes = "circle";
 
   getArea(): number {
     return round(Math.PI * this.radius ** 2);
@@ -70,19 +68,17 @@ export class Circle implements Figure {
 }
 
 export class Rectangle implements Figure {
-  shape: "rectangle";
-
   constructor(
-    public color: "red" | "green" | "blue",
+    public color: Colors,
     public width: number,
     public height: number
   ) {
-    this.shape = "rectangle";
-
     if (lengthIsLessThanZero(color, width, height)) {
       throw new Error("The data you entered is not valid");
     }
   }
+
+  shape: Shapes = "rectangle";
 
   getArea(): number {
     return round(this.width * this.height);
