0943366b24117963caf58cdbd97c7352b6239e8c
Alisa Vinnyk <alisavinnyk@gmail.com>
2023-04-06 06:55:12 +0100

    removed unnecessary code

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,24 +1,24 @@
-type Shapes = 'triangle' | 'circle' | 'rectangle';
-type Colors = 'red' | 'green' |'blue';
+type Shape = 'triangle' | 'circle' | 'rectangle';
+type Color = 'red' | 'green' |'blue';
 
 export interface Figure {
-  shape: Shapes,
-  color: Colors,
+  shape: Shape,
+  color: Color,
 
   getArea: () => number;
 }
 
 export class Triangle implements Figure {
-  shape: Shapes = 'triangle';
+  shape: Shape = 'triangle';
 
   constructor(
-    public color: Colors,
+    public color: Color,
     public a: number,
     public b: number,
     public c: number,
   ) {
     if (a <= 0 || b <= 0 || c <= 0) {
-    // eslint-disable-next-line
+      // eslint-disable-next-line
       throw new Error(`Every side (a: ${a}, b: ${b}, c: ${c})of triangle must be greater than 0`);
     }
 
@@ -26,20 +26,15 @@ export class Triangle implements Figure {
     // eslint-disable-next-line
       throw new Error('The longest side of a triangle should be greater or equal to a sum of two others');
     }
-
-    this.color = color;
-    this.a = a;
-    this.b = b;
-    this.c = c;
   }
 
   getArea(): number {
-    const halfPerimeter = (this.a + this.b + this.c) / 2;
+    const halfOfAPerimeter = (this.a + this.b + this.c) / 2;
     const square = Math.floor(
-      Math.sqrt(halfPerimeter
-        * (halfPerimeter - this.a)
-        * (halfPerimeter - this.b)
-        * (halfPerimeter - this.c)) * 100,
+      Math.sqrt(halfOfAPerimeter
+        * (halfOfAPerimeter - this.a)
+        * (halfOfAPerimeter - this.b)
+        * (halfOfAPerimeter - this.c)) * 100,
     ) / 100;
 
     return square;
@@ -47,18 +42,15 @@ export class Triangle implements Figure {
 }
 
 export class Circle implements Figure {
-  shape: Shapes = 'circle';
+  shape: Shape = 'circle';
 
   constructor(
-    public color: Colors,
+    public color: Color,
     public radius: number,
   ) {
     if (radius <= 0) {
       throw new Error(`Radius (${radius}) of a circle must be greater than 0`);
     }
-
-    this.color = color;
-    this.radius = radius;
   }
 
   getArea(): number {
@@ -67,23 +59,17 @@ export class Circle implements Figure {
 }
 
 export class Rectangle implements Figure {
-  shape: Shapes = 'rectangle';
+  shape: Shape = 'rectangle';
 
-  public color: Colors;
-
-  public width: number;
-
-  public height: number;
-
-  constructor(color: Colors, width: number, height: number) {
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
     if (width <= 0 || height <= 0) {
       // eslint-disable-next-line
       throw new Error(`Width (${width}) and heigth (${height}) must be greater than 0`);
     }
-
-    this.color = color;
-    this.width = width;
-    this.height = height;
   }
 
   getArea(): number {
