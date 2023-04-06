a1465a71261db34db418ceb91706e9815331b77f
Tanya Sydoruk <tanyasydoruk113022@gmail.com>
2023-04-06 14:10:52 +0300

    Solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,87 @@
+type Shape = 'triangle' | 'circle' | 'rectangle';
+type Color = 'red' | 'green' | 'blue';
+
 export interface Figure {
-
+  shape: Shape;
+  color: Color;
+  getArea(): number;
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  shape: Shape = 'triangle';
 
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    const sides = [a, b, c];
+    const sumOfSides = sides.reduce((acc, curr) => acc + curr, 0);
+    const maxSide = Math.max(...sides);
+
+    if (sides.some((side) => side <= 0)) {
+      throw new Error('One of the sides is less than 0.');
+    }
+
+    if (maxSide >= (sumOfSides - maxSide)) {
+      // eslint-disable-next-line
+      throw new Error('The longest side of a triangle is greater than or equal to the sum of the sides.');
+    }
+  }
+
+  getArea(): number {
+    const { a, b, c } = this;
+    const semiPerimeter = (a + b + c) / 2;
+    const square = Math.sqrt(
+      semiPerimeter
+      * (semiPerimeter - a)
+      * (semiPerimeter - b)
+      * (semiPerimeter - c),
+    );
+
+    return Number(square.toFixed(2));
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  shape: Shape = 'circle';
 
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('The radius is less than or equal to 0');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor((Math.PI * this.radius ** 2) * 100) / 100;
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  shape: Shape = 'rectangle';
 
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
+    const sides = [width, height];
+    const subZero = sides.some((side) => side <= 0);
+
+    if (subZero) {
+      throw new Error('One of your sides is less than 0');
+    }
+  }
+
+  getArea(): number {
+    return Number((this.width * this.height).toFixed(2));
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
