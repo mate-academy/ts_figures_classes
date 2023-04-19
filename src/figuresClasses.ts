0eb8ad0522d5fbc168a306a829752bd6a12f4af5
Uladzislau Bichko <wladyslaw.biczko@gmail.com>
2023-04-19 12:33:29 +0200

    fix

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -8,75 +8,74 @@ export interface Figure {
 }
 
 export class Triangle implements Figure {
-    shape: Shape = 'triangle';
+  shape: Shape = 'triangle';
 
-    constructor(
-      public color: Color,
-      public a: number,
-      public b: number,
-      public c: number,
-    ) {
-        if (a <= 0
-          || b <= 0
-          || c <= 0
-          || a >= b + c
-          || b >= a + c
-          || c >= a + b) {
-          throw new Error('Invalid params');
-        }
-  
-      this.a = a;
-      this.b = b;
-      this.c = c;
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (a <= 0
+      || b <= 0
+      || c <= 0
+      || a >= b + c
+      || b >= a + c
+      || c >= a + b) {
+      throw new Error('Invalid params');
     }
-  
-    getArea(): number {
-      const semiperimeter: number = (this.a + this.b + this.c) / 2;
-      const area: number = Math.sqrt(semiperimeter
-        * (semiperimeter - this.a)
-        * (semiperimeter - this.b)
-        * (semiperimeter - this.c));
-  
-      return Number(area.toFixed(2));
+
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+
+  getArea(): number {
+    const semiperimeter: number = (this.a + this.b + this.c) / 2;
+    const area: number = Math.sqrt(semiperimeter
+      * (semiperimeter - this.a)
+      * (semiperimeter - this.b)
+      * (semiperimeter - this.c));
+
+    return Number(area.toFixed(2));
+  }
+}
+
+export class Circle implements Figure {
+  shape: Shape = 'circle';
+
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('Invalid params');
     }
   }
-  
-  export class Circle implements Figure {
-    shape: Shape = 'circle';
-  
-    constructor(
-      public color: Color,
-      public radius: number,
-    ) {
-  
-      if (radius <= 0) {
-        throw new Error('Invalid params');
-      }
-    }
-  
-    getArea(): number {
-        return Math.floor((Math.PI * (this.radius * this.radius)) * 100) / 100;
+
+  getArea(): number {
+    return Math.floor((Math.PI * (this.radius * this.radius)) * 100) / 100;
+  }
+}
+
+export class Rectangle implements Figure {
+  shape: Shape = 'rectangle';
+
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('Invalid params');
     }
   }
-  
-  export class Rectangle implements Figure {
-    shape: Shape = 'rectangle';
-  
-    constructor(
-      public color: Color,
-      public width: number,
-      public height: number,
-    ) {
-      if (width <= 0 || height <= 0) {
-        throw new Error('Invalid params');
-      }
-    }
-  
-    getArea(): number {
-      return this.height * this.width;
-    }
+
+  getArea(): number {
+    return this.height * this.width;
   }
-  
-  export function getInfo(figure: Figure): string {
-    return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
-  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
+}
