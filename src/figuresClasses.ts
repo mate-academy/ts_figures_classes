92cbaeae37848ec9e324e765e8ade33e39534159
Mykhailo Orlov <mihalikmihalik85@gmail.com>
2023-04-23 21:04:56 +0300

    all tests good

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,86 @@
-export interface Figure {
+type Color = 'red' | 'blue' | 'green';
 
+enum Shape {
+  Triangle = 'triangle',
+  Circle = 'circle',
+  Rectangle = 'rectangle',
+}
+export interface Figure {
+  shape: Shape;
+  color: Color;
+
+  getArea(): number;
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  shape = Shape.Triangle;
 
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('Value must be greater than zero');
+    }
+
+    if (a + b <= c || a + c <= b || b + c <= a) {
+      throw new Error(
+        'The longest side of a triangle must be <= than a sum of two others',
+      );
+    }
+  }
+
+  getArea(): number {
+    const halfOfPerimeter: number = (this.a + this.b + this.c) / 2;
+
+    const triangleArea = Math.sqrt(
+      halfOfPerimeter
+      * (halfOfPerimeter - this.a)
+      * (halfOfPerimeter - this.b)
+      * (halfOfPerimeter - this.c),
+    );
+
+    return Math.floor(triangleArea * 100) / 100;
+  }
 }
 
 export class Circle {
+  shape = Shape.Circle;
 
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('Value must be greater than zero');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor(Math.PI * (this.radius ** 2) * 100) / 100;
+  }
 }
 
 export class Rectangle {
+  shape = Shape.Rectangle;
 
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('Value must be greater than zero');
+    }
+  }
+
+  getArea():number {
+    return this.width * this.height;
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
