f1e3bd70ca46b648d8d2b748792c172ad7577849
Mateusz Michalowski <mateusz.michalowski000@gmail.com>
2023-04-24 20:45:46 +0200

    Solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -64,7 +64,7 @@ export class Rectangle implements Figure {
 
   sides: number[];
 
-  constructor(color:Color, ...args: number[]) {
+  constructor(color: Color, ...args: number[]) {
     this.shape = 'rectangle';
     this.color = color;
     this.sides = [...args];
