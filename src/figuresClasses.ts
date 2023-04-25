2a3de3ab0c27516273b3d62d1627eef44d2c639d
sylcym <prusak.syl@gmail.com>
2023-04-25 15:25:27 +0200

    update after code review

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,7 +1,7 @@
 enum Shape {
-  'triangle',
-  'circle',
-  'rectangle',
+  Triangle = 'triangle',
+  Circle = 'circle',
+  Rectangle = 'rectangle',
 }
 
 type Color = 'red' | 'green' | 'blue';
@@ -13,14 +13,16 @@ export interface Figure {
 }
 
 export class Triangle implements Figure {
+  shape = Shape.Triangle;
+
   constructor(
     public color: Color,
     public a: number,
     public b: number,
     public c: number,
-    public shape: Shape.triangle,
+
   ) {
-    if ((a <= 0 || b <= 0 || c <= 0)) {
+    if (a <= 0 || b <= 0 || c <= 0) {
       throw new Error('All values must be positive');
     }
 
@@ -30,8 +32,6 @@ export class Triangle implements Figure {
         'The longest side must be greater or equal than a sum of two others',
       );
     }
-
-    this.shape = 'triangle';
   }
 
   getArea(): number {
@@ -43,16 +43,16 @@ export class Triangle implements Figure {
 }
 
 export class Circle implements Figure {
+  shape = Shape.Circle;
+
   constructor(
     public color: Color,
     public radius: number,
-    public shape: Shape.circle,
+
   ) {
     if (radius <= 0) {
       throw new Error('Radius must be positive');
     }
-
-    this.shape = 'circle';
   }
 
   getArea(): number {
@@ -61,22 +61,21 @@ export class Circle implements Figure {
 }
 
 export class Rectangle implements Figure {
+  shape= Shape.Rectangle;
+
   constructor(
     public color: Color,
     public width: number,
     public height: number,
-    public shape: Shape.rectangle,
+
   ) {
     if (width <= 0 || height <= 0) {
       throw new Error('Width and height must be positive');
     }
-    this.shape = 'rectangle';
   }
 
   getArea(): number {
-    const a = this.width * this.height;
-
-    return a;
+    return this.width * this.height;
   }
 }
 
