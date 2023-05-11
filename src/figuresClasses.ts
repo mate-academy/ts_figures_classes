6d5eeb62420d41bcd86522cf45068c9e6858a5b6
Olena Shevchenko <shevochkas@gmail.com>
2023-05-11 18:16:29 +0300

    classes for figures were added

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,79 @@
+type Shape = 'triangle' | 'circle' | 'rectangle';
+type Color = 'red' | 'green' | 'blue';
+
 export interface Figure {
-
+  shape: Shape;
+  color: Color;
+  getArea(): number,
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  public shape: Shape = 'triangle';
 
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('Wrong sides');
+    } else if ((a + b <= c) || (a + c <= b) || (b + c <= a)) {
+      throw new Error('This triangle doesn`t exist');
+    }
+  }
+
+  getArea(): number {
+    const { a, b, c } = {
+      a: this.a,
+      b: this.b,
+      c: this.c,
+    };
+
+    const s: number = (a + b + c) / 2;
+    const square: number = Math.sqrt((s * (s - a) * (s - b) * (s - c)));
+
+    return Math.floor(square * 100) / 100;
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  public shape: Shape = 'circle';
 
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('Wrong sides');
+    }
+  }
+
+  getArea(): number {
+    const square = Math.PI * this.radius ** 2;
+
+    return Math.floor(square * 100) / 100;
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  public shape: Shape = 'rectangle';
 
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
+    if (height <= 0 || width <= 0) {
+      throw new Error('Wrong sides');
+    }
+  }
+
+  getArea(): number {
+    return this.height * this.width;
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
