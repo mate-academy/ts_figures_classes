5d89874124b19031cf4cad30d807786aa199964f
Mateusz Michalowski <mateusz.michalowski000@gmail.com>
2023-05-11 17:06:50 +0200

    Final fix

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,83 +1,65 @@
-export interface Figure {
-  shape: 'triangle' | 'circle' | 'rectangle';
-  color: 'red' | 'green' | 'blue';
-  getArea(): number;
-}
-
 type Color = 'red' | 'green' | 'blue';
 
+enum Shape {
+  Triangle = 'triangle',
+  Circle = 'circle',
+  Rectangle = 'rectangle'
+}
+
+export interface Figure {
+  shape: Shape,
+  color: Color,
+  getArea: Function,
+}
+
 export class Triangle implements Figure {
-  shape: 'triangle';
+  constructor(public color:Color, public a: number, public b: number,
+    public c: number, public shape = Shape.Triangle) {
+    const sides = [a, b, c];
+    const sortedSides = sides.sort((sideA, sideB) => sideA - sideB);
 
-  sides: number[];
-
-  color: Color;
-
-  constructor(color:Color, ...args:number[]) {
-    this.shape = 'triangle';
-    this.color = color;
-    this.sides = [...args].sort((a, b) => a - b);
-
-    if (this.sides[0] <= 0 || this.sides[1] <= 0 || this.sides[2] <= 0) {
-      throw new Error('At least one property is less than 0');
+    if (sortedSides[2] >= sortedSides[0] + sortedSides[1]) {
+      throw new Error('This is not a triangle!');
     }
 
-    if (this.sides[0] + this.sides[1] <= this.sides[2]) {
-      throw new Error('Not a triangle');
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('This is not a triangle!');
     }
   }
 
   getArea(): number {
-    const [a, b, c] = [...this.sides];
-    const s = (a + b + c) / 2;
+    const p = (this.a + this.b + this.c) / 2;
 
-    return Math.floor(Math.sqrt(s * (s - a) * (s - b) * (s - c)) * 100) / 100;
+    return Math.round(
+      Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)) * 100,
+    ) / 100;
   }
 }
 
 export class Circle implements Figure {
-  shape: 'circle';
-
-  radius: number;
-
-  color: Color;
-
-  constructor(color: Color, radius:number) {
-    this.shape = 'circle';
-    this.radius = radius;
-    this.color = color;
-
+  constructor(public color: Color, public radius:number,
+    public shape = Shape.Circle) {
     if (radius <= 0) {
-      throw new Error('At least one property is less than 0');
+      throw new Error('This is not a circle!');
     }
   }
 
   getArea(): number {
-    return Math.floor(Math.PI * this.radius * this.radius * 100) / 100;
+    return Math.floor(Math.PI * (this.radius ** 2) * 100) / 100;
   }
 }
 
 export class Rectangle implements Figure {
-  shape: 'rectangle';
-
-  color: Color;
-
-  sides: number[];
-
-  constructor(color: Color, ...args: number[]) {
-    this.shape = 'rectangle';
-    this.color = color;
-    this.sides = [...args];
-
-    if (this.sides[0] <= 0 || this.sides[1] <= 0) {
-      throw new Error('At least one property is less than 0');
+  constructor(public color: Color,
+    public a: number, public b: number,
+    public shape = Shape.Rectangle) {
+    if (a <= 0 || b <= 0) {
+      throw new Error('This is not a rectangle!');
     }
   }
 
   getArea(): number {
-    const [a, b] = this.sides;
-
-    return Math.floor(a * b * 100) / 100;
+    return Math.floor(this.a * this.b * 100) / 100;
   }
 }
 
