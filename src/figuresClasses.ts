cb26050955108f1aeb02786e166b329baf9195c7
danulyk05 <ivan.danulyk2016@gmail.com>
2023-05-20 13:24:08 +0300

    solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,86 @@
 export interface Figure {
-
+  shape: 'triangle' | 'circle' | 'rectangle';
+  color: 'red' | 'green' | 'blue';
+  getArea(): number;
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  shape: 'triangle' | 'circle' | 'rectangle';
 
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public sideA: number,
+    public sideB: number,
+    public sideC: number,
+  ) {
+    if (sideA <= 0 || sideB <= 0 || sideC <= 0) {
+      throw new Error('Invalid side length. '
+      + 'Side length must be greater than 0.');
+    }
+
+    if (sideA + sideB <= sideC
+      || sideB + sideC <= sideA
+      || sideC + sideA <= sideB) {
+      throw new Error('Invalid triangle sides. '
+      + 'The sum of any two sides must be greater than the third side.');
+    }
+
+    this.shape = 'triangle';
+  }
+
+  getArea(): number {
+    const s = (this.sideA + this.sideB + this.sideC) / 2;
+    const area = Math.sqrt(s * (s - this.sideA) * (s - this.sideB)
+      * (s - this.sideC));
+
+    return Math.round(area * 100) / 100;
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  shape: 'triangle' | 'circle' | 'rectangle';
 
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('Invalid radius. Radius must be greater than 0.');
+    }
+
+    this.shape = 'circle';
+  }
+
+  getArea(): number {
+    const area = Math.PI * this.radius * this.radius;
+
+    return Math.floor(area * 100) / 100;
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  shape: 'triangle' | 'circle' | 'rectangle';
 
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public width: number,
+    public height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('Invalid width or height. '
+      + 'Width and height must be greater than 0.');
+    }
+
+    this.shape = 'rectangle';
+  }
+
+  getArea(): number {
+    const area = this.width * this.height;
+
+    return Math.round(area * 100) / 100;
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
