fc8f087b97061d477dff1d3019e779f57f266d41
Sergii Mishchenko <serge.mishchenko@gmail.com>
2023-05-25 15:11:37 +0300

    Figures have been programmed

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,67 @@
 export interface Figure {
-
+  shape: string;
+  color: string;
+  getArea(): number;
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  shape = 'triangle';
 
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (this.a <= 0 || this.b <= 0 || this.c <= 0
+      || a + b <= c || a + c <= b || b + c <= a) {
+      throw new Error('Error: this figure is not a triangle');
+    }
+  }
+
+  getArea(): number {
+    const p = (this.a + this.b + this.c) / 2;
+    const s = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+
+    return Math.floor(100 * s) / 100;
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  shape = 'circle';
 
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public radius: number,
+  ) {
+    if (this.radius <= 0) {
+      throw new Error('Error: this figure is not a circle');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor(100 * Math.PI * (this.radius ** 2)) / 100;
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  shape = 'rectangle';
 
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public width: number,
+    public height: number,
+  ) {
+    if (this.width <= 0 || this.height <= 0) {
+      throw new Error('Error: this figure is not a rectangle');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor(100 * this.width * this.height) / 100;
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
