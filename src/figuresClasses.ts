1f18d8e4349c2bdcd430fe15b8c9969151e77cf6
Oleksandr Kosohor <kosogor2007@gmail.com>
2023-05-31 11:38:35 +0300

    origin develop

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,74 @@
+
+type Shape = 'triangle' | 'circle' | 'rectangle';
+type Color = 'red' | 'green' | 'blue';
+
 export interface Figure {
+  shape: Shape,
+  color: Color,
 
+  getArea(): number,
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  shape: Shape = 'triangle';
 
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    const [shortSide1, shortSide2, longSide] = [a, b, c].sort((f, s) => f - s);
+
+    if (a <= 0 || b <= 0 || c <= 0 || longSide >= (shortSide1 + shortSide2)) {
+      throw new Error('Incorrect side length');
+    }
+  }
+
+  getArea(): number {
+    const p = (this.a + this.b + this.c) / 2;
+    const area = Math.floor((Math.sqrt(p * (p - this.a)
+    * (p - this.b) * (p - this.c))) * 100) / 100;
+
+    return area;
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  shape: Shape = 'circle';
 
+  constructor(
+    public color: Color,
+    public r: number,
+  ) {
+    if (r <= 0) {
+      throw new Error('Incorrect radius length');
+    }
+  }
+
+  getArea():number {
+    return Math.floor((Math.PI * this.r ** 2) * 100) / 100;
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  shape: Shape = 'rectangle';
 
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+  ) {
+    if (a <= 0 || b <= 0) {
+      throw new Error('Incorrect side length');
+    }
+  }
+
+  getArea():number {
+    return Math.floor((this.a * this.b) * 100) / 100;
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
