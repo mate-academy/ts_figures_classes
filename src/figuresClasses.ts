a160a7f80005f2ce55bc796277192c835274e2e1
Yehor Myronov <2726048@gmail.com>
2023-06-01 18:22:37 +0300

    add solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,80 @@
+type Shape = 'triangle' | 'circle' | 'rectangle';
+type Color = 'red' | 'green' | 'blue';
+
 export interface Figure {
-
+  shape: Shape;
+  color: Color;
+  getArea(): number;
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  public shape: Shape = 'triangle';
 
+  constructor(
+    public color: Color,
+    public sideA: number,
+    public sideB: number,
+    public sideC: number,
+  ) {
+    if (sideA <= 0 || sideB <= 0 || sideC <= 0) {
+      throw new Error('No one side cannot be less than or equal to 0');
+    }
+
+    const sides: number[] = [sideA, sideB, sideC].sort(
+      (current, next) => next - current,
+    );
+
+    if (sides[0] >= sides[1] + sides[2]) {
+      throw new Error(`sides ${sideA}, `
+        + `${sideB} and ${sideC} can't form a triangle`);
+    }
+  }
+
+  getArea(): number {
+    const p: number = (this.sideA + this.sideB + this.sideC) / 2;
+
+    return Math.floor(
+      Math.sqrt(p * (p - this.sideA) * (p - this.sideB) * (p - this.sideC))
+      * 100,
+    ) / 100;
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  public shape: Shape = 'circle';
 
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('Radius can\'t be less than or equal to 0');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor((Math.PI * this.radius ** 2) * 100) / 100;
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  public shape: Shape ='rectangle';
 
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('No one side cannot be less than or equal to 0');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor((this.width * this.height) * 100) / 100;
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
