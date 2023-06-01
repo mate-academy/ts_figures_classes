35b18a892875406a4307cdc6c2d10c4bc25eabab
Eugene Sherepa <evgenijserepa@gmail.com>
2023-06-01 09:55:13 +0300

    add task solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,90 @@
+export enum Shape {
+  Rectangle = 'rectangle',
+  Triangle = 'triangle',
+  Circle = 'circle',
+}
+
+export type Color = 'red' | 'green' | 'blue';
+
 export interface Figure {
+  shape: Shape;
+  color: Color;
 
-}
-
-export class Triangle {
-
-}
-
-export class Circle {
-
+  getArea(): number;
 }
 
 export class Rectangle {
+  shape = Shape.Rectangle;
 
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
+    if (this.width <= 0 || this.height <= 0) {
+      throw new Error('Unexpcted side length');
+    }
+  }
+
+  getArea(): number {
+    return this.width * this.height;
+  }
 }
 
-export function getInfo(figure) {
+export class Triangle {
+  shape = Shape.Triangle;
 
+  constructor(
+    public color: Color,
+    public sideA: number,
+    public sideB: number,
+    public sideC: number,
+  ) {
+    if (this.sideA <= 0 || this.sideB <= 0 || this.sideC <= 0) {
+      throw new Error('Unexpcted side length');
+    }
+
+    if (
+      this.sideA + this.sideB <= this.sideC
+      || this.sideA + this.sideC <= this.sideB
+      || this.sideB + this.sideC <= this.sideA
+    ) {
+      throw new Error(
+        'Any of the triangle`s side must be smaller than the sum of two other',
+      );
+    }
+  }
+
+  getArea(): number {
+    const semiPerimeter = (this.sideA + this.sideB + this.sideC) / 2;
+    const area = Math.sqrt(
+      semiPerimeter
+      * (semiPerimeter - this.sideA)
+      * (semiPerimeter - this.sideB)
+      * (semiPerimeter - this.sideC),
+    );
+
+    return Math.floor(area * 100) / 100;
+  }
+}
+
+export class Circle {
+  shape = Shape.Circle;
+
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (this.radius <= 0) {
+      throw new Error('Unexpcted radius');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor((Math.PI * this.radius ** 2) * 100) / 100;
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
