de50b5710bf8554e625a6d643b82b58eee43a4a0
Elijah Vinkov <illia.vinkov@gmail.com>
2023-06-05 14:02:35 +0200

    solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -2,80 +2,65 @@ type Shape = 'triangle' | 'circle' | 'rectangle';
 type Color = 'red' | 'green' | 'blue';
 
 export interface Figure {
-  shape: Shape,
-  color: Color,
+  shape: Shape;
+  color: Color;
   getArea: () => number;
 }
 
 export class Triangle implements Figure {
   public shape: Shape = 'triangle';
 
-  constructor(
-    public color: 'red' | 'green' | 'blue',
-    public a: number,
-    public b: number,
-    public c: number,
-  ) {
+  constructor(public color: Color,
+    public a: number, public b: number, public c: number) {
     if (a <= 0 || b <= 0 || c <= 0) {
-      throw new Error('One of the figure\'s sides less or 0');
+      throw new Error("One of the figure's sides is less than or equal to 0");
     }
 
-    if (Math.max(a, b, c) >= (a + b + c) - Math.max(a, b, c)) {
-      throw new Error(
-        `throws an error: sides ${a}, ${b} and ${c} can't form a triangle`,
-      );
+    if (Math.max(a, b, c) >= a + b + c - Math.max(a, b, c)) {
+      throw new Error(`Sides ${a}, ${b}, and ${c} cannot form a triangle`);
     }
   }
 
   getArea(): number {
-    const { a, b, c } = this;
-    const semiperimeter = (a + b + c) / 2;
-    const area = Math.sqrt(semiperimeter * (
-      semiperimeter - a) * (semiperimeter - b) * (semiperimeter - c));
-    const aroundArea = (Math.floor(area * 100)) / 100;
+    const semiPerimeter = (this.a + this.b + this.c) / 2;
+    const area = Math.sqrt(
+      semiPerimeter * ((semiPerimeter - this.a)
+      * (semiPerimeter - this.b) * (semiPerimeter - this.c)),
+    );
+    const aroundArea = Math.floor(area * 100) / 100;
 
     return aroundArea;
   }
 }
 
-export class Circle {
+export class Circle implements Figure {
   public shape: Shape = 'circle';
 
-  constructor(
-    public color: 'red' | 'green' | 'blue',
-    public a: number,
-  ) {
+  constructor(public color: Color, public a: number) {
     if (a <= 0) {
-      throw new Error('One of the figure\'s sides less or 0');
+      throw new Error("One of the figure's sides is less than or equal to 0");
     }
   }
 
   getArea(): number {
-    const { a } = this;
-    const area = (a * a * Math.PI);
-    const aroundArea = (Math.floor(area * 100)) / 100;
+    const area = this.a * this.a * Math.PI;
+    const aroundArea = Math.floor(area * 100) / 100;
 
     return aroundArea;
   }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
   public shape: Shape = 'rectangle';
 
-  constructor(
-    public color: 'red' | 'green' | 'blue',
-    public a: number,
-    public b: number,
-  ) {
+  constructor(public color: Color, public a: number, public b: number) {
     if (a <= 0 || b <= 0) {
-      throw new Error('One of the figure\'s sides less or 0');
+      throw new Error("One of the figure's sides is less than or equal to 0");
     }
   }
 
   getArea(): number {
-    const { a, b } = this;
-
-    return a * b;
+    return this.a * this.b;
   }
 }
 
