3d79beb4f82816dbd65cd05b78cf8d641e307f61
Marek Derlukiewicz <marek.derlukiewicz@gmail.com>
2023-06-13 10:20:07 +0200

    corrections for test (quotes, trailing commas, etc.)

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -11,15 +11,15 @@ export class Triangle implements Figure {
 
   constructor(color: string, a: number, b: number, c: number) {
     if (a <= 0 || b <= 0 || c <= 0) {
-      throw new Error("Invalid side length. Sides must be greater than 0.");
+      throw new Error('Invalid side length. Sides must be greater than 0.');
     }
     if (a + b <= c || a + c <= b || b + c <= a) {
       throw new Error(
-        "Invalid triangle. The sum of any two sides must be greater than the third side."
+        'Invalid triangle. Sum of any two sides must be > than third side.',
       );
     }
 
-    this.shape = "triangle";
+    this.shape = 'triangle';
     this.color = color;
     this.sides = [a, b, c];
   }
@@ -28,6 +28,7 @@ export class Triangle implements Figure {
     const [a, b, c] = this.sides;
     const s = (a + b + c) / 2;
     const area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+
     return Math.floor(area * 100) / 100;
   }
 }
@@ -39,10 +40,10 @@ export class Circle implements Figure {
 
   constructor(color: string, radius: number) {
     if (radius <= 0) {
-      throw new Error("Invalid radius. Radius must be greater than 0.");
+      throw new Error('Invalid radius. Radius must be greater than 0.');
     }
 
-    this.shape = "circle";
+    this.shape = 'circle';
     this.color = color;
     this.radius = radius;
   }
@@ -62,11 +63,11 @@ export class Rectangle implements Figure {
   constructor(color: string, width: number, height: number) {
     if (width <= 0 || height <= 0) {
       throw new Error(
-        "Invalid width or height. Width and height must be greater than 0."
+        'Invalid width or height. Width and height must be greater than 0.',
       );
     }
 
-    this.shape = "rectangle";
+    this.shape = 'rectangle';
     this.color = color;
     this.width = width;
     this.height = height;
