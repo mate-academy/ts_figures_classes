9838faa89b082974d5436ccfe4aa7d45f3bf683a
Amadeusz Lisiecki <amadeusz.lisiecki.1993@gmail.com>
2023-06-23 23:11:01 +0200

    Add figures

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,88 @@
+type Shape = 'triangle' | 'circle' | 'rectangle';
+type Color = 'red' | 'green' | 'blue';
+
 export interface Figure {
+  shape: Shape,
+  color: Color
 
+  getArea(): number
 }
 
-export class Triangle {
-
+function roundDown2(area: number): number {
+  return Math.floor(100 * area) / 100;
 }
 
-export class Circle {
-
+function handleLengthNotPositive(): void {
+  throw new Error('Length is not positive!');
 }
 
-export class Rectangle {
+export class Triangle implements Figure {
+  shape: Shape;
 
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    this.shape = 'triangle';
+
+    if (a <= 0 || b <= 0 || c <= 0) {
+      handleLengthNotPositive();
+    }
+
+    if (a + b <= c || a + c <= b || b + c <= a) {
+      throw new Error(`sided ${a}, ${b} and ${c} can't form a triangle`);
+    }
+  }
+
+  getArea(): number {
+    const s = (this.a + this.b + this.c) / 2;
+    const area = Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+
+    return roundDown2(area);
+  }
 }
 
-export function getInfo(figure) {
+export class Circle implements Figure {
+  shape: Shape;
 
+  constructor(
+    public color: Color,
+    public r: number,
+  ) {
+    this.shape = 'circle';
+
+    if (r <= 0) {
+      handleLengthNotPositive();
+    }
+  }
+
+  getArea(): number {
+    return roundDown2(this.r * this.r * Math.PI);
+  }
+}
+
+export class Rectangle implements Figure {
+  shape: Shape;
+
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
+    this.shape = 'rectangle';
+
+    if (width <= 0 || height <= 0) {
+      handleLengthNotPositive();
+    }
+  }
+
+  getArea(): number {
+    return roundDown2(this.width * this.height);
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
