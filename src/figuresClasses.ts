4c078f0963e254b01561daf819cba67072ba4739
Adrian Yevchuk <adrianyevchuk@MacBook-Air-Adrian.local>
2023-06-25 00:45:21 +0300

    solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,76 @@
-export interface Figure {
+type Shape = 'triangle' | 'circle' | 'rectangle';
+type Color = 'red' | 'green' | 'blue';
 
+export interface Figure{
+  shape: Shape,
+  color: Color,
+  getArea(): number,
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  public shape: Shape = 'triangle';
 
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('Side are not valid');
+    }
+
+    if (Math.max(a, b, c) >= (a + b + c)
+    - Math.max(a, b, c)) {
+      throw new Error('Side are not valid');
+    }
+  }
+
+  public getArea(): number {
+    const semiPerimetr = 0.5 * (this.a + this.b + this.c);
+
+    return +Math.sqrt(Math.abs(semiPerimetr
+      * (semiPerimetr - this.a)
+      * (semiPerimetr - this.b)
+      * (semiPerimetr - this.c))).toFixed(2);
+  }
 }
 
 export class Circle {
+  public shape: Shape = 'circle';
 
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('Radius must be not less 0 or equal 0');
+    }
+  }
+
+  getArea(): number {
+    return Math.trunc(((this.radius ** 2) * Math.PI) * 100) / 100;
+  }
 }
 
 export class Rectangle {
+  public shape: Shape = 'rectangle';
 
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+  ) {
+    if (a <= 0 || b <= 0) {
+      throw new Error('Side less 0 or equal 0');
+    }
+  }
+
+  getArea(): number {
+    return this.a * this.b;
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
