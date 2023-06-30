43da44cf937aa7094d0dd26dde074881fa576640
GGsmou <svyatoslav.golovin.2005@gmail.com>
2023-06-30 13:36:16 +0300

    change type to enum

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,4 +1,9 @@
-type Shape = 'triangle' | 'circle' | 'rectangle';
+enum Shape {
+  TRIANGLE = 'triangle',
+  CIRCLE = 'circle',
+  RECTANGLE = 'rectangle',
+}
+
 type Color = 'red' | 'green' | 'blue';
 
 export interface Figure {
@@ -8,7 +13,7 @@ export interface Figure {
 }
 
 export class Triangle implements Figure {
-  shape: Shape = 'triangle';
+  shape: Shape = Shape.TRIANGLE;
 
   constructor(
     public color: Color,
@@ -34,7 +39,7 @@ export class Triangle implements Figure {
 }
 
 export class Circle implements Figure {
-  shape: Shape = 'circle';
+  shape: Shape = Shape.CIRCLE;
 
   constructor(
     public color: Color,
@@ -54,7 +59,7 @@ export class Circle implements Figure {
 }
 
 export class Rectangle implements Figure {
-  shape: Shape = 'rectangle';
+  shape: Shape = Shape.RECTANGLE;
 
   constructor(
     public color: Color,
