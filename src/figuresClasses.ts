7e0f547fda9e22a76cd4466ed662959d9719bfdb
Vitalii Fedusov <vitalii.fedusov@gmail.com>
2023-07-01 18:31:34 +0300

    Solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,77 @@
+
 export interface Figure {
+  shape: string,
+  color: string,
 
+  getArea(): number;
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  shape: string = 'triangle';
 
+  constructor(
+    public color: string,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (this.a <= 0 || this.b <= 0 || this.c <= 0) {
+      throw new Error('sides are not positive');
+    }
+
+    const biggestSide = Math.max(this.a, this.b, this.c);
+
+    if (biggestSide >= this.a + this.b + this.c - biggestSide) {
+      throw new Error('it is not a triangle');
+    }
+  }
+
+  getArea(): number {
+    const s = (this.a + this.b + this.c) / 2;
+    const square = Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+
+    return Math.floor(square * 100) / 100;
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  shape: string = 'circle';
 
+  constructor(
+    public color: string,
+    public radius: number,
+  ) {
+    if (this.radius <= 0) {
+      throw new Error('radius must be positive');
+    }
+  }
+
+  getArea(): number {
+    const squareCircle = Math.PI * (this.radius ** 2);
+    const squareRounded = Math.floor(squareCircle * 100) / 100;
+
+    return squareRounded;
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  shape: string = 'rectangle';
 
+  constructor(
+    public color: string,
+    public a: number,
+    public b: number,
+  ) {
+    if (this.a <= 0 || this.b <= 0) {
+      throw new Error('incorrect side value');
+    }
+  }
+
+  getArea(): number {
+    return this.a * this.b;
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
