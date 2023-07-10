8f347b62fa040e24265216e22def07f26eee079c
Maksym Ptashnyk <super.zet65@gmail.com>
2023-07-10 22:54:24 +0300

    add task solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,69 @@
 export interface Figure {
-
+  color: 'red' | 'green' | 'blue';
+  shape: string;
+  getArea: () => number;
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  shape = 'triangle';
 
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    const maxSide = Math.max(a, b, c);
+    const summarySides = a + b + c;
+
+    if ([a, b, c].some((side) => side <= 0)
+    || (summarySides - maxSide < maxSide)) {
+      throw new Error('A triangle with such sides cannot exist.');
+    }
+  }
+
+  getArea(): number {
+    const halfPerimeter = (this.a + this.b + this.c) / 2;
+
+    return Number((Math.sqrt(halfPerimeter * (halfPerimeter - this.a)
+    * (halfPerimeter - this.b) * (halfPerimeter - this.c))).toFixed(2));
+  }
+}
+export class Circle implements Figure {
+  shape = 'circle';
+
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('The radius must be greater than 0');
+    }
+  }
+
+  getArea(): number {
+    return Number((Math.PI * (this.radius ** 2)).toFixed(2));
+  }
 }
 
-export class Circle {
+export class Rectangle implements Figure {
+  shape = 'rectangle';
 
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public width: number,
+    public height: number,
+  ) {
+    if ([width, height].some((side) => side <= 0)) {
+      throw new Error('All sides must be more than 0');
+    }
+  }
+
+  getArea(): number {
+    return Number((this.width * this.height).toFixed(2));
+  }
 }
 
-export class Rectangle {
-
-}
-
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
