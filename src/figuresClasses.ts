6059f7c03d88cdd9f50c68497aa666ff9ec2f570
Hanna Anishyna <ann.986@gmail.com>
2023-08-01 14:47:28 -0700

    fixes

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,28 +1,26 @@
-enum EnumShape {
+enum SHAPE {
   triangle = 'triangle',
   circle = 'circle',
   rectangle = 'rectangle',
 }
 
-enum EnumColor {
+enum COLOR {
   red = 'red',
   green = 'green',
   blue = 'blue',
 }
 
 export interface Figure {
-  shape: EnumShape,
-  color: EnumColor,
+  shape: SHAPE,
+  color: COLOR,
   getArea(): number
 }
 
 export class Triangle implements Figure {
-  shape = EnumShape.triangle;
-
-  area!: number;
+  shape = SHAPE.triangle;
 
   constructor(
-    public color: EnumColor,
+    public color: COLOR,
     public a: number,
     public b: number,
     public c: number,
@@ -30,75 +28,71 @@ export class Triangle implements Figure {
     this.color = color;
 
     if (this.a <= 0 || this.b <= 0 || this.c <= 0) {
-      throw new Error('Error');
+      throw new Error('Error: Sides can`t be less that 1');
     }
 
     const maxSide = Math.max(a, b, c);
     const sumOfSides = a + b + c - maxSide;
 
     if (maxSide >= sumOfSides) {
-      throw new Error('Error');
+      throw new Error('Error: One side can`t be bigger than sum of two sides');
     }
   }
 
   getArea(): number {
     const s = (this.a + this.b + this.c) / 2;
-    const area = Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+    let area = Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
 
-    this.area = Math.floor(area * 100) / 100;
+    area = Math.floor(area * 100) / 100;
 
-    return this.area;
+    return area;
   }
 }
 
 export class Circle implements Figure {
-  shape = EnumShape.circle;
-
-  area!: number;
+  shape = SHAPE.circle;
 
   constructor(
-    public color: EnumColor,
+    public color: COLOR,
     public radius: number,
   ) {
     this.color = color;
 
     if (this.radius <= 0) {
-      throw new Error('Error');
+      throw new Error('Error: The radius can`t be less than 1');
     }
   }
 
   getArea(): number {
-    const area = Math.PI * this.radius * this.radius;
+    let area = Math.PI * this.radius * this.radius;
 
-    this.area = Math.floor(area * 100) / 100;
+    area = Math.floor(area * 100) / 100;
 
-    return this.area;
+    return area;
   }
 }
 
 export class Rectangle {
-  shape = EnumShape.rectangle;
-
-  area!: number;
+  shape = SHAPE.rectangle;
 
   constructor(
-    public color: EnumColor,
+    public color: COLOR,
     public width: number,
     public height: number,
   ) {
     this.color = color;
 
     if (this.height <= 0 || this.width <= 0) {
-      throw new Error('Error');
+      throw new Error('Error: Sides can`t be less that 1');
     }
   }
 
   getArea(): number {
-    const area = this.height * this.width;
+    let area = this.height * this.width;
 
-    this.area = Math.floor(area * 100) / 100;
+    area = Math.floor(area * 100) / 100;
 
-    return this.area;
+    return area;
   }
 }
 
