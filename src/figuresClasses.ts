8f4eee04aa5786d0329bb7cd88aa4b508307dad0
Karolina <karolinabednarek888@gmail.com>
2023-08-07 13:28:22 +0200

    Solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,79 @@
+enum Shape {
+  Triangle = 'triangle',
+  Circle = 'circle',
+  Rectangle = 'rectangle'
+}
+
+type Color = 'blue' | 'red' | 'green';
+
 export interface Figure {
-
+  shape: string,
+  color: Color,
+  getArea(): number
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  public shape = Shape.Triangle;
 
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0
+      || ((a + b <= c) || (b + c <= a) || (a + c <= b))) {
+      throw new Error('Wrong params for a Triangle');
+    }
+  }
+
+  getArea(): number {
+    const p = (this.a + this.b + this.c) / 2;
+    const square = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+
+    return Math.floor(square * 100) / 100;
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  public shape = Shape.Circle;
 
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('You can not find square of circle');
+    }
+  }
+
+  getArea(): number {
+    const square = Math.PI * this.radius * this.radius;
+
+    return Math.floor(square * 100) / 100;
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  public shape = Shape.Rectangle;
 
+  constructor(
+    public color: Color,
+    public height: number,
+    public width: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('Invalid params');
+    }
+  }
+
+  getArea(): number {
+    const square = this.width * this.height;
+
+    return Math.floor(square * 100) / 100;
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
