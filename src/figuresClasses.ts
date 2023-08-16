a547ad0b99c2df06f01a22203c7eebc7b0485145
Andrzej Pec <pimatstudio@gmail.com>
2023-08-16 16:36:37 +0200

    Test pass commet

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,94 +1,119 @@
-// # Figures
-// We have 3 types of figures: triangles, circles and rectangles.
-
-// Write an interface `Figure` and 3 classes
-// implementing it so that every figure
-// has:
-// - a `shape` (`triangle`, `circle` or `rectangle`);
-// - a `color` (`red`, `green` or `blue`);
-// - a method `getArea` that returns the area of the figure rounded down to
-// hundredths.
-
-// In addition to a `color` constructors should accept required data:
-// - sides `a`, `b` and `c` for a triangle;
-// - a `radius` for a circle;
-// - a `width` and a `height` for a rectangle.
-
-// The constructors should `throw new Error('your error message')` if:
-// - any length is <= 0
-// - the longest side of a triangle is >= than a sum of two others
-
-// Hints:
-// - use `Math.PI` for calculating a circle square
-// - use [Heron's formula]
-// (://en.wikipedia.org/wiki/Heron%27s_formula) for triangles
-
-// Example:
-// ```typescript
-// new Rectangle('blue', 2, 0) // throws an error
-// new Triangle('red', 1, 2, 3) //
-// throws an error: sides 1, 2 and 3 can't form a triangle
-// ```
-
-// Also create a function `getInfo` that
-// takes a figure and returns a string in the
-// next format:
-// ```typescript
-// const redRectangle = new Rectangle('red', 3, 5);
-// getInfo(redRectangle) === 'A red rectangle - 15';
-
-// const greenCircle = new Circle('green', 1);
-// getInfo(greenCircle) === 'A green circle - 3.14';
-// ```
 type Shape = 'triangle' | 'circle' | 'rectangle';
 type Color = 'red' | 'green' | 'blue';
 
-// function roundAreaToThousands(area) {
-//   return Math.round(area * 1000) / 1000;
-// }
+function roundAreaToHundreths(area: number): number {
+  return Math.floor(area * 100) / 100;
+}
+
 export interface Figure {
+  getInfo(): string;
   shape: Shape;
   color: Color;
-  getArea(): number;
 }
 
-export interface Triangle extends Figure {
+export class Triangle implements Figure {
+  shape: Shape = 'triangle';
+
+  color: Color;
+
   a: number;
+
   b: number;
+
   c: number;
-  getArea(): number;
-  getInfo(): string;
+
+  constructor(color: Color, a: number, b: number, c: number) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new
+      Error('Verily, the sides ought to be numbers of positive countenance.');
+    }
+
+    const sides = [a, b, c].sort((x, y) => x - y);
+
+    if (sides[2] >= sides[0] + sides[1]) {
+      throw new
+      Error(`Verily, the sides ${a}, ${b} and ${c} dost not make a triangle`);
+    }
+
+    this.color = color;
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+
+  getArea(): number {
+    const p: number = (this.a + this.b + this.c) / 2;
+
+    const area: number = Math
+      .sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+
+    return roundAreaToHundreths(area);
+  }
+
+  getInfo(): string {
+    return `A ${this.color} ${this.shape} - ${this.getArea()}`;
+  }
 }
 
-export interface Circle extends Figure {
+export class Rectangle implements Figure {
+  shape: Shape = 'rectangle';
+
+  color: Color;
+
+  width: number;
+
+  height: number;
+
+  constructor(color: Color, width: number, height: number) {
+    if (width <= 0 || height <= 0) {
+      throw new
+      Error('Forsooth, width and height must be numbers of positive stature.');
+    }
+
+    this.color = color;
+    this.width = width;
+    this.height = height;
+  }
+
+  getArea(): number {
+    const area: number = this.width * this.height;
+
+    return roundAreaToHundreths(area);
+  }
+
+  getInfo(): string {
+    return `A ${this.color} ${this.shape} - ${this.getArea()}`;
+  }
+}
+
+export class Circle implements Figure {
+  shape: Shape = 'circle';
+
+  color: Color;
+
   radius: number;
-  getArea(): number;
-  getInfo(): string;
+
+  constructor(color: Color, radius: number) {
+    if (radius <= 0) {
+      throw new
+      Error('Verily, the radius ought to be a number of positive countenance.');
+    }
+
+    this.color = color;
+    this.radius = radius;
+  }
+
+  getArea(): number {
+    const area: number = Math.PI * (this.radius ** 2);
+
+    return roundAreaToHundreths(area);
+  }
+
+  getInfo(): string {
+    return `A ${this.color} ${this.shape} - ${this.getArea()}`;
+  }
 }
 
-export interface Rectangle extends Figure {
-  a: number;
-  b: number;
-  getArea(): number;
-  getInfo(): string;
-}
-
-// export function createFigure(type, color, sideA, sideB): void {
-//   type;
-// }
-
-// export class Triangle {
-
-// }
-
-// export class Circle {
-
-// }
-
-// export class Rectangle {
-
-// }
-
-export function getInfo(figure: Figure): Figure {
-  return figure;
+export function getInfo(figure: Figure): string {
+  return figure.getInfo();
 }
