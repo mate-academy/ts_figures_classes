726f9efcfdc765c96e6b3272b841004aa51d6fbd
Klaudia <kklis@interia.pl>
2023-08-16 14:17:36 +0200

    Make requested changes

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -8,18 +8,14 @@ export interface Figure {
 }
 
 export class Triangle implements Figure {
-  shape: Shape;
-
-  getArea: () => number;
+  shape: Figure['shape'] = 'triangle';
 
   constructor(
-    public color: Color,
+    public color: Figure['color'],
     public a: number,
     public b: number,
     public c: number,
   ) {
-    this.shape = 'triangle';
-
     if (a <= 0 || b <= 0 || c <= 0) {
       throw new Error('Side\'s length has to be greater than 0');
     } else if (
@@ -30,56 +26,48 @@ export class Triangle implements Figure {
       throw new Error('The longes side of a triangle cannot be'
         + 'greater or equal than a sum of two others');
     }
+  }
 
-    this.getArea = function getArea(): number {
-      const semiperimeter = 0.5 * (this.a + this.b + this.c);
+  getArea(): number {
+    const semiperimeter = 0.5 * (this.a + this.b + this.c);
 
-      return +Math.sqrt(semiperimeter * (semiperimeter - this.a)
-        * (semiperimeter - this.b) * (semiperimeter - this.c)).toFixed(2);
-    };
+    return +Math.sqrt(semiperimeter * (semiperimeter - this.a)
+      * (semiperimeter - this.b) * (semiperimeter - this.c)).toFixed(2);
   }
 }
 
 export class Circle implements Figure {
-  shape: Shape;
-
-  getArea: () => number;
+  shape: Figure['shape'] = 'circle';
 
   constructor(
-    public color: Color,
+    public color: Figure['color'],
     public radius: number,
   ) {
-    this.shape = 'circle';
-
     if (radius <= 0) {
       throw new Error('Radiuds has to be greater than 0');
     }
+  }
 
-    this.getArea = function getArea(): number {
-      return Math.floor((Math.PI * radius ** 2) * 100) / 100;
-    };
+  getArea(): number {
+    return Math.floor((Math.PI * this.radius ** 2) * 100) / 100;
   }
 }
 
 export class Rectangle implements Figure {
-  shape: Shape;
-
-  getArea: () => number;
+  shape: Figure['shape'] = 'rectangle';
 
   constructor(
-    public color: Color,
+    public color: Figure['color'],
     public width: number,
     public height: number,
   ) {
-    this.shape = 'rectangle';
-
     if (width <= 0 || height <= 0) {
       throw new Error('Width and height has to be greater than 0');
     }
+  }
 
-    this.getArea = function getArea(): number {
-      return +(this.width * this.height).toFixed(2);
-    };
+  getArea(): number {
+    return +(this.width * this.height).toFixed(2);
   }
 }
 
