57523dcfdc0bf7c6d8766e9bac830c98e37ec710
Ivan Krylo <ivan.poltaven@gmail.com>
2023-08-16 14:54:38 +0300

    solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,81 @@
-export interface Figure {
+type Shape = 'triangle' | 'circle' | 'rectangle';
+type Color = 'red' | 'green' | 'blue';
 
+export interface Figure {
+  shape: Shape
+  color: Color
+  getArea(): number,
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+    public shape: Shape = 'triangle',
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('message');
+    }
 
+    let hypotenuse = a;
+
+    if (hypotenuse < b) {
+      hypotenuse = b;
+    }
+
+    if (hypotenuse < c) {
+      hypotenuse = c;
+    }
+
+    if (hypotenuse >= (a + b + c - hypotenuse)) {
+      throw new Error('sides 1, 2 and 3 can`t form a triangle');
+    }
+  }
+
+  getArea(): number {
+    const s = (this.a + this.b + this.c) / 2;
+
+    return Math.floor(Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c))
+      * 100)
+      / 100;
+  }
 }
 
 export class Circle {
+  constructor(
+    public color: Color,
+    public radius: number,
+    public shape: Shape = 'circle',
+  ) {
+    if (radius <= 0) {
+      throw new Error('message');
+    }
+  }
 
+  getArea(): number {
+    return Math.floor(Math.PI * (this.radius ** 2) * 100) / 100;
+  }
 }
 
 export class Rectangle {
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+    public shape: Shape = 'rectangle',
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('message');
+    }
+  }
 
+  getArea(): number {
+    return Math.floor(this.width * this.height * 100) / 100;
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
