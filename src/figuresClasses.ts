9f609f61468fcadfbee6dcbec08c9f4cd55598f9
arturwisnia <wisnia3008@interia.pl>
2023-08-19 16:13:39 +0200

    add task solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,46 +1,46 @@
+enum Color {
+  Red = 'red',
+  Green = 'green',
+  Blue = 'blue',
+}
+
+enum Shape {
+  Triangle = 'triangle',
+  Circle = 'circle',
+  Rectangle = 'rectangle',
+}
+
 export interface Figure {
-  shape: string;
-  color: string;
+  shape: Shape;
+  color: Color;
   getArea(): number;
 }
 
 export class Triangle implements Figure {
-  shape = 'triangle';
+  shape: Shape = Shape.Triangle;
 
-  color: string;
-
-  sides: number[];
-
-  constructor(color: string, a: number, b: number, c: number) {
+  constructor(public color: Color,
+    public a: number, public b: number, public c: number) {
     if (a <= 0 || b <= 0 || c <= 0 || a + b <= c || a + c <= b || b + c <= a) {
-      throw new Error('Invalid sides for a triangle');
+      throw new Error('Invalid triangle sides');
     }
-
-    this.color = color;
-    this.sides = [a, b, c];
   }
 
   getArea(): number {
-    const [a, b, c] = this.sides;
-    const s = (a + b + c) / 2;
+    const s = (this.a + this.b + this.c) / 2;
 
-    return +(Math.sqrt(s * (s - a) * (s - b) * (s - c))).toFixed(2);
+    return +(Math.sqrt(s * (s - this.a)
+     * (s - this.b) * (s - this.c))).toFixed(2);
   }
 }
 
 export class Circle implements Figure {
-  shape = 'circle';
+  shape: Shape = Shape.Circle;
 
-  color: string;
-
-  radius: number;
-
-  constructor(color: string, radius: number) {
+  constructor(public color: Color, public radius: number) {
     if (radius <= 0) {
-      throw new Error('Invalid radius for a circle');
+      throw new Error('Invalid circle radius');
     }
-    this.color = color;
-    this.radius = radius;
   }
 
   getArea(): number {
@@ -49,21 +49,13 @@ export class Circle implements Figure {
 }
 
 export class Rectangle implements Figure {
-  shape = 'rectangle';
+  shape: Shape = Shape.Rectangle;
 
-  color: string;
-
-  width: number;
-
-  height: number;
-
-  constructor(color: string, width: number, height: number) {
+  constructor(public color: Color,
+    public width: number, public height: number) {
     if (width <= 0 || height <= 0) {
-      throw new Error('Invalid dimensions for a rectangle');
+      throw new Error('Invalid rectangle dimensions');
     }
-    this.color = color;
-    this.width = width;
-    this.height = height;
   }
 
   getArea(): number {
@@ -72,6 +64,91 @@ export class Rectangle implements Figure {
 }
 
 export function getInfo(figure: Figure): string {
-  return `A ${figure.color} ${figure.shape}`
-  + ` - ${figure.getArea()}`;
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
+
+// enum Color {
+//   Red = 'red',
+//   Green = 'green',
+//   Blue = 'blue',
+// }
+
+// enum Shape {
+//   Triangle = 'triangle',
+//   Circle = 'circle',
+//   Rectangle = 'rectangle',
+// }
+
+// export interface Figure {
+//   shape: Shape;
+//   color: Color;
+//   getArea(): number;
+// }
+
+// export class Triangle implements Figure {
+//   shape: Shape = Shape.Circle;
+
+//   constructor(public color: Color, public radius: number) {
+//   if (a <= 0 || b <= 0 || c <= 0 || a + b <= c || a + c <= b || b + c <= a) {
+//       throw new Error('Invalid sides for a triangle');
+//     }
+
+//     this.color = color;
+//     this.sides = [a, b, c];
+//   }
+
+//   getArea(): number {
+//     const [a, b, c] = this.sides;
+//     const s = (a + b + c) / 2;
+
+//     return +(Math.sqrt(s * (s - a) * (s - b) * (s - c))).toFixed(2);
+//   }
+// }
+
+// export class Circle implements Figure {
+//   shape = 'circle';
+
+//   color: string;
+
+//   radius: number;
+
+//   constructor(color: string, radius: number) {
+//     if (radius <= 0) {
+//       throw new Error('Invalid radius for a circle');
+//     }
+//     this.color = color;
+//     this.radius = radius;
+//   }
+
+//   getArea(): number {
+//     return Math.floor((Math.PI * this.radius * this.radius) * 100) / 100;
+//   }
+// }
+
+// export class Rectangle implements Figure {
+//   shape = 'rectangle';
+
+//   color: string;
+
+//   width: number;
+
+//   height: number;
+
+//   constructor(color: string, width: number, height: number) {
+//     if (width <= 0 || height <= 0) {
+//       throw new Error('Invalid dimensions for a rectangle');
+//     }
+//     this.color = color;
+//     this.width = width;
+//     this.height = height;
+//   }
+
+//   getArea(): number {
+//     return +(this.width * this.height).toFixed(2);
+//   }
+// }
+
+// export function getInfo(figure: Figure): string {
+//   return `A ${figure.color} ${figure.shape}`
+//   + ` - ${figure.getArea()}`;
+// }
