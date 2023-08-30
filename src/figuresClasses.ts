58b81cd2b8d54d2360e52a2975f04a2d9cb1032f
Oleg Tarasov <tarasov.oleg.9779@gmail.com>
2023-08-30 16:41:42 +0300

    fugires classes v1.1

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,25 +1,29 @@
+type Color = 'red' | 'green' | 'blue';
+
+enum Shape {
+  Triangle = 'triangle',
+  Circle = 'circle',
+  Rectangle = 'rectangle'
+}
+
 export interface Figure {
-  shape: string,
-  color: string,
+  shape: Shape,
+  color: Color,
   getArea(): number,
 }
 
 export class Triangle implements Figure {
-  shape: string = 'triangle';
+  shape: Shape = Shape.Triangle;
 
-  color: string;
-
-  a:number;
-
-  b:number;
-
-  c:number;
-
-  constructor(color: string, a: number, b: number, c: number) {
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
     if (a <= 0 || b <= 0 || c <= 0 || a + b <= c || a + c <= b || b + c <= a) {
       throw new Error('Invalid triangle sides');
     }
-    this.color = color;
     this.a = a;
     this.b = b;
     this.c = c;
@@ -34,13 +38,16 @@ export class Triangle implements Figure {
 }
 
 export class Circle implements Figure {
-  shape: string = 'circle';
+  shape: Shape = Shape.Circle;
 
-  color: string;
+  color: Color;
 
   radius: number;
 
-  constructor(color: string, radius: number) {
+  constructor(
+    color: Color,
+    radius: number,
+  ) {
     if (radius <= 0) {
       throw new Error('Invalid circle radius');
     }
@@ -56,21 +63,18 @@ export class Circle implements Figure {
 }
 
 export class Rectangle implements Figure {
-  shape: string = 'rectangle';
+  shape: Shape = Shape.Rectangle;
 
-  color: string;
-
-  width: number;
-
-  height: number;
-
-  constructor(color: string, width: number, height: number) {
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
     if (width <= 0 || height <= 0) {
       throw new Error('Invalid rectangle dimensions');
     }
     this.color = color;
     this.width = width;
-    this.height = height;
   }
 
   getArea(): number {
