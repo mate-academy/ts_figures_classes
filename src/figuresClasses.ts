eecf6bb66d5a30c128050a2aedd2f35a7b82cf1c
Dmytro Levchenko <levchenkod91@gmail.com>
2023-08-31 16:49:46 +0300

    solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,72 @@
+type Color = 'red' | 'green' | 'blue';
+type Shape = 'triangle' | 'circle' | 'rectangle';
+
 export interface Figure {
-
+  color: Color,
+  shape: Shape,
+  getArea(): number,
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  shape: Shape = 'triangle';
 
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0
+      || a + b + c - Math.max(a, b, c) <= Math.max(a, b, c)
+    ) {
+      throw new Error('Wrong dimensions');
+    }
+  }
+
+  getArea(): number {
+    const { a, b, c } = this;
+    const semiP = (a + b + c) / 2;
+    const area = Math.sqrt(semiP * (semiP - a) * (semiP - b) * (semiP - c));
+
+    return Math.floor(area * 100) / 100;
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  shape: Shape = 'circle';
 
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('Radius cannot be negative or 0');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor(Math.PI * this.radius ** 2 * 100) / 100;
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  shape: Shape = 'rectangle';
 
+  constructor(
+    public color: Color,
+    public sideA: number,
+    public sideB: number,
+  ) {
+    if (Math.min(sideA, sideB) <= 0) {
+      throw new Error('Side cannot be negative or 0');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor(this.sideA * this.sideB * 100) / 100;
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
