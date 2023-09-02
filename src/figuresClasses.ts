2b41dc49a1d7f77b884ff8248d56aaa1e8fc5f92
Taras Voievoda <voevoda.taras@gmail.com>
2023-09-02 18:12:10 +0300

    added functionality for classes

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,84 @@
+enum Shape {
+  Triangle = 'triangle',
+  Circle = 'circle',
+  Rectangle = 'rectangle',
+}
+
+type Color = 'red' | 'green' | 'blue';
+
+const ERROR_MESSAGE = 'Invalid values for this figure';
+
 export interface Figure {
+  shape: Shape;
+  color: Color;
 
+  getArea(): number;
+}
+export class Triangle implements Figure {
+  shape: Shape = Shape.Triangle;
+
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (a + b <= c || a + c <= b || c + b <= a) {
+      throw new Error(ERROR_MESSAGE);
+    }
+  }
+
+  getArea(): number {
+    const { a, b, c } = this;
+    const p = (a + b + c) / 2;
+    const square = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+
+    return Math.floor(square * 100) / 100;
+  }
 }
 
-export class Triangle {
+export class Circle implements Figure {
+  shape: Shape = Shape.Circle;
 
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error(ERROR_MESSAGE);
+    }
+  }
+
+  getArea(): number {
+    const { radius } = this;
+    const square = Math.PI * (radius ** 2);
+
+    return Math.floor(square * 100) / 100;
+  }
 }
 
-export class Circle {
+export class Rectangle implements Figure {
+  shape: Shape = Shape.Rectangle;
 
+  constructor(
+    public color: Color,
+    protected width: number,
+    protected height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error(ERROR_MESSAGE);
+    }
+  }
+
+  getArea(): number {
+    const { width, height } = this;
+
+    return width * height;
+  }
 }
 
-export class Rectangle {
+export function getInfo(figure: Figure): string {
+  const { color, shape } = figure;
 
-}
-
-export function getInfo(figure) {
-
+  return `A ${color} ${shape} - ${figure.getArea()}`;
 }
