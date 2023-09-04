e3fb27ab9f75e8861ed8695b26ca4b1bd97e5769
AndrMar1939 <advo737@gmail.com>
2023-09-04 15:24:00 +0300

    fix

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,4 +1,5 @@
-const ERROR_ANY_SIDE_BELOW_OR_IS_ZERO = 'Error, any side length is <= 0';
+const ERROR_ANY_SIDE_BELOW_OR_IS_ZERO
+  = 'Error, one of the sides (or radius) is less than or equal to zero';
 const ERROR_CANNOT_FORM_TRIANGLE
   = 'Error, the longest side of a triangle is >= than a sum of two others';
 
@@ -8,7 +9,11 @@ enum Shape {
   Rectangle = 'rectangle'
 }
 
-type Color = 'red' | 'green' | 'blue';
+enum Color {
+  Red = 'red',
+  Green = 'green',
+  Blue = 'blue'
+}
 
 export interface Figure {
   shape: Shape;
@@ -17,7 +22,7 @@ export interface Figure {
 }
 
 export class Triangle implements Figure {
-  public shape :Shape = Shape.Triangle;
+  public shape: Shape = Shape.Triangle;
 
   constructor(
     public color: Color,
@@ -36,28 +41,28 @@ export class Triangle implements Figure {
     }
   }
 
-  public getArea() :number {
+  public getArea(): number {
     const semiPerimeter = (this.a + this.b + this.c) / 2;
     const area = Math.sqrt(semiPerimeter * (semiPerimeter - this.a)
       * (semiPerimeter - this.b) * (semiPerimeter - this.c));
 
-    return Math.round(area * 100) / 100;
+    return Math.trunc(area * 100) / 100;
   }
 }
 
 export class Circle implements Figure {
-  public shape :Shape = Shape.Circle;
+  public shape: Shape = Shape.Circle;
 
   constructor(
     public color: Color,
     public a: number,
   ) {
     if (a <= 0) {
-      throw new Error(ERROR_CANNOT_FORM_TRIANGLE);
+      throw new Error(ERROR_ANY_SIDE_BELOW_OR_IS_ZERO);
     }
   }
 
-  public getArea() :number {
+  public getArea(): number {
     const area = Math.PI * this.a ** 2;
 
     return Math.trunc(area * 100) / 100;
@@ -65,7 +70,7 @@ export class Circle implements Figure {
 }
 
 export class Rectangle {
-  public shape :Shape = Shape.Rectangle;
+  public shape: Shape = Shape.Rectangle;
 
   constructor(
     public color: Color,
@@ -77,13 +82,13 @@ export class Rectangle {
     }
   }
 
-  public getArea() :number {
+  public getArea(): number {
     return this.a * this.b;
   }
 }
 
 export function getInfo(
-  figure :Triangle | Circle | Rectangle,
+  figure: Triangle | Circle | Rectangle,
 ) :string {
   return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
