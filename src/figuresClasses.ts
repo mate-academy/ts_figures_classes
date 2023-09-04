9078bbae8d10f88269da23b2f29ba5082efa9db1
KatOlista^^! <olya.katyushina@gmail.com>
2023-09-04 18:59:20 +0200

    implement 3 classes for figures

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,97 @@
+const errorMessage = {
+  wrongLength: 'length is <= 0',
+  tooLongSide: 'the longest side of a triangle is >= than a sum of two others',
+};
+
+const standardArea = (area: number): number => {
+  return Math.floor(area * 100) / 100;
+};
+
+enum Shape {
+  triangle = 'triangle',
+  circle = 'circle',
+  rectangle = 'rectangle',
+}
+
+enum Color {
+  red = 'red',
+  green = 'green',
+  blue = 'blue',
+}
+
 export interface Figure {
+  shape: Shape;
+  color: Color;
 
+  getArea(): number;
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  public shape: Shape = Shape.triangle;
 
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error(errorMessage.wrongLength);
+    }
+
+    if (a >= b + c
+      || b >= a + c
+      || c >= b + a) {
+      throw new Error(errorMessage.tooLongSide);
+    }
+  }
+
+  getArea(): number {
+    const semiperimeter = (this.a + this.b + this.c) / 2;
+    const area = Math.sqrt(semiperimeter
+        * (semiperimeter - this.a)
+        * (semiperimeter - this.b)
+        * (semiperimeter - this.c));
+
+    return standardArea(area);
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  public shape: Shape = Shape.circle;
 
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error(errorMessage.wrongLength);
+    }
+  }
+
+  getArea(): number {
+    return standardArea(Math.PI * (this.radius ** 2));
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  public shape: Shape = Shape.rectangle;
 
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error(errorMessage.wrongLength);
+    }
+  }
+
+  getArea(): number {
+    return standardArea(this.width * this.height);
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
