5c888c288218d395af84ef8431f1ab5def047f19
Tamara Chernetska <cty2802@gmail.com>
2023-09-05 21:05:15 +0300

    fixed a lot of tasks

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,5 +1,10 @@
 type Color = 'red' | 'green' | 'blue';
-type Shape = 'triangle' | 'circle' | 'rectangle';
+
+enum Shape {
+  Triangle = 'triangle',
+  Circle = 'circle',
+  Rectangle = 'rectangle',
+}
 
 export interface Figure {
   color: Color,
@@ -8,78 +13,75 @@ export interface Figure {
   getArea(): number,
 }
 
-function isCompare(a: number, b: number, c: number): boolean {
+function isTheBigestSide(a: number, b: number, c: number): boolean {
   return a + b <= c || b + c <= a || c + a <= b;
 }
 
-export class Triangle implements Figure {
-  shape = 'triangle' as Shape;
+function roundToHundredths(value: number):number {
+  return Math.round(value * 100) / 100;
+}
 
-  color: Color;
+export class Triangle implements Figure {
+  public shape: Shape = Shape.Triangle;
 
   constructor(
-    color: Color,
+    public color: Color,
     private a: number,
     private b: number,
     private c: number,
   ) {
-    this.color = color;
-
-    const isBigger = isCompare(this.a, this.b, this.c);
+    const isBigger = isTheBigestSide(this.a, this.b, this.c);
 
     if (this.a <= 0 || this.b <= 0 || this.c <= 0 || isBigger) {
-      throw new Error('Error -:)');
+      throw new Error('The longest side is greater than a sum of two others');
     }
   }
 
   getArea(): number {
-    const s: number = (this.a + this.b + this.c) / 2;
+    const semiPerimeter = (this.a + this.b + this.c) / 2;
+    const triangleArea = Math.sqrt(semiPerimeter * (semiPerimeter - this.a)
+    * (semiPerimeter - this.b) * (semiPerimeter - this.c));
 
-    return Math.round(Math.sqrt(s * (s - this.a)
-      * (s - this.b) * (s - this.c)) * 100) / 100;
+    return roundToHundredths(triangleArea);
   }
 }
 
 export class Circle implements Figure {
-  shape = 'circle' as Shape;
-
-  color: Color;
+  public shape: Shape = Shape.Circle;
 
   constructor(
-    color: Color,
+    public color: Color,
     private radius: number,
   ) {
-    this.color = color;
-
     if (this.radius <= 0) {
-      throw new Error('Error -:)');
+      throw new Error('Radius cannot be a negative number');
     }
   }
 
   getArea(): number {
-    return Math.floor(this.radius * this.radius * Math.PI * 100) / 100;
+    const circleArea = this.radius * this.radius * Math.PI;
+
+    return Math.floor(circleArea * 100) / 100;
   }
 }
 
 export class Rectangle implements Figure {
-  shape = 'rectangle' as Shape;
-
-  color: Color;
+  public shape: Shape = Shape.Rectangle;
 
   constructor(
-    color: Color,
+    public color: Color,
     private width: number,
     private height: number,
   ) {
-    this.color = color;
-
     if (this.width <= 0 || this.height <= 0) {
-      throw new Error('Error -:)');
+      throw new Error('Width or height cannot be a negative number');
     }
   }
 
   getArea(): number {
-    return Math.round(this.width * this.height * 100) / 100;
+    const rectangleArea = this.width * this.height;
+
+    return roundToHundredths(rectangleArea);
   }
 }
 
