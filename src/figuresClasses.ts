6aac467495196fdf4f1d8e2b3d2f3418b596534e
Vitalii Serhiichuk <vitalya.sergiychuk19999@gmail.com>
2023-09-06 17:55:37 +0300

    create a function roundArea, create enum Shape and enum Color

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,35 @@
+enum Shape {
+  Triangle = 'triangle',
+  Circle = 'circle',
+  Rectangle = 'rectangle',
+}
+
+enum Color {
+  Red = 'red',
+  Green = 'green',
+  Blue = 'blue',
+}
+
 export interface Figure {
-  shape: 'triangle' | 'circle' | 'rectangle',
-  color: 'red' | 'green' | 'blue',
+  shape: string,
+  color: string,
   getArea(): number,
 }
 
+function roundArea(area: number): number {
+  return Math.floor(area * 100) / 100;
+}
+
 export class Triangle implements Figure {
-  public shape: 'triangle';
+  public shape: Shape.Triangle;
 
   constructor(
-    public color: 'red' | 'green' | 'blue',
+    public color: Color,
     public a: number,
     public b: number,
     public c: number,
   ) {
-    this.shape = 'triangle';
+    this.shape = Shape.Triangle;
 
     if (!this.isValidTriangle()) {
       throw new Error('Invalid triangle sides');
@@ -29,23 +45,28 @@ export class Triangle implements Figure {
   }
 
   getArea(): number {
-    const s = (this.a + this.b + this.c) / 2;
+    const perimetrTriangle = (this.a + this.b + this.c) / 2;
 
     const areaTriangle = Math
-      .sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+      .sqrt(
+        perimetrTriangle
+        * (perimetrTriangle - this.a)
+        * (perimetrTriangle - this.b)
+        * (perimetrTriangle - this.c),
+      );
 
-    return +areaTriangle.toFixed(2);
+    return roundArea(areaTriangle);
   }
 }
 
 export class Circle implements Figure {
-  public shape: 'circle';
+  public shape: Shape.Circle;
 
   constructor(
-    public color: 'red' | 'green' | 'blue',
+    public color: Color,
     public radius: number,
   ) {
-    this.shape = 'circle';
+    this.shape = Shape.Circle;
 
     if (this.radius <= 0) {
       throw new Error('Invalid circle radius');
@@ -53,21 +74,21 @@ export class Circle implements Figure {
   }
 
   getArea(): number {
-    const areaCircle = Math.floor(Math.PI * this.radius ** 2 * 100) / 100;
+    const areaCircle = Math.PI * this.radius ** 2;
 
-    return areaCircle;
+    return roundArea(areaCircle);
   }
 }
 
 export class Rectangle implements Figure {
-  public shape: 'rectangle';
+  public shape: Shape.Rectangle;
 
   constructor(
-    public color: 'red' | 'green' | 'blue',
+    public color: Color,
     public width: number,
     public height: number,
   ) {
-    this.shape = 'rectangle';
+    this.shape = Shape.Rectangle;
 
     if (this.width <= 0 || this.height <= 0) {
       throw new Error('Invalid rectangle side');
@@ -75,12 +96,13 @@ export class Rectangle implements Figure {
   }
 
   getArea(): number {
-    const areaRectangle = this.width * this.height;
-
-    return areaRectangle;
+    return this.width * this.height;
   }
 }
 
 export function getInfo(figure: Figure): string {
-  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
+  const { color, shape } = figure;
+  const area = figure.getArea();
+
+  return `A ${color} ${shape} - ${area}`;
 }
