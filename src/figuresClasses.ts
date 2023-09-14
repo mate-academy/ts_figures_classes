76026d5b6dd2b2bf99058347ec65e4f5ccec625e
Kyryl Maslov <kyrylmaslov2007@gmail.com>
2023-09-14 15:35:24 +0300

    figure classes were fixed

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,17 +1,22 @@
+enum ShapeType {
+  Triangle = 'triangle',
+  Circle = 'circle',
+  Rectangle = 'rectangle',
+}
 export interface Figure {
-  shape: 'triangle' | 'circle' | 'rectangle',
+  shape: ShapeType,
   color: 'red' | 'green' | 'blue',
   area: number,
 }
 
-function HeronsFormula(a: number, b: number, c: number): number {
+function calculateArea(a: number, b: number, c: number): number {
   const s: number = 1 / 2 * (a + b + c);
 
   return Math.sqrt(s * (s - a) * (s - b) * (s - c));
 }
 
 export class Triangle implements Figure {
-  public readonly shape: 'triangle';
+  public readonly shape: ShapeType = ShapeType.Triangle;
 
   public readonly area: number;
 
@@ -21,9 +26,8 @@ export class Triangle implements Figure {
     b = 0,
     c = 0,
   ) {
-    this.shape = 'triangle';
     this.color = color;
-    this.area = HeronsFormula(a, b, c);
+    this.area = calculateArea(a, b, c);
 
     if (a <= 0 || b <= 0 || c <= 0
       || a >= b + c
@@ -39,7 +43,7 @@ export class Triangle implements Figure {
 }
 
 export class Circle implements Figure {
-  public readonly shape: 'circle';
+  public readonly shape: ShapeType = ShapeType.Circle;
 
   public readonly area: number;
 
@@ -47,7 +51,6 @@ export class Circle implements Figure {
     public color: Figure['color'],
     radius: number = 0,
   ) {
-    this.shape = 'circle';
     this.color = color;
     this.area = Math.PI * radius ** 2;
 
@@ -62,7 +65,7 @@ export class Circle implements Figure {
 }
 
 export class Rectangle {
-  public readonly shape: 'rectangle';
+  public readonly shape: ShapeType = ShapeType.Rectangle;
 
   public readonly area: number;
 
@@ -71,7 +74,6 @@ export class Rectangle {
     width: number,
     height: number,
   ) {
-    this.shape = 'rectangle';
     this.color = color;
     this.area = width * height;
 
