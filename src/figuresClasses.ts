e38ac5f17eafe39cd22ea1982cac4acd1b24491b
Olena Shchipina <shipina.olena@gmail.com>
2023-10-16 20:28:11 +0300

    solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,82 @@
+enum Shape {
+  Triangle = 'triangle',
+  Circle = 'circle',
+  Rectangle = 'rectangle'
+}
+
+enum Color{
+  Red = 'red',
+  Green = 'green',
+  Blue = 'blue',
+}
+
 export interface Figure {
-
+  shape: Shape;
+  color: Color;
+  getArea(): number;
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  shape: Shape = Shape.Triangle;
 
+  constructor(
+    public color: string,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('value cannot be negative');
+    }
+
+    if (a + b <= c || a + c <= b || c + b <= a) {
+      throw new Error('invalid value for triangle, we can`t build it.');
+    }
+  }
+
+  getArea(): number {
+    const { a, b, c } = this;
+    const p = (a + b + c) / 2;
+
+    return +(Math.sqrt(p * (p - a) * (p - b) * (p - c)).toFixed(2));
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  shape: Shape = Shape.Circle;
 
+  constructor(
+    public color: string,
+    public r: number,
+  ) {
+    if (r <= 0) {
+      throw new Error('value cannot be negative');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor(Math.PI * (this.r ** 2) * 100) / 100;
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  shape: Shape = Shape.Rectangle;
 
+  constructor(
+    public color: string,
+    public width: number,
+    public height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('value cannot be negative');
+    }
+  }
+
+  getArea(): number {
+    return this.width * this.height;
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure : Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
