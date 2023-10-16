80c64334d45d682fd5deab0e676202ee708a1468
Serhii Mykhailivskyi <sergeymih098123@gmail.com>
2023-10-16 17:07:09 +0300

    add task solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,81 @@
+type Shape = 'triangle' | 'circle' | 'rectangle';
+
 export interface Figure {
-
+  shape: Shape;
+  color: string;
+  getArea: Function;
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  public shape: Shape = 'triangle';
 
+  constructor(
+    public color: string,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    const maxSide = Math.max(a, b, c);
+    const sumOfTwoSides = (a + b + c) - maxSide;
+
+    if (sumOfTwoSides - maxSide <= 0) {
+      throw new Error(
+        `sides ${a}, ${b} and ${c} can't form a ${this.shape}`,
+      );
+    }
+
+    if (Math.min(a, b, c) <= 0) {
+      throw new Error('Cant form circle with side <= 0');
+    }
+  }
+
+  getArea(): number {
+    const p = (this.a + this.b + this.c) / 2;
+    const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+
+    return Math.floor(area * 100) / 100;
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  public shape: Shape = 'circle';
 
+  constructor(
+    public color: string,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('Cant form circle with radius <= 0');
+    }
+  }
+
+  getArea(): number {
+    const area = Math.PI * this.radius ** 2;
+
+    return Math.floor(area * 100) / 100;
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  public shape: Shape = 'rectangle';
 
+  constructor(
+    public color: string,
+    public width: number,
+    public height: number,
+  ) {
+    if (Math.min(width, height) <= 0) {
+      throw new Error('Cant form rectangle with sides <= 0');
+    }
+  }
+
+  getArea(): number {
+    const area = this.height * this.width;
+
+    return Math.floor(area * 100) / 100;
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
