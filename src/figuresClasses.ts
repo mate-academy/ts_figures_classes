a5ecd4558f282bafafac8909f0faec1add71b8d1
Anna Basenyuk <annabasenyuk@gmail.com>
2023-10-18 11:27:47 +0300

    add tack solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,79 @@
 export interface Figure {
-
+  shape: string;
+  color: string;
+  getArea(): number;
 }
 
-export class Triangle {
-
+enum Shape {
+  Triangle = 'triangle',
+  Circle = 'circle',
+  Rectangle = 'rectangle',
 }
 
-export class Circle {
+type Color = 'green' | 'red' | 'blue';
 
+export class Triangle implements Figure {
+  public shape = Shape.Triangle;
+
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0 || a + b <= c || a + c <= b || b + c <= a) {
+      throw new Error('Input data is incorrect');
+    }
+  }
+
+  getArea(): number {
+    const p: number = (this.a + this.b + this.c) / 2;
+    const s: number = +Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c))
+      .toFixed(2);
+
+    return s;
+  }
 }
 
-export class Rectangle {
+export class Circle implements Figure {
+  public shape = Shape.Circle;
 
+  constructor(
+    public color: Color,
+    public r: number,
+  ) {
+    if (r <= 0) {
+      throw new Error('Input data is incorrect');
+    }
+  }
+
+  getArea(): number {
+    const s: number = ((this.r ** 2) * Math.PI);
+
+    return Math.floor(s * 100) / 100;
+  }
 }
 
-export function getInfo(figure) {
+export class Rectangle implements Figure {
+  public shape = Shape.Rectangle;
 
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+  ) {
+    if (a <= 0 || b <= 0) {
+      throw new Error('Input data is incorrect');
+    }
+  }
+
+  getArea(): number {
+    const s: number = this.a * this.b;
+
+    return s;
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
