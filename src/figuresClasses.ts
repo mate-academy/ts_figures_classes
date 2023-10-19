b4f3fd7bffaab9d83011a37cbf0baea06fa1b885
nadia vlasenko <nadiavlasenko@MacBook-Pro-nadia.local>
2023-10-19 21:49:36 +0200

    fixed errors

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -10,7 +10,8 @@ export class Triangle implements Figure {
     public color: string,
     public a:number,
     public b:number,
-    public c:number) {
+    public c:number)
+  {
     if (a <= 0 || b <= 0 || c <= 0 || a + b <= c || a + c <= b || b + c <= a) {
       throw new Error(`Invalid triangle: sides ${a},`
       + `${b}, and ${c} cannot form a ${shape}`);
@@ -19,7 +20,8 @@ export class Triangle implements Figure {
 
   getArea(): number {
     const semi = (this.a + this.b + this.c) / 2;
-    const square = Math.sqrt(semi * (semi - this.a) * (semi - this.b) * (semi - this.c));
+    const square = Math.sqrt(semi * (semi - this.a)
+    * (semi - this.b) * (semi - this.c));
 
     return Math.round(square * 100) / 100;
   }
@@ -29,7 +31,8 @@ export class Circle implements Figure {
   constructor(
     public color: string,
     public shape: string,
-    public radius: number) {
+    public radius: number)
+  {
     if (radius <= 0) {
       throw new Error('Invalid circle: radius must be greater than 0');
     }
@@ -47,7 +50,8 @@ export class Rectangle implements Figure {
     public color: string,
     public shape: string,
     public width: number,
-    public height: number) {
+    public height: number)
+  {
     if (this.width <= 0 || this.height <= 0) {
       throw new Error('Invalid rectangle:'
       + 'width and height must be greater than 0');
