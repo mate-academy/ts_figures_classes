4d40aad9bf3819122f7c96f6b5ef27be05d68f96
nadia vlasenko <nadiavlasenko@MacBook-Pro-nadia.local>
2023-10-19 21:46:02 +0200

    fixed errors

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -10,17 +10,16 @@ export class Triangle implements Figure {
     public color: string,
     public a:number,
     public b:number,
-    public c:number)
-    {
+    public c:number) {
     if (a <= 0 || b <= 0 || c <= 0 || a + b <= c || a + c <= b || b + c <= a) {
-      throw new Error(`Invalid triangle: sides ${a},` +
-       `${b}, and ${c} cannot form a ${shape}`);
+      throw new Error(`Invalid triangle: sides ${a},`
+      + `${b}, and ${c} cannot form a ${shape}`);
     }
   }
 
   getArea(): number {
     const semi = (this.a + this.b + this.c) / 2;
-    let square = Math.sqrt(semi * (semi - this.a) * (semi - this.b)*(semi - this.c));
+    const square = Math.sqrt(semi * (semi - this.a) * (semi - this.b) * (semi - this.c));
 
     return Math.round(square * 100) / 100;
   }
@@ -30,8 +29,7 @@ export class Circle implements Figure {
   constructor(
     public color: string,
     public shape: string,
-    public radius: number)
-  {
+    public radius: number) {
     if (radius <= 0) {
       throw new Error('Invalid circle: radius must be greater than 0');
     }
@@ -49,16 +47,15 @@ export class Rectangle implements Figure {
     public color: string,
     public shape: string,
     public width: number,
-    public height: number)
-  {
+    public height: number) {
     if (this.width <= 0 || this.height <= 0) {
-      throw new Error('Invalid rectangle:' +
-      'width and height must be greater than 0');
+      throw new Error('Invalid rectangle:'
+      + 'width and height must be greater than 0');
     }
   }
 
   getArea(): number {
-    let square = this.width * this.height;
+    const square = this.width * this.height;
 
     return square;
   }
