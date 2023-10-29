f1a68c11b0b34f1d5b815baf24a029f984fd69c0
andrew gidrovich <mr.androvich55555@gmail.com>
2023-10-29 14:53:56 +0200

    add task solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,22 +1,35 @@
+/* eslint-disable max-len */
+enum Shape {
+  Triangle = 'triangle',
+  Circle = 'circle',
+  Rectangle = 'rectangle',
+}
+
+enum Color {
+  Red = 'red',
+  Blue = 'blue',
+  Green = 'green',
+}
+
 export interface Figure {
-  shape: string;
-  color: string;
+  shape: Shape;
+  color: Color;
   getArea(): number;
 }
 
 export class Triangle {
-  shape = 'triangle';
+  shape: Shape = Shape.Triangle;
 
-  color: string;
+  color: Color;
 
   sides: [number, number, number];
 
-  constructor(color: string, ...sides: [number, number, number]) {
-    if (sides.some((side) => side <= 0) || this.isNotTriangle(sides)) {
+  constructor(color: Color, side1: number, side2: number, side3: number) {
+    if (side1 <= 0 || side2 <= 0 || side3 <= 0 || this.isNotTriangle(side1, side2, side3)) {
       throw new Error('Invalid triangle');
     }
     this.color = color;
-    this.sides = sides;
+    this.sides = [side1, side2, side3];
   }
 
   getArea(): number {
@@ -26,17 +39,15 @@ export class Triangle {
     return Number((Math.sqrt(s * (s - a) * (s - b) * (s - c))).toFixed(2));
   }
 
-  private isNotTriangle = (sides: [number, number, number]): boolean => {
-    const [a, b, c] = sides;
-
-    return a + b <= c || a + c <= b || b + c <= a;
+  private isNotTriangle = (side1: number, side2: number, side3: number): boolean => {
+    return side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1;
   };
 }
 
 export class Circle {
-  shape = 'circle';
+  shape: Shape = Shape.Circle;
 
-  constructor(public color: string, public radius: number) {
+  constructor(public color: Color, public radius: number) {
     if (radius <= 0) {
       throw new Error('Invalid circle');
     }
@@ -48,15 +59,15 @@ export class Circle {
 }
 
 export class Rectangle {
-  shape = 'rectangle';
+  shape: Shape = Shape.Rectangle;
 
-  color: string;
+  color: Color;
 
   width: number;
 
   height: number;
 
-  constructor(color: string, width: number, height: number) {
+  constructor(color: Color, width: number, height: number) {
     if (width <= 0 || height <= 0) {
       throw new Error('Invalid rectangle');
     }
