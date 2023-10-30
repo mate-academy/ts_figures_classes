4894cf85bc51ab5e868a177fe109bb3164b76ad4
Ivan Orinchak <124816409+IvanOrinchak@users.noreply.github.com>
2023-10-30 20:17:10 +0200

    fix--1

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,49 +1,59 @@
+export enum Shape {
+  TRIANGLE = 'triangle',
+  CIRCLE = 'circle',
+  RECTANGLE = 'rectangle'
+}
+
 export interface Figure {
-  shape:string,
-  color:string,
-  getArea():number,
+  shape: Shape,
+  color: string,
+  getArea(): number,
+}
+
+export function roundedDownToHundredths(value: number): number {
+  return Math.floor(value * 100) / 100;
 }
 
 export class Triangle implements Figure {
-  shape = 'triangle';
+  shape = Shape.TRIANGLE;
 
   constructor(
-    public color:string,
-    public a:number,
-    public b:number,
-    public c:number,
+    public color: string,
+    public a: number,
+    public b: number,
+    public c: number,
   ) {
     if (a <= 0 || b <= 0 || c <= 0 || a + b <= c || a + c <= b || b + c <= a) {
       throw new Error('The sides of the triangle are not correctly specified');
     }
   }
 
-  getArea():number {
-    const p:number = (this.a + this.b + this.c) / 2;
+  getArea(): number {
+    const p: number = (this.a + this.b + this.c) / 2;
     const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
 
-    return Math.floor(area * 100) / 100;
+    return roundedDownToHundredths(area);
   }
 }
 
 export class Circle implements Figure {
-  shape = 'circle';
+  shape = Shape.CIRCLE;
 
-  constructor(public color:string, public radius: number) {
+  constructor(public color: string, public radius: number) {
     if (this.radius <= 0) {
       throw new Error('The radius is incorrect');
     }
   }
 
-  getArea():number {
+  getArea(): number {
     const area = Math.PI * this.radius * this.radius;
 
-    return Math.floor(area * 100) / 100;
+    return roundedDownToHundredths(area);
   }
 }
 
 export class Rectangle implements Figure {
-  shape = 'rectangle';
+  shape = Shape.RECTANGLE;
 
   constructor(
     public color: string,
@@ -55,13 +65,13 @@ export class Rectangle implements Figure {
     }
   }
 
-  getArea():number {
+  getArea(): number {
     const area = this.width * this.height;
 
-    return Math.floor(area * 100) / 100;
+    return roundedDownToHundredths(area);
   }
 }
 
-export function getInfo(figure:Figure):string {
+export function getInfo(figure: Figure): string {
   return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
