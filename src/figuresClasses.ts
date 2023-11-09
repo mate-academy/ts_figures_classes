fe4e14400ddca03f6f1c9e54129c0b08b265fe6d
Damian <damian.adamczewski92@gmail.com>
2023-11-09 11:12:32 +0100

    add task solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,82 @@
+type Shape = 'circle' |'rectangle' | 'triangle';
+type Color = 'blue' | 'green' |'red';
+
 export interface Figure {
-
+  shape: Shape;
+  color: Color;
+  getArea(): number;
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  shape: Shape = 'triangle';
 
+  constructor(
+    public color: Color,
+    private a: number,
+    private b: number,
+    private c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('The sides of a triangle must be positive');
+    }
+
+    if (a + b <= c || a + c <= b || b + c <= a) {
+      throw new Error('The sides of a triangle must be in ascending order');
+    }
+  }
+
+  getArea(): number {
+    const { a, b, c } = this;
+    const semiPerimeter = (a + b + c) / 2;
+    const area = Math.sqrt(
+      semiPerimeter
+      * (semiPerimeter - a)
+      * (semiPerimeter - b)
+      * (semiPerimeter - c),
+    );
+
+    return Math.floor(area * 100) / 100;
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  shape: Shape = 'circle';
 
+  constructor(
+    public color: Color,
+    private radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('The radius of a circle must be positive');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor(Math.PI * this.radius * this.radius * 100) / 100;
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  shape: Shape = 'rectangle';
 
+  constructor(
+    public color: Color,
+    private height: number,
+    private width: number,
+  ) {
+    if (height <= 0 || width <= 0) {
+      throw new Error('The height and width of a rectangle must be positive');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor(this.height * this.width * 100) / 100;
+  }
 }
 
-export function getInfo(figure) {
+export function getInfo(figure: Figure): string {
+  const { shape, color } = figure;
+  const area = figure.getArea();
 
+  return `A ${color} ${shape} - ${area}`;
 }
