527bbf98a6f5ec85620d51638965773ce153346a
Artem Sydun <sydun2003@gmail.com>
2023-11-17 15:35:40 +0200

    solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,77 @@
+type Shape = 'triangle' | 'circle' | 'rectangle';
+type Color = 'red' | 'green' | 'blue';
+
 export interface Figure {
-
+  shape: Shape;
+  color: Color;
+  getArea(): number;
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  shape: Shape = 'triangle';
 
+  private perimeter: number;
+
+  constructor(
+    public color: Color,
+    private a: number,
+    private b: number,
+    private c: number,
+  ) {
+    if (
+      a <= 0
+      || b <= 0
+      || c <= 0
+      || Math.max(a, b, c) >= a + b + c - Math.max(a, b, c)
+    ) {
+      throw new Error('Wrong data');
+    }
+    this.perimeter = (this.a + this.b + this.c) / 2;
+  }
+
+  getArea(): number {
+    const { a, b, c } = this;
+
+    return Math.floor(Math.sqrt(this.perimeter
+    * (this.perimeter - a)
+    * (this.perimeter - b)
+    * (this.perimeter - c)) * 100) / 100;
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  shape: Shape = 'circle';
 
+  constructor(public color: Color, private radius: number) {
+    if (radius <= 0) {
+      throw new Error('Wrong data');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor((Math.PI * this.radius ** 2) * 100) / 100;
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  shape: Shape = 'rectangle';
 
+  constructor(
+    public color: Color,
+    private height: number,
+    private width: number,
+  ) {
+    if (height <= 0
+      || width <= 0) {
+      throw new Error('Wrong data');
+    }
+  }
+
+  getArea(): number {
+    return this.height * this.width;
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
