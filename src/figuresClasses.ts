adf63581b2efc351bed9c8a4ac9b52b05526efc4
Dmytro Krasnoholovenko <errisi.v@gmail.com>
2023-11-21 12:37:36 +0200

    result

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,85 @@
+type Shape = 'triangle' | 'circle' | 'rectangle';
+type Color = 'red' | 'green' | 'blue';
+
 export interface Figure {
-
+  shape: Shape,
+  color: Color,
+  getArea(): number;
 }
-
 export class Triangle implements Figure {
+  shape: Shape = 'triangle';
 
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (
+      a <= 0
+      || b <= 0
+      || c <= 0
+      || Math.max(a, b, c) >= a + b + c - Math.max(a, b, c)
+    ) {
+      throw new Error(`sides ${a}, ${b} and ${c} can't form a triangle`);
+    }
+  }
+
+  getArea(): number {
+    const { a, b, c } = this;
+    const s = (a + b + c) / 2;
+    const area = Math.sqrt(
+      s
+        * (s - a)
+        * (s - b)
+        * (s - c),
+    );
+
+    return Math.floor(area * 100) / 100;
+  }
 }
 
 export class Circle implements Figure {
+  shape: Shape = 'circle';
 
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error(`radius ${radius} can't form a circle`);
+    }
+  }
+
+  getArea(): number {
+    const area = Math.PI * this.radius * this.radius;
+
+    return Math.floor(area * 100) / 100;
+  }
 }
 
 export class Rectangle implements Figure {
+  shape: Shape = 'rectangle';
 
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error(
+        `width ${width} and height ${height} can't form a rectangle`,
+      );
+    }
+  }
+
+  getArea(): number {
+    const area = this.width * this.height;
+
+    return Math.floor(area * 100) / 100;
+  }
 }
 
-export function getInfo(figure): string {
-  return typeof figure;
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
