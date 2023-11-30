b1dd83e9c76a144671ca8907e4863f2b19ed6e4e
Malyholovka Ostap <maligolovkaostap@gmail.com>
2023-11-30 15:29:00 +0200

    solution 2

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -2,13 +2,13 @@ type Shape = 'triangle' | 'circle' | 'rectangle';
 type Color = 'red' | 'green' | 'blue';
 
 export interface Figure {
-  shape: Shape,
-  color: Color,
-  getArea(): number,
+  shape: Shape;
+  color: Color;
+  getArea(): number;
 }
 
 export class Triangle implements Figure {
-  shape: Shape;
+  shape: Shape = 'triangle';
 
   constructor(
     public color: Color,
@@ -23,29 +23,31 @@ export class Triangle implements Figure {
     if (a + b <= c || a + c <= b || b + c <= a) {
       throw new Error(`sides ${a}, ${b} and ${c} can't form a triangle`);
     }
-
-    this.shape = 'triangle';
   }
 
   getArea(): number {
     const { a, b, c } = this;
-    const p = (a + b + c) / 2;
-    const area
-      = Math.floor(Math.sqrt(p * (p - a) * (p - b) * (p - c)) * 100) / 100;
+    const perimeter = (a + b + c) / 2;
+    const area = Math.floor(
+      Math.sqrt(
+        perimeter * (perimeter - a) * (perimeter - b) * (perimeter - c),
+      ) * 100,
+    ) / 100;
 
     return area;
   }
 }
 
 export class Circle implements Figure {
-  shape: Shape;
+  shape: Shape = 'circle';
 
-  constructor(public color: Color, public radius: number) {
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
     if (radius < 1) {
       throw new Error('radius is null');
     }
-
-    this.shape = 'circle';
   }
 
   getArea(): number {
@@ -54,7 +56,7 @@ export class Circle implements Figure {
 }
 
 export class Rectangle implements Figure {
-  shape: Shape;
+  shape: Shape = 'rectangle';
 
   constructor(
     public color: Color,
@@ -62,10 +64,8 @@ export class Rectangle implements Figure {
     public height: number,
   ) {
     if (weidth < 1 || height < 1) {
-      throw new Error('must be a size');
+      throw new Error('All side lengths must be more than 0');
     }
-
-    this.shape = 'rectangle';
   }
 
   getArea(): number {
