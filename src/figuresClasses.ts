346e34340fb91548519efc69ff4c390b34dfbe0c
Roman Perepelkin <perepelkinrs@gmail.com>
2023-12-15 16:27:48 +0200

    Solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,81 @@
 export interface Figure {
-
+  shape: 'triangle' | 'circle' | 'rectangle',
+  color: 'red' | 'green' | 'blue',
+  getArea(): number,
 }
 
 export class Triangle implements Figure {
+  shape: Figure['shape'] = 'triangle';
 
+  constructor(
+    public color: Figure['color'],
+    private a: number,
+    private b: number,
+    private c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('Length of sides must be > 0');
+    }
+
+    if (a + b <= c || b + c <= a || c + a <= b) {
+      throw new Error('Invalid triangle');
+    }
+  }
+
+  getArea(): number {
+    const { a, b, c } = this;
+    const p: number = (a + b + c) / 2;
+
+    const area: number = Math.floor(Math.sqrt(p * (p - a) * (p - b) * (p - c))
+     * 100) / 100;
+
+    return area;
+  }
 }
 
 export class Circle implements Figure {
+  shape: Figure['shape'] = 'circle';
 
+  constructor(
+    public color: Figure['color'],
+    private radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('Radius must be > 0');
+    }
+  }
+
+  getArea(): number {
+    const { radius } = this;
+
+    const area: number = Math.floor(Math.PI * (radius ** 2) * 100) / 100;
+
+    return area;
+  }
 }
 
 export class Rectangle implements Figure {
+  shape: Figure['shape'] = 'rectangle';
 
+  constructor(
+    public color: Figure['color'],
+    private width: number,
+    private height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('Width and height must be > 0');
+    }
+  }
+
+  getArea(): number {
+    const { width, height } = this;
+
+    const area: number = Math.floor(width * height * 100) / 100;
+
+    return area;
+  }
 }
 
-export function getInfo(figure): string {
-  return typeof figure;
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
