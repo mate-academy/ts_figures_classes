4d7fd6f160605224d0140f80df76f207c706f3b8
Kriss Kozak <misskrissretro@gmail.com>
2023-12-27 22:20:11 +0200

    ts_figures_classes

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,62 @@
 export interface Figure {
-
+  color: string;
+  getArea(): number;
 }
 
 export class Triangle implements Figure {
+  constructor(
+    public color: string,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('Triangle sides must be greater than 0.');
+    }
 
+    if (a + b <= c || a + c <= b || b + c <= a) {
+      throw new Error(
+        'Invalid triangle sides. They do not form a valid triangle.',
+      );
+    }
+  }
+
+  getArea(): number {
+    const s = (this.a + this.b + this.c) / 2;
+
+    return Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+  }
 }
 
 export class Circle implements Figure {
+  constructor(public color: string, public radius: number) {
+    if (radius <= 0) {
+      throw new Error('Circle radius must be greater than 0.');
+    }
+  }
 
+  getArea(): number {
+    return Math.PI * this.radius * this.radius;
+  }
 }
 
 export class Rectangle implements Figure {
+  constructor(
+    public color: string,
+    public width: number,
+    public height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('Rectangle dimensions must be greater than 0.');
+    }
+  }
 
+  getArea(): number {
+    return this.width * this.height;
+  }
 }
 
-export function getInfo(figure): string {
-  return typeof figure;
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color.toLowerCase()}
+    ${figure.constructor.name.toLowerCase()} - ${figure.getArea().toFixed(2)}`;
 }
