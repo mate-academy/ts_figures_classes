06f252fea8e035d101f97cd652c738a5f64d556f
Kriss Kozak <misskrissretro@gmail.com>
2023-12-27 22:28:43 +0200

    ts_figures_classes

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,15 +1,20 @@
 export interface Figure {
+  shape: string;
   color: string;
   getArea(): number;
 }
 
 export class Triangle implements Figure {
+  shape: string;
+
   constructor(
     public color: string,
     public a: number,
     public b: number,
     public c: number,
   ) {
+    this.shape = 'triangle';
+
     if (a <= 0 || b <= 0 || c <= 0) {
       throw new Error('Triangle sides must be greater than 0.');
     }
@@ -29,7 +34,11 @@ export class Triangle implements Figure {
 }
 
 export class Circle implements Figure {
+  shape: string;
+
   constructor(public color: string, public radius: number) {
+    this.shape = 'circle';
+
     if (radius <= 0) {
       throw new Error('Circle radius must be greater than 0.');
     }
@@ -41,11 +50,15 @@ export class Circle implements Figure {
 }
 
 export class Rectangle implements Figure {
+  shape: string;
+
   constructor(
     public color: string,
     public width: number,
     public height: number,
   ) {
+    this.shape = 'rectangle';
+
     if (width <= 0 || height <= 0) {
       throw new Error('Rectangle dimensions must be greater than 0.');
     }
