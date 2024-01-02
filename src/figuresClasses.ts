6c15d0102618a271d682060e01761dd044db106f
Heorhii Churhuliia <gio4929189@gmail.com>
2024-01-02 19:38:38 +0000

    add task solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -68,7 +68,7 @@ export class Rectangle implements Figure {
   }
 
   getArea(): number {
-    return this.width * this.height;
+    return Math.floor(this.width * this.height * 100) / 100;
   }
 }
 
