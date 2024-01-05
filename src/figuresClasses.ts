895083a72d05021655b9401a0d9b50331771d8e9
Oleksandr Podvoiskyi <podvoyskiy01@gmail.com>
2024-01-05 16:39:34 +0200

    Add task solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,74 @@
 export interface Figure {
-
+  shape: 'triangle' | 'circle' | 'rectangle',
+  color: 'red' | 'green' | 'blue',
+  getArea(): number;
 }
 
 export class Triangle implements Figure {
+  public shape: 'triangle' = 'triangle';
 
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (a + b <= c || a + c <= b || b + c <= a) {
+      throw new Error('It is not a triangle!');
+    }
+
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('Triangle sides should be positive!');
+    }
+  }
+
+  getArea(): number {
+    const p = (this.a + this.b + this.c) / 2;
+
+    return Math.trunc(Math.sqrt(
+      p
+      * (p - this.a)
+      * (p - this.b)
+      * (p - this.c),
+    ) * 100) / 100;
+  }
 }
 
 export class Circle implements Figure {
+  public shape: 'circle' = 'circle';
 
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('Radius should be positive!');
+    }
+  }
+
+  getArea(): number {
+    return Math.trunc((Math.PI * (this.radius ** 2)) * 100) / 100;
+  }
 }
 
 export class Rectangle implements Figure {
+  public shape: 'rectangle' = 'rectangle';
 
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public a: number,
+    public b: number,
+  ) {
+    if (a <= 0 || b <= 0) {
+      throw new Error('Rectangle sides should be positive!');
+    }
+  }
+
+  getArea(): number {
+    return Math.trunc((this.a * this.b) * 100) / 100;
+  }
 }
 
-export function getInfo(figure): string {
-  return typeof figure;
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
