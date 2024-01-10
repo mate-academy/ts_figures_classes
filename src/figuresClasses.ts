66570a46524b00b7fe96d7c320a56d046848549b
Oleksandr Boiko <iccc97@gmail.com>
2024-01-10 21:27:40 +0200

    add figures solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,75 @@
 export interface Figure {
-
+  shape: string, // 'triangle' | 'circle' | 'rectangle'
+  color: string, // 'red' | 'green' | 'blue'
+  getArea(): number,
 }
 
 export class Triangle implements Figure {
+  shape = 'triangle';
 
+  constructor(
+    public color: string,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (
+      (this.a <= 0 || this.b <= 0 || this.c <= 0)
+      || (this.a + this.b <= this.c)
+    ) {
+      throw new Error('should have correct sides');
+    }
+  }
+
+  getArea(): number {
+    const semiP: number = (this.a + this.b + this.c) / 2;
+    const area: number = Math.sqrt(
+      semiP * (semiP - this.a) * (semiP - this.b) * (semiP - this.c),
+    );
+
+    return Math.floor(area * 100) / 100;
+  }
 }
 
 export class Circle implements Figure {
+  shape = 'circle';
 
+  constructor(
+    public color: string,
+    public radius: number,
+  ) {
+    if (this.radius <= 0) {
+      throw new Error('should have correct radius');
+    }
+  }
+
+  getArea(): number {
+    const area: number = Math.PI * this.radius ** 2;
+
+    return Math.floor(area * 100) / 100;
+  }
 }
 
 export class Rectangle implements Figure {
+  shape = 'rectangle';
 
+  constructor(
+    public color: string,
+    public width: number,
+    public height: number,
+  ) {
+    if (this.width <= 0 || this.height <= 0) {
+      throw new Error('should have correct width or height');
+    }
+  }
+
+  getArea(): number {
+    const area: number = this.width * this.height;
+
+    return Math.floor(area * 100) / 100;
+  }
 }
 
-export function getInfo(figure): string {
-  return typeof figure;
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
