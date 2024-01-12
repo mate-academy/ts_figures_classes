b1978cbe51aff02df5c23284534d2175425a5d1b
Vlad Pashko <vladpavloggg@gmail.com>
2024-01-12 21:29:42 +0200

    calc area

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,99 @@
 export interface Figure {
-
+  shape: string,
+  color: string,
+  getArea(): number,
 }
 
 export class Triangle implements Figure {
+  shape = 'triangle';
 
+  constructor(
+    public color: string,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (![this.a, this.b, this.c].every((side) => side > 0)) {
+      throw new Error(
+        'some side(-s) equal(-s) 0, non-existent triangle',
+      );
+    }
+
+    const sides = [];
+
+    sides.push(this.a, this.b, this.c);
+
+    sides.sort((side1, side2) => side2 - side1);
+
+    if (sides[0] >= sides[1] + sides[2]) {
+      throw new Error(
+        'wrong sides, non-existent triangle',
+      );
+    }
+  }
+
+  getArea(): number {
+    const sides: number[] = [];
+
+    sides.push(this.a, this.b, this.c);
+
+    const halfPerimetr = sides.reduce((sum, element) => sum + element, 0) / 2;
+
+    const area = Math
+      .sqrt(
+        halfPerimetr
+        * (halfPerimetr - this.a)
+        * (halfPerimetr - this.b)
+        * (halfPerimetr - this.c),
+      );
+
+    return Math.floor(area * 100) / 100;
+  }
 }
 
 export class Circle implements Figure {
+  shape = 'circle';
 
+  constructor(
+    public color: string,
+    public radius: number,
+  ) {
+    if (this.radius <= 0) {
+      throw new Error(
+        'radius should be > 0',
+      );
+    }
+  }
+
+  getArea(): number {
+    const area = Math.PI * this.radius ** 2;
+
+    return Math.floor(area * 100) / 100;
+  }
 }
 
 export class Rectangle implements Figure {
+  shape = 'rectangle';
 
+  constructor(
+    public color: string,
+    public a: number,
+    public b: number,
+  ) {
+    if ([a, b].some((x) => x <= 0)) {
+      throw new Error(
+        'sides should be >= 0',
+      );
+    }
+  }
+
+  getArea(): number {
+    const area = this.a * this.b;
+
+    return Math.floor(area * 100) / 100;
+  }
 }
 
-export function getInfo(figure): string {
-  return typeof figure;
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
