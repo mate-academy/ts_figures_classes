2972d43dc5a84d93b9f0c52d8578c0995d780a13
Yura Bublyk <yurabublik703@gmail.com>
2024-01-13 17:51:05 +0200

    Solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,78 @@
 export interface Figure {
+  shape: string;
+  color: string;
 
+  getArea (): number;
 }
 
 export class Triangle implements Figure {
+  shape: string = 'triangle';
 
+  constructor(
+    public color: string,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('your length should be more than 0');
+    }
+
+    if (Math.max(a, b, c) >= (a + b + c) - Math.max(a, b, c)) {
+      throw new Error('your length of sides are incorrect');
+    }
+  }
+
+  getArea(): number {
+    const halfP = (this.a + this.b + this.c) / 2;
+
+    const result = Math.sqrt(
+      (halfP * (halfP - this.a) * (halfP - this.b) * (halfP - this.c)),
+    );
+
+    return +result.toFixed(2);
+  }
 }
 
 export class Circle implements Figure {
+  shape: string = 'circle';
 
+  constructor(
+    public color: string,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('radius must be greater than 0');
+    }
+  }
+
+  getArea(): number {
+    const result = Math.PI * this.radius ** 2;
+
+    return Math.floor(result * 100) / 100;
+  }
 }
 
 export class Rectangle implements Figure {
+  shape: string = 'rectangle';
 
+  constructor(
+    public color: string,
+    public width: number,
+    public height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('sides must be greater than 0');
+    }
+  }
+
+  getArea(): number {
+    const result = this.width * this.height;
+
+    return +result.toFixed(2);
+  }
 }
 
-export function getInfo(figure): string {
-  return typeof figure;
+export function getInfo(figure:Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
