beb07892fc24849dbb09553a09b284421618799c
Vasilliy Yarosh <vasilliy.yarosh@gmail.com>
2024-01-15 18:32:28 +0200

    Solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,75 @@
-export interface Figure {
+export type ShapeType = 'triangle' | 'circle' | 'rectangle';
+export type ColorType = 'red' | 'green' | 'blue';
 
+export interface Figure {
+  shape: 'triangle' | 'circle' | 'rectangle';
+  color: 'red' | 'green' | 'blue';
+  getArea(): number;
 }
 
 export class Triangle implements Figure {
+  readonly shape: ShapeType = 'triangle';
 
+  constructor(
+    public color: ColorType,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('Side lengths must be less than 0');
+    }
+
+    if (a + b <= c || a + c <= b || b + c <= a) {
+      throw new Error('Invalid triangle sides');
+    }
+  }
+
+  getArea(): number {
+    const s = (this.a + this.b + this.c) / 2;
+
+    return Math.floor(Math
+      .sqrt(s * (s - this.a) * (s - this.b) * (s - this.c)) * 100) / 100;
+  }
 }
 
 export class Circle implements Figure {
+  readonly shape: ShapeType = 'circle';
 
+  constructor(
+    public color: ColorType,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('Radius must not be less than 0');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor(Math.PI * this.radius * this.radius * 100) / 100;
+  }
 }
 
 export class Rectangle implements Figure {
+  readonly shape: ShapeType = 'rectangle';
 
+  constructor(
+    public color: ColorType,
+    public width: number,
+    public height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('Data less than 0');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor(this.width * this.height * 100) / 100;
+  }
 }
 
-export function getInfo(figure): string {
-  return typeof figure;
+export function getInfo(figure: Figure): string {
+  const area = figure.getArea();
+
+  return `A ${figure.color} ${figure.shape} - ${area}`;
 }
