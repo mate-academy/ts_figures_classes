7586b8753860c587bf14276d8a29dd489d0f1006
Illya <katinskyi.illia@lll.kpi.ua>
2024-01-29 15:21:48 +0200

    Add solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,79 @@
-export interface Figure {
+type Shape = 'triangle' | 'circle' | 'rectangle';
+type Color = 'red' | 'green' | 'blue';
 
+export interface Figure {
+  shape: Shape;
+  color: Color;
+  getArea(): number;
 }
 
 export class Triangle implements Figure {
+  shape: Shape = 'triangle';
 
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    const sides = [this.a, this.b, this.c];
+    const maxSide = Math.max(...sides);
+    const sumOfTwo = sides
+      .filter((side) => side !== maxSide)
+      .reduce((acc, val) => acc + val);
+
+    if (maxSide >= sumOfTwo || Math.min(...sides) <= 0) {
+      throw new Error('Incorrect sides of triangle');
+    }
+  }
+
+  getArea(): number {
+    const p = (this.a + this.b + this.c) / 2;
+    const area
+    = Math.floor(
+      Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)) * 100,
+    ) / 100;
+
+    return area;
+  }
 }
 
 export class Circle implements Figure {
+  shape: Shape = 'circle';
 
+  constructor(public color: Color, public radius: number) {
+    if (radius <= 0) {
+      throw new Error('Incorrect radius of circle');
+    }
+  }
+
+  getArea(): number {
+    const area = Math.floor(Math.PI * (this.radius ** 2) * 100) / 100;
+
+    return area;
+  }
 }
 
 export class Rectangle implements Figure {
+  shape: Shape = 'rectangle';
 
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
+    if (Math.min(width, height) <= 0) {
+      throw new Error('Incorrect sides of rectangle');
+    }
+  }
+
+  getArea(): number {
+    const area = Math.floor(this.width * this.height * 100) / 100;
+
+    return area;
+  }
 }
 
-export function getInfo(figure): string {
-  return typeof figure;
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
