3e62434c763bd8eeab3faeeb2974cefa4f6e6b6a
Dmytro Chadov <Dmytrochd@gmail.com>
2024-02-08 19:24:38 +0200

    add task solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,91 @@
-export interface Figure {
+type Shape = 'triangle' | 'circle' | 'rectangle';
+type Color = 'red' | 'green' | 'blue';
 
+export interface Figure {
+  getArea(): number;
+  shape: Shape;
+  color: Color;
 }
 
 export class Triangle implements Figure {
+  shape: Shape = 'triangle';
 
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('throws an error');
+    }
+
+    if (a + b <= c || a + c <= b || b + c <= a) {
+      throw new Error('throws an error: sides 1, 2 and 3 can`t form triangle');
+    }
+  }
+
+  getArea(): number {
+    const { a, b, c } = this;
+    const p: number = (a + b + c) / 2;
+    const s: number = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+
+    return Math.round(s * 100) / 100;
+  }
 }
 
 export class Circle implements Figure {
+  shape: Shape = 'circle';
 
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('throws an error');
+    }
+  }
+
+  getArea(): number {
+    const { radius } = this;
+    const area = Math.PI * (radius ** 2);
+
+    return Math.floor(area * 100) / 100;
+  }
 }
 
 export class Rectangle implements Figure {
+  shape: Shape = 'rectangle';
 
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('throws an error');
+    }
+  }
+
+  getArea(): number {
+    const { width, height } = this;
+
+    return width * height;
+  }
 }
 
-export function getInfo(figure): string {
-  return typeof figure;
+export function getInfo(figure: Figure): string {
+  switch (figure.shape) {
+    case 'triangle':
+      return `A ${figure.color} triangle - ${figure.getArea()}`;
+
+    case 'circle':
+      return `A ${figure.color} circle - ${figure.getArea()}`;
+
+    case 'rectangle':
+      return `A ${figure.color} rectangle - ${figure.getArea()}`;
+
+    default:
+      return 'Unknown figure';
+  }
 }
