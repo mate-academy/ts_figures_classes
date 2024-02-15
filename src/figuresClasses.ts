dd919bf67e9f785bea72378d837de48c1826b1dc
volodymyr <volodiayurovych@gmail.com>
2024-02-15 21:10:30 +0100

    edit2

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -33,10 +33,6 @@ export class Triangle implements Figure {
     const result: number = (Math.sqrt(hp
       * (hp - this.a) * (hp - this.b) * (hp - this.c)));
 
-    if (result % 1 === 0) {
-      return result;
-    }
-
     return Math.floor(result * 100) / 100;
   }
 }
@@ -57,10 +53,6 @@ export class Circle implements Figure {
 
   getArea(): number | string {
     const result: number = (Math.PI * (this.radius * this.radius));
-
-    if (result % 1 === 0) {
-      return result;
-    }
 
     return Math.floor(result * 100) / 100;
   }
@@ -83,10 +75,6 @@ export class Rectangle implements Figure {
 
   getArea(): number | string {
     const result: number = (this.width * this.height);
-
-    if (result % 1 === 0) {
-      return result;
-    }
 
     return Math.floor(result * 100) / 100;
   }
