81031aded63df8b9c39f5de745cb2d14009a61a9
Andrew Rich <andy.rich3a@gmail.com>
2024-03-01 10:41:42 +0200

    add fix

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,17 +1,22 @@
+type Shape = 'triangle' | 'circle' | 'rectangle';
+
 export interface Figure {
-  shape: string;
+  shape: Shape;
   color: string;
   getArea(): number;
 }
 
 export class Triangle implements Figure {
-  shape = 'triangle';
-
-  color: string;
+  shape: Shape = 'triangle';
 
   sides: number[];
 
-  constructor(color: string, a: number, b: number, c: number) {
+  constructor(
+    public color: string,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
     if (a <= 0 || b <= 0 || c <= 0) {
       throw new Error('Sides must be greater than 0.');
     }
@@ -20,7 +25,6 @@ export class Triangle implements Figure {
       throw new Error('Invalid triangle sides.');
     }
 
-    this.color = color;
     this.sides = [a, b, c];
   }
 
@@ -36,19 +40,12 @@ export class Triangle implements Figure {
 }
 
 export class Circle implements Figure {
-  shape = 'circle';
+  shape: Shape = 'circle';
 
-  color: string;
-
-  radius: number;
-
-  constructor(color: string, radius: number) {
+  constructor(public color: string, public radius: number) {
     if (radius <= 0) {
       throw new Error('Radius must be greater than 0.');
     }
-
-    this.color = color;
-    this.radius = radius;
   }
 
   getArea(): number {
@@ -59,22 +56,16 @@ export class Circle implements Figure {
 }
 
 export class Rectangle implements Figure {
-  shape = 'rectangle';
+  shape: Shape = 'rectangle';
 
-  color: string;
-
-  width: number;
-
-  height: number;
-
-  constructor(color: string, width: number, height: number) {
+  constructor(
+    public color: string,
+    public width: number,
+    public height: number,
+  ) {
     if (width <= 0 || height <= 0) {
       throw new Error('Width and height must be greater than 0.');
     }
-
-    this.color = color;
-    this.width = width;
-    this.height = height;
   }
 
   getArea(): number {
@@ -83,17 +74,5 @@ export class Rectangle implements Figure {
 }
 
 export function getInfo(figure: Figure): string {
-  if (figure instanceof Triangle) {
-    return `A ${figure.color} triangle - ${figure.getArea()}`;
-  }
-
-  if (figure instanceof Circle) {
-    return `A ${figure.color} circle - ${figure.getArea()}`;
-  }
-
-  if (figure instanceof Rectangle) {
-    return `A ${figure.color} rectangle - ${figure.getArea()}`;
-  }
-
-  throw new Error('Invalid figure type.');
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
