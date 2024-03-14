3bf9911f937f911a47a9a5e9a0e3a327cedcad68
Kacper Grosicki <kacpergrosicki2002@gmail.com>
2024-03-14 16:15:02 +0100

    task solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,87 +1,74 @@
+type Color = 'red' | 'green' | 'blue';
+type Shape = 'triangle' | 'rectangle' | 'circle';
+
 export interface Figure {
-  color: string;
-  shape: string;
+  color: Color;
+  shape: Shape;
   getArea(): number;
 }
 
 export class Triangle implements Figure {
-  shape = 'triangle';
+  shape: Shape = 'triangle';
 
-  color: string;
-
-  a: number;
-
-  b: number;
-
-  c: number;
-
-  constructor(color: string, a: number, b: number, c: number) {
-    this.a = a;
-    this.b = b;
-    this.c = c;
-    this.color = color;
-
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
     if (a <= 0 || b <= 0 || c <= 0) {
       throw new Error('All sides must be greater than 0');
     }
 
-    if (a + b <= c || a + c <= b || b + c <= a) {
-      throw new Error('Invalid triangle sides');
+    if (Math.max(a, b, c) * 2 >= a + b + c) {
+      throw new Error('Side bigger than others combined');
     }
   }
 
   getArea(): number {
     const s = (this.a + this.b + this.c) / 2;
 
-    return Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+    return Math.floor(
+      Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c)),
+    );
   }
 }
 
 export class Circle implements Figure {
-  shape = 'circle';
+  shape: Shape = 'circle';
 
-  color: string;
-
-  radius: number;
-
-  constructor(color: string, radius: number) {
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
     if (radius <= 0) {
       throw new Error('Radius must be greater than 0');
     }
-    this.radius = radius;
-    this.color = color;
   }
 
   getArea(): number {
-    return Math.PI * this.radius ** 2;
+    return Math.floor(Math.PI * this.radius ** 2);
   }
 }
 
 export class Rectangle implements Figure {
-  shape = 'rectangle';
+  shape: Shape = 'rectangle';
 
-  color: string;
-
-  width: number;
-
-  height: number;
-
-  constructor(color: string, height: number, width: number) {
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
     if (height <= 0 || width <= 0) {
       throw new Error('Width and height must be greater than 0');
     }
-    this.width = width;
-    this.height = height;
-    this.color = color;
   }
 
   getArea(): number {
-    return this.width * this.height;
+    return Math.floor(this.width * this.height);
   }
 }
 
 export function getInfo(figure: Figure): string {
-  const area = +figure.getArea().toFixed(2);
-
-  return `A ${figure.color} ${figure.shape} - ${area}`;
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
