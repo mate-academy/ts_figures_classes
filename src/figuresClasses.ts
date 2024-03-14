3bee312cc06ccc336da78ed3d242f03fc79c016d
Kacper Grosicki <kacpergrosicki2002@gmail.com>
2024-03-14 12:35:40 +0100

    task solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,87 @@
-export interface Figure {}
+export interface Figure {
+  color: string;
+  shape: string;
+  getArea(): number;
+}
 
-export class Triangle implements Figure {}
+export class Triangle implements Figure {
+  shape = 'triangle';
 
-export class Circle implements Figure {}
+  color: string;
 
-export class Rectangle implements Figure {}
+  a: number;
 
-export function getInfo(figure): string {
-  return typeof figure;
+  b: number;
+
+  c: number;
+
+  constructor(color: string, a: number, b: number, c: number) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    this.color = color;
+
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('All sides must be greater than 0');
+    }
+
+    if (a + b <= c || a + c <= b || b + c <= a) {
+      throw new Error('Invalid triangle sides');
+    }
+  }
+
+  getArea(): number {
+    const s = (this.a + this.b + this.c) / 2;
+
+    return Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+  }
+}
+
+export class Circle implements Figure {
+  shape = 'circle';
+
+  color: string;
+
+  radius: number;
+
+  constructor(color: string, radius: number) {
+    if (radius <= 0) {
+      throw new Error('Radius must be greater than 0');
+    }
+    this.radius = radius;
+    this.color = color;
+  }
+
+  getArea(): number {
+    return Math.PI * this.radius ** 2;
+  }
+}
+
+export class Rectangle implements Figure {
+  shape = 'rectangle';
+
+  color: string;
+
+  width: number;
+
+  height: number;
+
+  constructor(color: string, height: number, width: number) {
+    if (height <= 0 || width <= 0) {
+      throw new Error('Width and height must be greater than 0');
+    }
+    this.width = width;
+    this.height = height;
+    this.color = color;
+  }
+
+  getArea(): number {
+    return this.width * this.height;
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  const area = +figure.getArea().toFixed(2);
+
+  return `A ${figure.color} ${figure.shape} - ${area}`;
 }
