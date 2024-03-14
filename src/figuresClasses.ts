3b2bfddd275eee032501ee1e562bb9af4f5d5b7d
Davwidos <d.jurkowski015@gmail.com>
2024-03-14 07:32:21 +0100

    Changes after review

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -10,17 +10,11 @@ export interface Figure {
 export class Triangle implements Figure {
   shape: Shape = 'triangle';
 
-  a: number;
-
-  b: number;
-
-  c: number;
-
   constructor(
     public color: Color,
-    a: number,
-    b: number,
-    c: number,
+    public a: number,
+    public b: number,
+    public c: number,
   ) {
     if (a <= 0 || b <= 0 || c <= 0) {
       throw new Error('All sides shuld be grater than 0');
@@ -29,10 +23,6 @@ export class Triangle implements Figure {
     if (a >= b + c || b >= a + c || c >= a + b) {
       throw new Error(`sides ${a}, ${b}, ${c} can't form triangle`);
     }
-
-    this.a = a;
-    this.b = b;
-    this.c = c;
   }
 
   getArea(): number {
@@ -46,17 +36,13 @@ export class Triangle implements Figure {
 export class Circle implements Figure {
   shape: Shape = 'circle';
 
-  radius: number;
-
   constructor(
     public color: Color,
-    radius: number,
+    public radius: number,
   ) {
     if (radius <= 0) {
       throw new Error('radius should be greater than 0');
     }
-
-    this.radius = radius;
   }
 
   getArea(): number {
@@ -69,21 +55,14 @@ export class Circle implements Figure {
 export class Rectangle implements Figure {
   shape: Shape = 'rectangle';
 
-  width: number;
-
-  height: number;
-
   constructor(
     public color: Color,
-    width: number,
-    height: number,
+    public width: number,
+    public height: number,
   ) {
     if (width <= 0 || height <= 0) {
       throw new Error('width and height should be greater than 0');
     }
-
-    this.width = width;
-    this.height = height;
   }
 
   getArea(): number {
