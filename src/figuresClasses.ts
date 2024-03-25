820ddf2185b04168acb489f9cf1243cbe3f89fb0
Anastasiia <vasilisandreeva00@gmail.com>
2024-03-25 15:58:38 +0200

    Solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,79 @@
-export interface Figure {}
+type Shape = 'triangle' | 'circle' | 'rectangle';
+type Color = 'red' | 'green' | 'blue';
 
-export class Triangle implements Figure {}
+const roundArea = (area: number): number => Math.floor(area * 100) / 100;
 
-export class Circle implements Figure {}
+export interface Figure {
+  shape: Shape;
+  color: Color;
+  getArea(): number;
+}
 
-export class Rectangle implements Figure {}
+export class Triangle implements Figure {
+  shape: Shape = 'triangle';
 
-export function getInfo(figure): string {
-  return typeof figure;
+  constructor(
+    public color: Color,
+    private a: number,
+    private b: number,
+    private c: number,
+  ) {}
+
+  getArea(): number {
+    const { a, b, c } = this;
+
+    const errorChecking: boolean = a !== 0 && b !== 0 && c !== 0;
+    const maxSide: number = Math.max(a, b, c);
+
+    if (!errorChecking && maxSide >= a + b + c - maxSide) {
+      throw new Error('Sizes are not correct');
+    }
+
+    const s = (a + b + c) / 2;
+
+    return roundArea(Math.sqrt(s * (s - a) * (s - b) * (s - c)));
+  }
+}
+
+export class Circle implements Figure {
+  shape: Shape = 'circle';
+
+  constructor(
+    public color: Color,
+    private radius: number,
+  ) {}
+
+  getArea(): number {
+    const { radius } = this;
+
+    if (radius <= 0) {
+      throw new Error('Radius is not correct');
+    }
+
+    return roundArea(Math.PI * Math.pow(radius, 2));
+  }
+}
+
+export class Rectangle implements Figure {
+  shape: Shape = 'rectangle';
+
+  constructor(
+    public color: Color,
+    private width: number,
+    private height: number,
+  ) {}
+
+  getArea(): number {
+    const { width, height } = this;
+
+    if (width <= 0 || height <= 0) {
+      throw new Error('Sizes are not correct');
+    }
+
+    return roundArea(width * height);
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
