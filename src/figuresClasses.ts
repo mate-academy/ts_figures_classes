988bf9484fea5daa9105d5df49c40387ef8a0d01
Bohdan Shyrii <shyrii11bohdan@gmail.com>
2024-03-28 17:58:13 +0100

    farmed

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,81 @@
-export interface Figure {}
+export interface Figure {
+  shape: string;
+  color: string;
+  getArea(): number;
+}
 
-export class Triangle implements Figure {}
+export class Triangle implements Figure {
+  shape = 'triangle';
 
-export class Circle implements Figure {}
+  constructor(
+    public color: string,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (this.a <= 0 || this.b <= 0 || this.c <= 0) {
+      throw new Error('The sides of the triangle must be greater than zero.');
+    }
 
-export class Rectangle implements Figure {}
+    const longest: number = Math.max(this.a, this.b, this.c);
+    const sumOfOther:number = this.a + this.b + this.c - longest;
 
-export function getInfo(figure): string {
-  return typeof figure;
+        if (longest >= sumOfOther) {
+          throw new Error(
+            'The longest side should be less than the sum of the other.',
+          );
+        }
+  }
+
+  getArea(): number {
+
+
+    const p = (this.a + this.b + this.c) / 2;
+    const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+
+    return Math.round(area * 100) / 100;
+  }
+}
+
+export class Circle implements Figure {
+  shape = 'circle';
+
+  constructor(
+    public color: string,
+    public radius: number,
+  ) {
+    if (this.radius <= 0) {
+      throw new Error('The radius of the circle must be greater than zero.');
+    }
+  }
+
+  getArea(): number {
+    const area: number = Math.PI * this.radius * this.radius;
+
+    return Math.floor(area * 100) / 100;
+  }
+}
+
+export class Rectangle implements Figure {
+  shape = 'rectangle';
+
+  constructor(
+    public color: string,
+    public width: number,
+    public height: number,
+  ) {
+    if (this.width <= 0 || this.height <= 0) {
+      throw new Error(
+        'The width and height of the rectangle must be greater than zero.',
+      );
+    }
+  }
+
+  getArea(): number {
+    return this.width * this.height;
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
