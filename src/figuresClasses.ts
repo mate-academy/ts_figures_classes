9d8fd107f5887d3f45acf25a5b058f7a2a4baa99
Alena Sokol <alenashpakova27021995@gmail.com>
2024-04-03 00:24:54 +0300

    Add solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,86 @@
-export interface Figure {}
+type Color = `red` | `green` | `blue`;
 
-export class Triangle implements Figure {}
+enum Shape {
+  triangle = `triangle`,
+  circle = `circle`,
+  rectangle = `rectangle`,
+}
 
-export class Circle implements Figure {}
+export interface Figure {
+  shape: Shape;
+  color: Color;
+  getArea: () => number;
+}
 
-export class Rectangle implements Figure {}
+export class Triangle implements Figure {
+  shape: Shape = Shape.triangle;
 
-export function getInfo(figure): string {
-  return typeof figure;
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    const max = Math.max(a, b, c);
+    const sum = a + b + c - max;
+
+    if (a <= 0 || b <= 0 || c <= 0 || max >= sum) {
+      throw new Error('sides 1, 2 and 3 can not form a triangle');
+    }
+  }
+
+  getArea(): number {
+    const { a, b, c } = this;
+    const p = (a + b + c) / 2;
+    const s = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+
+    return Math.floor(s * 100) / 100;
+  }
+}
+
+export class Circle implements Figure {
+  shape: Shape = Shape.circle;
+
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error();
+    }
+  }
+
+  getArea(): number {
+    const { radius } = this;
+    const s = Math.PI * Math.pow(radius, 2);
+
+    return Math.floor(s * 100) / 100;
+  }
+}
+
+export class Rectangle implements Figure {
+  shape: Shape = Shape.rectangle;
+
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+  ) {
+    if (a <= 0 || b <= 0) {
+      throw new Error();
+    }
+  }
+
+  getArea(): number {
+    const { a, b } = this;
+    const s = a * b;
+
+    return Math.floor(s * 100) / 100;
+  }
+}
+
+export function getInfo(figure: Triangle | Circle | Rectangle): string {
+  const s = figure.getArea();
+
+  return `A ${figure.color} ${figure.shape} - ${s}`;
 }
