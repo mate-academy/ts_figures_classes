c2a35af0e6b6ef9b167c82916347d50b0590854c
Nikita <2004nnikita@gmail.com>
2024-04-05 21:04:03 +0200

    second

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,45 +1,68 @@
-type Shape = 'triangle' | 'circle' | "rectangle";
+type Shape = 'triangle' | 'circle' | 'rectangle';
 type Color = 'red' | 'green' | 'blue';
 export interface Figure {
-shape :Shape;
-color :Color;
-getArea(): number;
+  shape: Shape;
+  color: Color;
+  getArea(): number;
 }
 
 export class Triangle implements Figure {
   shape: Shape = 'triangle';
-constructor(public color: Color, public a: number, public b: number, public c: number) {
-  if (a <= 0 || b <= 0 || c <= 0) {
-    throw new Error('Length of each side must be greater than 0');
-  }
-  if (a >= b + c || b >= a + c || c >= a + b) {
-    throw new Error('The longest side of a triangle must be less than the sum of the other two sides');
-  }
-}
 
-getArea(): number {
-  const s = (this.a + this.b + this.c) / 2;
-  return Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
-}
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('Length of each side must be greater than 0');
+    }
+
+    if (a >= b + c || b >= a + c || c >= a + b) {
+      throw new Error(
+        'Longest side of a triangle must be less than the sum of other sides',
+      );
+    }
+  }
+
+  getArea(): number {
+    const halfP = (this.a + this.b + this.c) / 2;
+    const s = Math.sqrt(
+      halfP * (halfP - this.a) * (halfP - this.b) * (halfP - this.c),
+    );
+
+    return Math.floor(s * 100) / 100;
+  }
 }
 
 export class Circle implements Figure {
   shape: Shape = 'circle';
-  constructor(public color: Color, public radius: number) {
+
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
     if (radius <= 0) {
       throw new Error('Length of radius must be greater than 0');
     }
   }
 
   getArea(): number {
-      const s = this.radius * this.radius * Math.PI;
-      return s;
+    const s = Math.PI * this.radius * this.radius;
+
+    return Math.floor(s * 100) / 100;
   }
 }
 
 export class Rectangle implements Figure {
   shape: Shape = 'rectangle';
-  constructor(public color: Color, public width: number, public height: number ) {
+
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
     if (width <= 0 || height <= 0) {
       throw new Error('Sides must be greater than 0');
     }
@@ -47,15 +70,11 @@ export class Rectangle implements Figure {
 
   getArea(): number {
     const s = this.width * this.height;
-    return s;
-}
+
+    return Math.floor(s * 100) / 100;
+  }
 }
 
 export function getInfo(figure: Figure): string {
   return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
-
-
-const redRectangle = new Rectangle('red', 3, 5);
-
-console.log(getInfo(redRectangle));
