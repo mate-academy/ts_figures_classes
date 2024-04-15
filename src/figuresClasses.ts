d8da9ae222e65502ad08ba0578f82cc09d0784cc
Mateusz Oślizło <amti4osl@gmail.com>
2024-04-15 14:43:20 +0200

    Solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,114 @@
-export interface Figure {}
+enum Shape {
+  Triangle = 'triangle',
+  Circle = 'circle',
+  Rectangle = 'rectangle',
+}
 
-export class Triangle implements Figure {}
+enum Color {
+  Red = 'red',
+  Green = 'green',
+  Blue = 'blue',
+}
 
-export class Circle implements Figure {}
+function roundToHundreths(area: number): number {
+  return Math.floor(area * 100) / 100;
+}
 
-export class Rectangle implements Figure {}
+export interface Figure {
+  shape: Shape;
+  color: Color;
+  getArea(): number;
+}
 
-export function getInfo(figure): string {
-  return typeof figure;
+export class Triangle implements Figure {
+  shape: Shape;
+
+  constructor(
+    public color: Color,
+    private a: number,
+    private b: number,
+    private c: number,
+  ) {
+    if (a <= 0) {
+      throw new Error('Side a should be positive!');
+    }
+
+    if (b <= 0) {
+      throw new Error('Side b should be positive!');
+    }
+
+    if (c <= 0) {
+      throw new Error('Side c should be positive!');
+    }
+
+    let longestSide = a >= b ? a : b;
+
+    longestSide = c >= longestSide ? c : longestSide;
+
+    if (longestSide >= a + b + c - longestSide) {
+      throw new Error('The longest side is longer than a sum of two others!');
+    }
+
+    this.shape = Shape.Triangle;
+  }
+
+  getArea(): number {
+    const semiperimeter = (this.a + this.b + this.c) / 2;
+
+    return roundToHundreths(
+      Math.sqrt(
+        semiperimeter *
+          (semiperimeter - this.a) *
+          (semiperimeter - this.b) *
+          (semiperimeter - this.c),
+      ),
+    );
+  }
+}
+
+export class Circle implements Figure {
+  shape: Shape;
+
+  constructor(
+    public color: Color,
+    private radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('Radius should be positive!');
+    }
+
+    this.shape = Shape.Circle;
+  }
+
+  getArea(): number {
+    return roundToHundreths(Math.PI * this.radius ** 2);
+  }
+}
+
+export class Rectangle implements Figure {
+  shape: Shape;
+
+  constructor(
+    public color: Color,
+    private width: number,
+    private height: number,
+  ) {
+    if (width <= 0) {
+      throw new Error('Width should be positive!');
+    }
+
+    if (height <= 0) {
+      throw new Error('Height should be positive!');
+    }
+
+    this.shape = Shape.Rectangle;
+  }
+
+  getArea(): number {
+    return roundToHundreths(this.width * this.height);
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return 'A ' + figure.color + ' ' + figure.shape + ' - ' + figure.getArea();
 }
