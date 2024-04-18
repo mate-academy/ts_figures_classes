e2090e0a8610b409fb07d50f38bbdd90d9dc6f1b
Daniil Manakov <daniilmanakov04@gmail.com>
2024-04-18 09:02:12 +0200

    solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,67 @@
-export interface Figure {}
+export interface Figure {
+  shape: string;
+  color: string;
+  getArea(): number;
+}
 
-export class Triangle implements Figure {}
+export class Triangle implements Figure {
+  shape: string = 'triangle';
 
-export class Circle implements Figure {}
+  constructor(
+    public color: string,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0 || a + b <= c || a + c <= b || c + b <= a) {
+      throw new Error('Invalid triangle sides');
+    }
+  }
 
-export class Rectangle implements Figure {}
+  getArea(): number {
+    const s = (this.a + this.b + this.c) / 2;
 
-export function getInfo(figure): string {
-  return typeof figure;
+    return Number(
+      Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c)).toFixed(2),
+    );
+  }
+}
+
+export class Circle implements Figure {
+  shape: string = 'circle';
+
+  constructor(
+    public color: string,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('Invalid circle radius');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor(Math.PI * this.radius ** 2 * 100) / 100;
+  }
+}
+
+export class Rectangle implements Figure {
+  shape: string = 'rectangle';
+
+  constructor(
+    public color: string,
+    public width: number,
+    public heidht: number,
+  ) {
+    if (width <= 0 || heidht <= 0) {
+      throw new Error('Invalid rectangle dimensions');
+    }
+  }
+
+  getArea(): number {
+    return Number((this.width * this.heidht).toFixed(2));
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
