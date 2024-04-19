010356bba48cbee3ddcb223ab38dfe73441278d7
Maria Shmakova <masha.shmakova@gmail.com>
2024-04-19 12:09:53 +0300

    Figures were created

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,82 @@
-export interface Figure {}
+type Shapes = 'triangle' | 'circle' | 'rectangle';
+type Colors = 'red' | 'green' | 'blur';
 
-export class Triangle implements Figure {}
+export interface Figure {
+  shape?: Shapes;
+  color: Colors;
+  getArea(): number;
+}
 
-export class Circle implements Figure {}
+export class Triangle implements Figure {
+  shape: Shapes = 'triangle';
 
-export class Rectangle implements Figure {}
+  constructor(
+    public color: Colors,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('All lengths must be greater than 0');
+    }
 
-export function getInfo(figure): string {
-  return typeof figure;
+    const maxSide = Math.max(a, b, c);
+    const sumOfOtherSides = a + b + c - maxSide;
+
+    if (maxSide >= sumOfOtherSides) {
+      throw new Error(
+        'The longest side must be less than the sum of the other two sides',
+      );
+    }
+  }
+
+  getArea(): number {
+    const s = (this.a + this.b + this.c) / 2;
+    const area = Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+
+    return Math.floor(area * 100) / 100;
+  }
+}
+
+export class Circle implements Figure {
+  shape: Shapes = 'circle';
+
+  constructor(
+    public color: Colors,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('All lengths must be greater than 0');
+    }
+  }
+
+  getArea(): number {
+    const area = Math.PI * this.radius ** 2;
+
+    return Math.floor(area * 100) / 100;
+  }
+}
+
+export class Rectangle implements Figure {
+  shape: Shapes = 'rectangle';
+
+  constructor(
+    public color: Colors,
+    public width: number,
+    public height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('All lengths must be greater than 0');
+    }
+  }
+
+  getArea(): number {
+    const area = +this.width * +this.height;
+
+    return +Math.floor(area * 100) / 100;
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
