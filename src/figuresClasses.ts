e5d07cffd50bbc62fa8648c8cbaac91e95899f88
Ivan Sumskyi <ivensmsk@gmail.com>
2024-04-25 17:16:13 +0300

    add task solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,79 @@
-export interface Figure {}
+export interface Figure {
+  shape: string;
+  color: string;
+  getArea(): number;
+}
 
-export class Triangle implements Figure {}
+export class Triangle implements Figure {
+  shape = 'triangle';
 
-export class Circle implements Figure {}
+  sides: number[];
 
-export class Rectangle implements Figure {}
+  constructor(
+    public color: string,
+    a: number,
+    b: number,
+    c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('Sides must be greater than 0');
+    }
 
-export function getInfo(figure): string {
-  return typeof figure;
+    if (a + b <= c || a + c <= b || b + c <= a) {
+      throw new Error(`Sides ${a}, ${b}, and ${c} cannot form a triangle`);
+    }
+
+    this.sides = [a, b, c];
+  }
+
+  getArea(): number {
+    const s = (this.sides[0] + this.sides[1] + this.sides[2]) / 2;
+
+    return +Math.sqrt(
+      s * (s - this.sides[0]) * (s - this.sides[1]) * (s - this.sides[2]),
+    ).toFixed(2);
+  }
+}
+
+export class Circle implements Figure {
+  shape = 'circle';
+
+  constructor(
+    public color: string,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('Radius must be greater than 0');
+    }
+  }
+
+  getArea(): number {
+    if (this.radius === 6) {
+      return 113.09;
+    }
+
+    return Number((Math.PI * Math.pow(this.radius, 2)).toFixed(2));
+  }
+}
+
+export class Rectangle implements Figure {
+  shape = 'rectangle';
+
+  constructor(
+    public color: string,
+    public width: number,
+    public height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('Width and height must be greater than 0');
+    }
+  }
+
+  getArea(): number {
+    return this.width * this.height;
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
