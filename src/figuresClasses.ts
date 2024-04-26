2814800c0db5d7211df12f4d9bec80ade897d8d3
Prytula Olexandr <liga5872@gmail.com>
2024-04-26 17:09:03 +0300

    add tasl solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,92 @@
-export interface Figure {}
+export interface Figure {
+  shape: string;
+  color: string;
+  getArea(): number;
+}
 
-export class Triangle implements Figure {}
+export class Triangle implements Figure {
+  shape: string = 'triangle';
 
-export class Circle implements Figure {}
+  color: string;
 
-export class Rectangle implements Figure {}
+  constructor(
+    color: string,
+    public sideA: number,
+    public sideB: number,
+    public sideC: number,
+  ) {
+    if (sideA <= 0 || sideB <= 0 || sideC <= 0) {
+      throw new Error('Length of sides should be greater than 0');
+    }
 
-export function getInfo(figure): string {
-  return typeof figure;
+    if (
+      sideA + sideB <= sideC ||
+      sideB + sideC <= sideA ||
+      sideA + sideC <= sideB
+    ) {
+      throw new Error('Sides cannot form a triangle');
+    }
+
+    this.color = color;
+  }
+
+  getArea(): number {
+    const p = (this.sideA + this.sideB + this.sideC) / 2;
+    const areaTriangle = Math.sqrt(
+      p * (p - this.sideA) * (p - this.sideB) * (p - this.sideC),
+    );
+
+    return Math.floor(areaTriangle * 100) / 100;
+  }
+}
+
+export class Circle implements Figure {
+  shape: string = 'circle';
+
+  color: string;
+
+  constructor(
+    color: string,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('Radius should be greater than 0');
+    }
+
+    this.color = color;
+  }
+
+  getArea(): number {
+    const areaCircle = Math.PI * this.radius * this.radius;
+
+    return Math.floor(areaCircle * 100) / 100;
+  }
+}
+
+export class Rectangle implements Figure {
+  shape: string = 'rectangle';
+
+  color: string;
+
+  constructor(
+    color: string,
+    public width: number,
+    public height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('Width and height should be greater than 0');
+    }
+
+    this.color = color;
+  }
+
+  getArea(): number {
+    const areaRectangle = this.width * this.height;
+
+    return Math.floor(areaRectangle * 100) / 100;
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
