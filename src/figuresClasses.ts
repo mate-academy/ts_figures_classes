e842c3b7e13e0966820987c1428ae9a00eb792f8
Higor Pedroso Arruda <higor533@gmail.com>
2024-04-26 23:15:23 -0300

    add task solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,83 +1,94 @@
+export enum Color {
+  Red = 'red',
+  Blue = 'blue',
+  Green = 'green',
+}
+
+export enum Shape {
+  Triangle = 'triangle',
+  Rectangle = 'rectangle',
+  Circle = 'circle',
+}
+
 export interface Figure {
-  shape: 'triangle' | 'circle' | 'rectangle';
-  color: 'red' | 'green' | 'blue';
-  getArea: () => number;
+  shape: Shape;
+  color: Color;
+  getArea(): number;
+}
+
+function isValidTriangle(side1: number, side2: number, side3: number): boolean {
+  const maxSide = Math.max(side1, side2, side3);
+  const sumOfOtherSides = side1 + side2 + side3 - maxSide;
+
+  return maxSide < sumOfOtherSides;
 }
 
 export class Triangle implements Figure {
-  shape: 'triangle' = 'triangle';
-
-  getArea: () => number;
+  shape: Shape = Shape.Triangle;
 
   constructor(
-    public color: 'red' | 'green' | 'blue',
+    public color: Color,
     public a: number,
     public b: number,
     public c: number,
   ) {
-    if (this.a <= 0 || this.b <= 0 || this.c <= 0) {
-      throw new Error('every value in Triangle must be greater than 0');
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('All sides must be a positive numbers');
     }
 
-    if (
-      this.b + this.c <= this.a ||
-      this.c + this.a <= this.b ||
-      this.b + this.a <= this.c
-    ) {
-      throw new Error('figure is not triangle');
+    if (!isValidTriangle(a, b, c)) {
+      throw new Error("This sides can't create a triangle");
     }
+    this.color = color;
+  }
 
-    this.getArea = (): number => {
-      const semi = (this.a + this.b + this.c) * 0.5;
-      const triangleArea = Math.sqrt(
-        (semi - this.a) * (semi - this.b) * (semi - this.c) * semi,
-      );
+  getArea(): number {
+    const halfPerimeter = (this.a + this.b + this.c) / 2;
+    const area = Math.sqrt(
+      halfPerimeter *
+        (halfPerimeter - this.a) *
+        (halfPerimeter - this.b) *
+        (halfPerimeter - this.c),
+    );
 
-      return Math.floor(triangleArea * 100) / 100;
-    };
+    return Math.floor(area * 100) / 100;
   }
 }
 
 export class Circle implements Figure {
-  shape: 'circle' = 'circle';
-
-  getArea: () => number;
+  shape: Shape = Shape.Circle;
 
   constructor(
-    public color: 'red' | 'green' | 'blue',
+    public color: Color,
     public radius: number,
   ) {
-    if (this.radius <= 0) {
-      throw new Error('value in Circle must be greater than 0');
+    if (radius <= 0) {
+      throw new Error('Radius must be a positive numbers');
     }
+  }
 
-    this.getArea = (): number => {
-      const circleArea = this.radius ** 2 * Math.PI;
+  getArea(): number {
+    const area = Math.PI * this.radius ** 2;
 
-      return Math.floor(circleArea * 100) / 100;
-    };
+    return Math.floor(area * 100) / 100;
   }
 }
 
 export class Rectangle implements Figure {
-  shape: 'rectangle' = 'rectangle';
-
-  getArea: () => number;
+  shape: Shape = Shape.Rectangle;
 
   constructor(
-    public color: 'red' | 'green' | 'blue',
+    public color: Color,
     public width: number,
     public height: number,
   ) {
-    if (this.width <= 0 || this.height <= 0) {
-      throw new Error('every value in Rectangle must be greater than 0');
+    if (width <= 0 || height <= 0) {
+      throw new Error('All sides must be a positive numbers');
     }
+  }
 
-    this.getArea = (): number => {
-      const rectangleArea = this.width * this.height;
-
-      return Math.floor(rectangleArea * 100) / 100;
-    };
+  getArea(): number {
+    return Math.floor(this.width * this.height);
   }
 }
 
