3a6a0917a060872d391534f9015172390c5c80d9
marchAlbion <marchAlbion@gmail.com>
2024-04-29 13:55:08 +0300

    Solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -8,16 +8,16 @@ export class Triangle implements Figure {
   readonly shape: Figure['shape'] = 'triangle';
 
   constructor(
-    public color: 'red' | 'green' | 'blue',
+    public color: Figure['color'],
     public a: number,
     public b: number,
     public c: number,
   ) {
-    const sorted: number[] = [a, b, c].sort((el1, el2) => el2 - el1);
-
     if (this.a <= 0 || this.b <= 0 || this.c <= 0) {
       throw new Error('all sides should be positive number');
     }
+
+    const sorted: number[] = [a, b, c].sort((el1, el2) => el2 - el1);
 
     if (sorted[0] >= sorted[1] + sorted[2]) {
       throw new Error(
@@ -32,12 +32,9 @@ export class Triangle implements Figure {
   }
 
   getArea(): number {
-    const semiparam = (this.a + this.b + this.c) / 2;
+    const halfSum = (this.a + this.b + this.c) / 2;
     const area = Math.sqrt(
-      semiparam *
-        (semiparam - this.a) *
-        (semiparam - this.b) *
-        (semiparam - this.c),
+      halfSum * (halfSum - this.a) * (halfSum - this.b) * (halfSum - this.c),
     );
 
     return Math.floor(area * 100) / 100;
@@ -48,7 +45,7 @@ export class Circle implements Figure {
   readonly shape: Figure['shape'] = 'circle';
 
   constructor(
-    public color: 'red' | 'green' | 'blue',
+    public color: Figure['color'],
     public radius: number,
   ) {
     if (this.radius <= 0) {
@@ -69,7 +66,7 @@ export class Rectangle implements Figure {
   readonly shape: Figure['shape'] = 'rectangle';
 
   constructor(
-    public color: 'red' | 'green' | 'blue',
+    public color: Figure['color'],
     public a: number,
     public b: number,
   ) {
