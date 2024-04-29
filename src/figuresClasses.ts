0ba9c50eb77cdab91295eae578c217d10618e792
Vladislav <7evenLays@gmail.com>
2024-04-29 12:16:26 +0300

    fixed comments

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,26 +1,25 @@
+type Shape = 'triangle' | 'circle' | 'rectangle';
+
+type Color = 'red' | 'green' | 'blue';
+
 export interface Figure {
-  shape: string;
-  color: string;
+  shape: Shape;
+  color: Color;
   getArea: () => number;
 }
 
 export class Triangle implements Figure {
-  shape = 'triangle';
+  shape: Shape = 'triangle';
 
   constructor(
-    public color: string,
+    public color: Color,
     public a: number,
     public b: number,
     public c: number,
   ) {
-    const sides = [this.a, this.b, this.c].sort((x, y) => y - x);
+    const sides = [a, b, c].sort((x, y) => y - x);
 
-    if (
-      sides[0] >= sides[1] + sides[2] ||
-      this.a <= 0 ||
-      this.b <= 0 ||
-      this.c <= 0
-    ) {
+    if (sides[0] >= sides[1] + sides[2] || a <= 0 || b <= 0 || c <= 0) {
       throw new Error(
         'Longest side of triangle cant be less than sum of others',
       );
@@ -33,15 +32,15 @@ export class Triangle implements Figure {
       halfP * (halfP - this.a) * (halfP - this.b) * (halfP - this.c),
     );
 
-    return +area.toFixed(2);
+    return Math.floor(area * 100) / 100;
   }
 }
 
 export class Circle implements Figure {
-  shape = 'circle';
+  shape: Shape = 'circle';
 
   constructor(
-    public color: string,
+    public color: Color,
     public radius: number,
   ) {
     if (radius <= 0) {
@@ -57,10 +56,10 @@ export class Circle implements Figure {
 }
 
 export class Rectangle implements Figure {
-  shape = 'rectangle';
+  shape: Shape = 'rectangle';
 
   constructor(
-    public color: string,
+    public color: Color,
     public width: number,
     public height: number,
   ) {
@@ -70,9 +69,9 @@ export class Rectangle implements Figure {
   }
 
   getArea(): number {
-    const area = (this.width * this.height * 100) / 100;
+    const area = this.width * this.height;
 
-    return area;
+    return Math.floor(area * 100) / 100;
   }
 }
 
