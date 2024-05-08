275c29a6cbdb2197eff101d461f6435a0dc18f4a
Ilya Solonetskiy <mr.king101@ukr.net>
2024-05-08 14:28:33 +0300

    Fix Figure Solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -15,19 +15,19 @@ export class Triangle implements Figure {
     public b: number,
     public c: number,
   ) {
-    if (!(a + b > c && b + c > a && a + c > b) || a <= 0 || b <= 0 || c <= 0) {
-      throw new Error('Error');
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('input the sides of the triangle are less than 0');
+    }
+
+    if (!(a + b > c && b + c > a && a + c > b)) {
+      throw new Error('input sides wrong length');
     }
   }
 
   getArea = (): number => {
-    const a = this.a;
-    const b = this.b;
-    const c = this.c;
+    const s = 0.5 * (this.a + this.b + this.c);
 
-    const s = 0.5 * (a + b + c);
-
-    const area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+    const area = Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
 
     return Math.floor(area * 100) / 100;
   };
@@ -46,9 +46,7 @@ export class Circle implements Figure {
   }
 
   getArea = (): number => {
-    const radius = this.radius;
-
-    const area = Math.PI * radius ** 2;
+    const area = Math.PI * this.radius ** 2;
 
     return Math.floor(area * 100) / 100;
   };
@@ -68,10 +66,7 @@ export class Rectangle implements Figure {
   }
 
   getArea = (): number => {
-    const width = this.width;
-    const height = this.height;
-
-    const area = width * height;
+    const area = this.width * this.height;
 
     return Math.floor(area * 100) / 100;
   };
