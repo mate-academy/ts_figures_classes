149364c02be5724e9b7b1b5b9d61269154db4a4b
SrTrace <levashov04sergey@gmail.com>
2024-06-04 14:11:35 +0300

    solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,106 @@
-export interface Figure {}
+type Shape = 'triangle' | 'circle' | 'rectangle';
 
-export class Triangle implements Figure {}
+type Color = 'red' | 'green' | 'blue';
 
-export class Circle implements Figure {}
+export interface Figure {
+  shape: Shape;
+  color: Color;
+  getArea(): number;
+}
 
-export class Rectangle implements Figure {}
+export class Triangle implements Figure {
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+    public shape: Shape = 'triangle',
+  ) {
+    this.color = color;
 
-export function getInfo(figure): string {
-  return typeof figure;
+    function isRequiredSides(
+      side1: number,
+      side2: number,
+      side3: number,
+    ): boolean {
+      const minSide = Math.min(side1, side2, side3);
+      const maxSide = Math.max(side1, side2, side3);
+      const mediumSide = side1 + side2 + side3 - maxSide - minSide;
+
+      if (
+        side1 <= 0 ||
+        side2 <= 0 ||
+        side3 <= 0 ||
+        maxSide >= minSide + mediumSide
+      ) {
+        return false;
+      }
+
+      return true;
+    }
+
+    if (!isRequiredSides(this.a, this.b, this.c)) {
+      throw new Error('not valid sides');
+    }
+
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    this.shape = 'triangle';
+  }
+
+  getArea(): number {
+    const s = (this.a + this.b + this.c) * 0.5;
+    const area = Math.sqrt((s - this.a) * (s - this.b) * (s - this.c) * s);
+
+    return Math.floor(area * 100) / 100;
+  }
+}
+
+export class Circle implements Figure {
+  constructor(
+    public color: Color,
+    public radius: number,
+    public shape: Shape = 'circle',
+  ) {
+    this.shape = 'circle';
+    this.color = color;
+
+    if (radius <= 0) {
+      throw new Error('not valid radius');
+    }
+    this.radius = radius;
+  }
+
+  getArea(): number {
+    return Math.floor(Math.PI * Math.pow(this.radius, 2) * 100) / 100;
+  }
+}
+
+export class Rectangle implements Figure {
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+    public shape: Shape = 'rectangle',
+  ) {
+    this.color = color;
+
+    if (width <= 0 || height <= 0) {
+      throw new Error('not valid sides');
+    }
+    this.width = width;
+    this.height = height;
+    this.shape = 'rectangle';
+  }
+
+  getArea(): number {
+    const area = this.width * this.height;
+
+    return Math.floor(area * 100) / 100;
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
