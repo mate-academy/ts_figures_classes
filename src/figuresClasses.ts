832f94c56ffd247a64cc3fa9055fe12fe9861b0f
Nazarii Siryi <aakkrryyttoo99@gmail.com>
2024-07-21 13:01:07 +0300

    solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,78 @@
-export interface Figure {}
+export interface Figure {
+  shape: 'triangle' | 'circle' | 'rectangle';
+  color: 'red' | 'green' | 'blue';
+  getArea: () => number;
+}
 
-export class Triangle implements Figure {}
+function area(areaRound: number): number {
+  return Math.floor(areaRound * 100) / 100;
+}
 
-export class Circle implements Figure {}
+export class Triangle implements Figure {
+  public shape: Figure['shape'] = 'triangle';
 
-export class Rectangle implements Figure {}
+  constructor(
+    public color: Figure['color'],
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('Enter correct values');
+    }
 
-export function getInfo(figure): string {
-  return typeof figure;
+    if (a + b <= c || b + c <= a || a + c <= b) {
+      throw new Error('Enter correct values');
+    }
+  }
+
+  getArea(): number {
+    const s = (this.a + this.b + this.c) / 2;
+    const areaRound = Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+
+    return area(areaRound);
+  }
+}
+
+export class Circle implements Figure {
+  public shape: Figure['shape'] = 'circle';
+
+  constructor(
+    public color: Figure['color'],
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('Enter correct values');
+    }
+  }
+
+  getArea(): number {
+    const areaRound = Math.PI * this.radius ** 2;
+
+    return area(areaRound);
+  }
+}
+
+export class Rectangle implements Figure {
+  public shape: Figure['shape'] = 'rectangle';
+
+  constructor(
+    public color: Figure['color'],
+    public width: number,
+    public height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('Enter correct values');
+    }
+  }
+
+  getArea(): number {
+    const areaRound = this.width * this.height;
+
+    return area(areaRound);
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
