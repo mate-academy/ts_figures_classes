07ef2a87d18f5228cb2eaa540ae2b2a4e83edfe5
Viktoriya Riabets <vikamalik30@gmail.com>
2024-07-24 16:16:49 -0400

    Solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,104 @@
-export interface Figure {}
+interface Figure {
+  shape: 'triangle' | 'circle' | 'rectangle';
+  color: 'red' | 'green' | 'blue';
+  getArea(): number;
+}
 
-export class Triangle implements Figure {}
+function roundToHundredths(num: number): number {
+  return Math.floor(num * 100) / 100;
+}
 
-export class Circle implements Figure {}
+export class Triangle implements Figure {
+  shape: 'triangle';
 
-export class Rectangle implements Figure {}
+  color: 'red' | 'green' | 'blue';
 
-export function getInfo(figure): string {
-  return typeof figure;
+  a: number;
+
+  b: number;
+
+  c: number;
+
+  constructor(
+    color: 'red' | 'green' | 'blue',
+    a: number,
+    b: number,
+    c: number,
+  ) {
+    this.shape = 'triangle';
+    this.color = color;
+    this.a = a;
+    this.b = b;
+    this.c = c;
+
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('Side lengths must be greater than zero');
+    }
+
+    if (a + b <= c || a + c <= b || b + c <= a) {
+      throw new Error(`Sides ${a}, ${b} and ${c} can't form a triangle`);
+    }
+  }
+
+  getArea(): number {
+    const s = (this.a + this.b + this.c) / 2;
+    const area = Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+
+    return roundToHundredths(area);
+  }
+}
+
+export class Circle implements Figure {
+  shape: 'circle';
+
+  color: 'red' | 'green' | 'blue';
+
+  radius: number;
+
+  constructor(color: 'red' | 'green' | 'blue', radius: number) {
+    this.shape = 'circle';
+    this.color = color;
+    this.radius = radius;
+
+    if (radius <= 0) {
+      throw new Error('Radius must be greater than zero');
+    }
+  }
+
+  getArea(): number {
+    const area = Math.PI * this.radius * this.radius;
+
+    return roundToHundredths(area);
+  }
+}
+
+export class Rectangle implements Figure {
+  shape: 'rectangle';
+
+  color: 'red' | 'green' | 'blue';
+
+  width: number;
+
+  height: number;
+
+  constructor(color: 'red' | 'green' | 'blue', width: number, height: number) {
+    this.shape = 'rectangle';
+    this.color = color;
+    this.width = width;
+    this.height = height;
+
+    if (width <= 0 || height <= 0) {
+      throw new Error('Width and height must be greater than zero');
+    }
+  }
+
+  getArea(): number {
+    const area = this.width * this.height;
+
+    return roundToHundredths(area);
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
