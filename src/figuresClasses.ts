2cb3061cd6075584cfc88e0cbc20f252ae364395
Kate Marchuk <pion3219@gmail.com>
2024-08-05 20:05:40 +0300

    Added solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,78 @@
-export interface Figure {}
+export interface Figure {
+  shape: 'triangle' | 'circle' | 'rectangle';
+  color: 'red' | 'green' | 'blue';
+  getArea: Function;
+}
 
-export class Triangle implements Figure {}
+export class Triangle implements Figure {
+  shape = 'triangle' as const;
 
-export class Circle implements Figure {}
+  constructor(
+    public color: Figure['color'],
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (Math.min(a, b, c) < 0) {
+      throw new Error('your error message');
+    }
 
-export class Rectangle implements Figure {}
+    const [min, mid, max] = [a, b, c].sort((x, y) => x - y);
 
-export function getInfo(figure): string {
-  return typeof figure;
+    if (max >= mid + min) {
+      throw new Error('your error message');
+    }
+  }
+
+  getArea(): number {
+    const semiperimeter: number = (this.a + this.b + this.c) / 2;
+
+    return Number(
+      Math.sqrt(
+        semiperimeter *
+          (semiperimeter - this.a) *
+          (semiperimeter - this.b) *
+          (semiperimeter - this.c),
+      ).toFixed(2),
+    );
+  }
+}
+
+export class Circle implements Figure {
+  shape = 'circle' as const;
+
+  constructor(
+    public color: Figure['color'],
+    public radius: number,
+  ) {
+    if (radius < 0) {
+      throw new Error('your error message');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor(Math.PI * Math.pow(this.radius, 2) * 100) / 100;
+  }
+}
+
+export class Rectangle implements Figure {
+  shape = 'rectangle' as const;
+
+  constructor(
+    public color: Figure['color'],
+    public width: number,
+    public height: number,
+  ) {
+    if (Math.min(width, height) < 0) {
+      throw new Error('your error message');
+    }
+  }
+
+  getArea(): number {
+    return Number((this.width * this.height).toFixed(2));
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
