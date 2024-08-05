87b1bcf6fce8aa6a6f6261bcccadc03dc7c7ac9a
YaroslavKolesnyk <kolesnik.y2004@gmail.com>
2024-08-05 16:08:08 +0300

    add task solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,77 @@
-export interface Figure {}
+export interface Figure {
+  color: string;
+  getArea(): number;
+}
 
-export class Triangle implements Figure {}
+export class Triangle implements Figure {
+  public shape: string = 'triangle';
 
-export class Circle implements Figure {}
+  constructor(
+    public color: string,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('your error message');
+    }
 
-export class Rectangle implements Figure {}
+    if (a + b <= c || a + c <= b || c + b <= a) {
+      throw new Error('your error message');
+    }
+  }
 
-export function getInfo(figure): string {
-  return typeof figure;
+  getArea(): number {
+    const s = (this.a + this.b + this.c) / 2;
+    const result = Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+
+    return Math.round(result * 100) / 100;
+  }
+}
+
+export class Circle implements Figure {
+  public shape: string = 'circle';
+
+  constructor(
+    public color: string,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('your error message');
+    }
+  }
+
+  getArea(): number {
+    const result = Math.PI * this.radius ** 2;
+
+    return Math.floor(result * 100) / 100;
+  }
+}
+
+export class Rectangle implements Figure {
+  public shape: string = 'rectangle';
+
+  constructor(
+    public color: string,
+    public width: number,
+    public height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('your error message');
+    }
+  }
+
+  getArea(): number {
+    const result = this.width * this.height;
+
+    return Math.round(result * 100) / 100;
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  const color = figure.color;
+  const result = figure.getArea();
+  const name = figure.constructor.name.toLowerCase();
+
+  return `A ${color} ${name} - ${result}`;
 }
