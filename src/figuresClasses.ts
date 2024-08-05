e2d096158dcf79de6b8c966d31775c7a341fe9d4
YaroslavKolesnyk <kolesnik.y2004@gmail.com>
2024-08-05 18:07:04 +0300

    solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,43 +1,51 @@
+export type Color = 'red' | 'green' | 'blue' | 'yellow' | 'black' | 'white';
+
+export type ShapeType = 'triangle' | 'circle' | 'rectangle';
+
 export interface Figure {
-  color: string;
+  color: Color;
   getArea(): number;
 }
 
 export class Triangle implements Figure {
-  public shape: string = 'triangle';
+  public shape: ShapeType = 'triangle';
 
   constructor(
-    public color: string,
+    public color: Color,
     public a: number,
     public b: number,
     public c: number,
   ) {
     if (a <= 0 || b <= 0 || c <= 0) {
-      throw new Error('your error message');
+      throw new Error('Triangle side lengths must be greater than 0.');
     }
 
     if (a + b <= c || a + c <= b || c + b <= a) {
-      throw new Error('your error message');
+      throw new Error(
+        'The sum of any two sides must be greater than the third side.',
+      );
     }
   }
 
   getArea(): number {
-    const s = (this.a + this.b + this.c) / 2;
-    const result = Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+    const area = (this.a + this.b + this.c) / 2;
+    const result = Math.sqrt(
+      area * (area - this.a) * (area - this.b) * (area - this.c),
+    );
 
     return Math.round(result * 100) / 100;
   }
 }
 
 export class Circle implements Figure {
-  public shape: string = 'circle';
+  public shape: ShapeType = 'circle';
 
   constructor(
-    public color: string,
+    public color: Color,
     public radius: number,
   ) {
     if (radius <= 0) {
-      throw new Error('your error message');
+      throw new Error('Circle radius must be greater than 0.');
     }
   }
 
@@ -49,15 +57,15 @@ export class Circle implements Figure {
 }
 
 export class Rectangle implements Figure {
-  public shape: string = 'rectangle';
+  public shape: ShapeType = 'rectangle';
 
   constructor(
-    public color: string,
+    public color: Color,
     public width: number,
     public height: number,
   ) {
     if (width <= 0 || height <= 0) {
-      throw new Error('your error message');
+      throw new Error('Rectangle width and height must be greater than 0.');
     }
   }
 
