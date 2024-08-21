3ec043382f82db86326ceb521af5986989829075
Tanya Baletska <t.baletska@gmail.com>
2024-08-21 18:52:17 +0300

    add types

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -4,10 +4,13 @@ export interface Figure {
   getArea(): number;
 }
 
-export class Triangle implements Figure {
-  shape: string = 'triangle';
+type Color = 'red' | 'green' | 'blue';
+type Shape = 'triangle' | 'circle' | 'rectangle';
 
-  color: string;
+export class Triangle implements Figure {
+  shape: Shape = 'triangle';
+
+  color: Color;
 
   private a: number;
 
@@ -15,7 +18,7 @@ export class Triangle implements Figure {
 
   private c: number;
 
-  constructor(color: string, a: number, b: number, c: number) {
+  constructor(color: Color, a: number, b: number, c: number) {
     if (a <= 0 || b <= 0 || c <= 0) {
       throw new Error('Side lengths must be greater than 0');
     }
@@ -41,13 +44,13 @@ export class Triangle implements Figure {
 }
 
 export class Circle implements Figure {
-  shape: string = 'circle';
+  shape: Shape = 'circle';
 
-  color: string;
+  color: Color;
 
   private radius: number;
 
-  constructor(color: string, radius: number) {
+  constructor(color: Color, radius: number) {
     if (radius <= 0) {
       throw new Error('Radius must be greater than 0');
     }
@@ -64,15 +67,15 @@ export class Circle implements Figure {
 }
 
 export class Rectangle implements Figure {
-  shape: string = 'rectangle';
+  shape: Shape = 'rectangle';
 
-  color: string;
+  color: Color;
 
   private width: number;
 
   private height: number;
 
-  constructor(color: string, width: number, height: number) {
+  constructor(color: Color, width: number, height: number) {
     if (width <= 0 || height <= 0) {
       throw new Error('Width and height must be greater than 0');
     }
