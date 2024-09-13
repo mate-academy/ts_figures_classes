32aef4e56e26240412ea2cfb193c5d5f3e0e0b17
luter <newgglutt@gmail.com>
2024-09-13 23:25:55 +0300

    sips

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,80 @@
-export interface Figure {}
+import { get } from 'http';
 
-export class Triangle implements Figure {}
+interface Figure {
+  getArea(): number;
+  getInfo(): string;
+}
 
-export class Circle implements Figure {}
+class Triangle implements Figure {
+  constructor(
+    private color: string,
+    private a: number,
+    private b: number,
+    private c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('Side length cannot be zero or negative');
+    }
 
-export class Rectangle implements Figure {}
+    if (a + b <= c || a + c <= b || b + c <= a) {
+      throw new Error(`Sides ${a}, ${b}, and ${c} can't form a triangle`);
+    }
+  }
 
-export function getInfo(figure): string {
-  return typeof figure;
+  getArea(): number {
+    const s = (this.a + this.b + this.c) / 2;
+
+    return (
+      Math.round(
+        Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c)) * 100,
+      ) / 100
+    );
+  }
+
+  getInfo(): string {
+    return `${this.color} triangle - ${this.getArea()}`;
+  }
+}
+
+class Circle implements Figure {
+  constructor(
+    private color: string,
+    private radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('Radius cannot be zero or negative');
+    }
+  }
+
+  getArea(): number {
+    return Math.round(Math.PI * this.radius * this.radius * 100) / 100;
+  }
+
+  getInfo(): string {
+    return `${this.color} circle - ${this.getArea()}`;
+  }
+}
+
+class Rectangle implements Figure {
+  constructor(
+    private color: string,
+    private width: number,
+    private height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('Width and height must be positive numbers');
+    }
+  }
+
+  getArea(): number {
+    return Math.round(this.width * this.height * 100) / 100;
+  }
+
+  getInfo(): string {
+    return `${this.color} rectangle - ${this.getArea()}`;
+  }
+}
+
+function getInfo(figure: Figure): string {
+  return figure.getInfo();
 }
