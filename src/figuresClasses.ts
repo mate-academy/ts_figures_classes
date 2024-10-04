b5e919dc12da58b50b04c9dcf67bb10334d5ede9
Oleh Senkiv <o.senkiv.v@gmail.com>
2024-10-05 02:20:47 +0300

    Created figures classes

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,84 @@
-export interface Figure {}
+export interface Figure {
+  shape: 'triangle' | 'circle' | 'rectangle';
+  color: 'red' | 'green' | 'blue';
+}
 
-export class Triangle implements Figure {}
+function isTriangle(a: number, b: number, c: number): boolean {
+  if (a + b <= c || a + c <= b || b + c <= a) {
+    return true;
+  }
 
-export class Circle implements Figure {}
+  return false;
+}
 
-export class Rectangle implements Figure {}
+export class Triangle implements Figure {
+  constructor(
+    public color: Figure,
+    public a: number,
+    public b: number,
+    public c: number,
+    public shape,
+  ) {
+    this.shape = 'triangle';
 
-export function getInfo(figure): string {
-  return typeof figure;
+    if (this.a <= 0 || this.b <= 0 || this.c <= 0) {
+      throw new Error('errMessage');
+    }
+
+    if (isTriangle(this.a, this.b, this.c)) {
+      throw new Error('errMessage');
+    }
+  }
+
+  getArea(): number | never {
+    const p = (this.a + this.b + this.c) / 2;
+    const square = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+
+    return Math.floor(square * 100) / 100;
+  }
+}
+
+export class Circle implements Figure {
+  constructor(
+    public color: Figure,
+    public radius: number,
+    public shape,
+  ) {
+    this.shape = 'circle';
+
+    if (this.radius <= 0) {
+      throw new Error('errMessage');
+    }
+  }
+
+  getArea(): number | never {
+    const square = Math.PI * this.radius * this.radius;
+
+    return Math.floor(square * 100) / 100;
+  }
+}
+
+export class Rectangle implements Figure {
+  constructor(
+    public color: Figure,
+    public width: number,
+    public height: number,
+    public shape,
+  ) {
+    this.shape = 'rectangle';
+
+    if (this.width <= 0 || this.height <= 0) {
+      throw new Error('errMessage');
+    }
+  }
+
+  getArea(): number {
+    const square = this.width * this.height;
+
+    return Math.floor(square * 100) / 100;
+  }
+}
+
+export function getInfo(figure: Triangle | Circle | Rectangle): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
