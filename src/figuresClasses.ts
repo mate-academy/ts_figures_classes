096a69f4c81f3f1aaf38dc2ec518e0bac31a20ad
Anton Tkachuk <tkacuk2291@gmail.com>
2025-02-12 20:22:50 +0200

    olution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,81 @@
-export interface Figure {}
+type Shape = 'triangle' | 'circle' | 'rectangle';
+type Color = 'red' | 'green' | 'blue';
 
-export class Triangle implements Figure {}
+export interface Figure {
+  shape: Shape;
+  color: Color;
+  getArea: Function;
+}
 
-export class Circle implements Figure {}
+export class Triangle implements Figure {
+  public shape: Shape = 'triangle';
 
-export class Rectangle implements Figure {}
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (this.a <= 0 || this.b <= 0 || this.c <= 0) {
+      throw new Error('Side lengths must be grow than 0');
+    }
 
-export function getInfo(figure): string {
-  return typeof figure;
+    const maxNum = Math.max(this.a, this.b, this.c);
+    const sumOthers = this.a + this.b + this.c - maxNum;
+
+    if (sumOthers <= maxNum) {
+      throw new Error(
+        'The longest side of the triangle is >= ' +
+        'than the sum of the other two sides',
+      );
+    }
+  }
+
+  getArea(): number {
+    const s: number = (this.a + this.b + this.c) / 2;
+    const area = Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+
+    return +area.toFixed(2);
+  }
+}
+
+export class Circle implements Figure {
+  public shape: Shape = 'circle';
+
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (this.radius <= 0) {
+      throw new Error('radius must be grow than 0');
+    }
+  }
+
+  getArea(): number {
+    const area = Math.PI * Math.pow(this.radius, 2);
+
+    return Math.floor(area * 100) / 100;
+  }
+}
+
+export class Rectangle implements Figure {
+  public shape: Shape = 'rectangle';
+
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
+    if (this.width <= 0 || this.height <= 0) {
+      throw new Error('width and height must be grow than 0');
+    }
+  }
+
+  getArea(): number {
+    return this.height * this.width;
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
