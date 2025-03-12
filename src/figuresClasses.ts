447eee78f9b31845ef5d2f37a1863b2611fd8c21
Daniil Trusov <daniiltrusov00@gmail.com>
2025-03-12 16:24:45 +0200

    create solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,93 @@
-export interface Figure {}
+enum Shape {
+  Triangle = 'triangle',
+  Circle = 'circle',
+  Rectangle = 'rectangle',
+}
 
-export class Triangle implements Figure {}
+enum Color {
+  Red = 'red',
+  Green = 'green',
+  Blue = 'blue',
+}
 
-export class Circle implements Figure {}
+export interface Figure {
+  shape: Shape;
+  color: Color;
+  getArea(): number;
+}
 
-export class Rectangle implements Figure {}
+function roundToHundreds(num: number): number {
+  return Math.floor(num * 100) / 100;
+}
 
-export function getInfo(figure): string {
-  return typeof figure;
+export class Triangle implements Figure {
+  readonly shape = Shape.Triangle;
+
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('Triangle sides must be > 0');
+    }
+
+    const longest = Math.max(a, b, c);
+
+    if (longest >= a + b + c - longest) {
+      throw new Error('Triangle sides must be shorter then sum of other sides');
+    }
+  }
+
+  public getArea(): number {
+    const s = (this.a + this.b + this.c) * 0.5;
+
+    const area = Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+
+    return roundToHundreds(area);
+  }
+}
+
+export class Circle implements Figure {
+  readonly shape = Shape.Circle;
+
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('Circle radius must be > 0');
+    }
+  }
+
+  public getArea(): number {
+    const area = Math.PI * this.radius ** 2;
+
+    return roundToHundreds(area);
+  }
+}
+
+export class Rectangle implements Figure {
+  readonly shape = Shape.Rectangle;
+
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('Rectangle sides must be > 0');
+    }
+  }
+
+  public getArea(): number {
+    const area = this.width * this.height;
+
+    return roundToHundreds(area);
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
