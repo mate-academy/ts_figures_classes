b96ad7f57559cee3e0badfdf39a63138f4096167
Olha Shtyfliuk <149540630+fwrwxx@users.noreply.github.com>
2025-04-28 14:14:35 +0300

    solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,93 @@
-export interface Figure {}
+export interface Figure {
+  color: string;
+  getArea(): number;
+}
 
-export class Triangle implements Figure {}
+export class Triangle implements Figure {
+  sides: number[];
 
-export class Circle implements Figure {}
+  public color: string;
 
-export class Rectangle implements Figure {}
+  public shape: string = 'triangle';
 
-export function getInfo(figure): string {
-  return typeof figure;
+  constructor(sides: number[], color: string) {
+    this.sides = sides;
+    this.color = color;
+
+    if (
+      sides.length !== 3 ||
+      this.sides[0] <= 0 ||
+      this.sides[1] <= 0 ||
+      this.sides[2] <= 0 ||
+      this.sides[0] + this.sides[1] <= this.sides[2] ||
+      this.sides[0] + this.sides[2] <= this.sides[1] ||
+      this.sides[1] + this.sides[2] <= this.sides[0]
+    ) {
+      throw new Error('Triangle must have 3 sides');
+    }
+  }
+
+  getArea(): number {
+    const s = this.sides.reduce((a, b) => a + b) / 2;
+
+    // eslint-disable-next-line max-len
+    const area = Math.sqrt(
+      s * (s - this.sides[0]) * (s - this.sides[1]) * (s - this.sides[2]),
+    );
+
+    return parseFloat(area.toFixed(2));
+  }
+}
+
+export class Circle implements Figure {
+  radius: number;
+
+  color: string;
+
+  shape: string = 'circle';
+
+  constructor(radius: number, color: string) {
+    this.radius = radius;
+    this.color = color;
+
+    if (radius <= 0) {
+      throw new Error('Circle must have a positive radius');
+    }
+  }
+
+  getArea(): number {
+    const area = Math.PI * Math.pow(this.radius, 2);
+
+    return parseFloat(area.toFixed(2));
+  }
+}
+
+export class Rectangle implements Figure {
+  width: number;
+
+  height: number;
+
+  color: string;
+
+  shape: string = 'rectangle';
+
+  constructor(width: number, height: number, color: string) {
+    this.width = width;
+    this.height = height;
+    this.color = color;
+
+    if (width <= 0 || height <= 0) {
+      throw new Error('Rectangle must have positive width and height');
+    }
+  }
+
+  getArea(): number {
+    const area = this.width * this.height;
+
+    return parseFloat(area.toFixed(2));
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea().toFixed(2)}`;
 }
