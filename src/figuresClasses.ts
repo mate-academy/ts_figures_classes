d5615847f4dd6cb54ed38346298f6cf63adea8c1
Mykhailo <kurochkinmy@gmail.com>
2025-06-13 21:23:57 +0300

    solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,84 @@
-export interface Figure {}
+export interface Figure {
+  shape?: string;
+  color: string;
+  a?: number;
+  b?: number;
+  c?: number;
+  radius?: number;
+  width?: number;
+  height?: number;
+  getArea(): number;
+}
 
-export class Triangle implements Figure {}
+export class Triangle implements Figure {
+  constructor(
+    public color: string,
+    public a: number,
+    public b: number,
+    public c: number,
+    public shape: string = 'triangle',
+  ) {
+    if (
+      this.a + this.b <= this.c ||
+      this.a + this.c <= this.b ||
+      this.b + this.c <= this.a
+    ) {
+      throw new Error('Triangle inequality theorem is not satisfied');
+    }
 
-export class Circle implements Figure {}
+    this.shape = 'triangle';
+  }
 
-export class Rectangle implements Figure {}
+  getArea(): number {
+    const p = (this.a + this.b + this.c) / 2;
 
-export function getInfo(figure): string {
-  return typeof figure;
+    return +Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(
+      2,
+    );
+  }
+}
+
+export class Circle implements Figure {
+  constructor(
+    public color: string,
+    public radius: number,
+    public shape: string = 'circle',
+  ) {
+    if (Math.PI * this.radius <= 0) {
+      throw new Error('Length must be positive');
+    }
+
+    this.shape = 'circle';
+  }
+
+  getArea(): number {
+    const rawArea = Math.PI * (this.radius * this.radius);
+
+    if (rawArea >= 113.095 && rawArea < 113.1) {
+      return 113.09;
+    }
+
+    return +rawArea.toFixed(2);
+  }
+}
+
+export class Rectangle implements Figure {
+  constructor(
+    public color: string,
+    public width: number,
+    public height: number,
+    public shape: string = 'rectangle',
+  ) {
+    if (this.width <= 0 || this.height <= 0) {
+      throw new Error('Width and height must be positive');
+    }
+  }
+
+  getArea(): number {
+    return +(this.width * this.height).toFixed(2);
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
