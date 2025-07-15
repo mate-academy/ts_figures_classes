f7a8ddb8113852cc0151ee671b42c570f889033b
Nikita Doroshenko <nikitokd05@gmail.com>
2025-07-15 20:22:25 +0300

    fix method getArea in class Rectangle

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -73,7 +73,7 @@ export class Rectangle implements Figure {
   }
 
   getArea(): number {
-    return (this.width * this.height * 100) / 100;
+    return Math.floor(this.width * this.height * 100) / 100;
   }
 }
 
