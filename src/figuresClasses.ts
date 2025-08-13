81579d6c0d5282b20b7673838a3214376bd8bca5
Eksonurit <fizervitalij897@gmail.com>
2025-08-14 00:14:06 +0300

    add solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,98 @@
-export interface Figure {}
+export interface Figure {
+  shape: 'triangle' | 'circle' | 'rectangle';
+  color: 'red' | 'green' | 'blue';
+}
 
-export class Triangle implements Figure {}
+export class Triangle implements Figure {
+  public shape: 'triangle' = 'triangle';
 
-export class Circle implements Figure {}
+  public color: 'red' | 'green' | 'blue';
 
-export class Rectangle implements Figure {}
+  public a: number;
+
+  public b: number;
+
+  public c: number;
+
+  constructor(
+    color: 'red' | 'green' | 'blue',
+    a: number,
+    b: number,
+    c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('invalid side length');
+    }
+
+    const longest = Math.max(a, b, c);
+
+    if (longest >= a + b + c - longest) {
+      throw new Error('invalid triangle');
+    }
+
+    this.color = color;
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+
+  getArea(): number {
+    const s = (this.a + this.b + this.c) / 2;
+    const area = Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+
+    return Math.floor(area * 100) / 100;
+  }
+}
+
+export class Circle implements Figure {
+  public shape: 'circle' = 'circle';
+
+  public color: 'red' | 'green' | 'blue';
+
+  public radius: number;
+
+  constructor(color: 'red' | 'green' | 'blue', radius: number) {
+    if (radius <= 0) {
+      throw new Error('invalid radius');
+    }
+
+    this.color = color;
+    this.radius = radius;
+  }
+
+  getArea(): number {
+    const area = Math.PI * (this.radius * this.radius);
+
+    return Math.floor(area * 100) / 100;
+  }
+}
+
+export class Rectangle implements Figure {
+  public shape: 'rectangle' = 'rectangle';
+
+  public color: 'red' | 'green' | 'blue';
+
+  public a: number;
+
+  public b: number;
+
+  constructor(color: 'red' | 'green' | 'blue', a: number, b: number) {
+    if (a <= 0 || b <= 0) {
+      throw new Error('invalid length');
+    }
+
+    this.color = color;
+    this.a = a;
+    this.b = b;
+  }
+
+  getArea(): number {
+    const area = this.a * this.b;
+
+    return Math.floor(area * 100) / 100;
+  }
+}
 
 export function getInfo(figure): string {
-  return typeof figure;
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
