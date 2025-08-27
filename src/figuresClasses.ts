f0703c74feeb894c73bc2ade11fb5da8ac2fefe7
Uandrik Laptop <uandriy4141@gmail.com>
2025-08-28 06:24:58 +1400

    Solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,67 @@
-export interface Figure {}
+type Shape = 'triangle' | 'circle' | 'rectangle';
+type Color = 'red' | 'green' | 'blue';
+export interface Figure {
+  shape: Shape;
+  color: Color;
+  getArea(): number;
+}
 
-export class Triangle implements Figure {}
+export class Triangle implements Figure {
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+    public shape: Shape = 'triangle',
+  ) {
+    const longestSide = Math.max(a, b, c);
+    const twoOthers = [a, b, c]
+      .filter((n) => n !== longestSide)
+      .reduce((acc, n) => acc + n, 0);
 
-export class Circle implements Figure {}
+    if ([a, b, c].some((l) => l <= 0) || longestSide >= twoOthers) {
+      throw new Error('your error message');
+    }
+  }
 
-export class Rectangle implements Figure {}
+  getArea(): number {
+    return (this.a + this.b + this.c) / 2;
+  }
+}
 
-export function getInfo(figure): string {
-  return typeof figure;
+export class Circle implements Figure {
+  constructor(
+    public color: Color,
+    public radius: number,
+    public shape: Shape = 'circle',
+  ) {
+    if (radius <= 0) {
+      throw new Error('your error message');
+    }
+  }
+
+  getArea(): number {
+    return Math.PI * this.radius ** 2;
+  }
+}
+
+export class Rectangle implements Figure {
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+    public shape: Shape = 'rectangle',
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('yor error message');
+    }
+  }
+
+  getArea(): number {
+    return this.width * this.height;
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
