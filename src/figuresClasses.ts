4a70d3fceefbfd8fb955a2764485dc9ad9aa8eb5
voidmain69 <photoreznichenko@gmail.com>
2025-09-10 16:21:50 +0300

    solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,87 @@
-export interface Figure {}
+enum Color {
+  Red = 'red',
+  Green = 'green',
+  Blue = 'blue',
+}
 
-export class Triangle implements Figure {}
+enum Shape {
+  Triangle = 'triangle',
+  Circle = 'circle',
+  Rectangle = 'rectangle',
+}
 
-export class Circle implements Figure {}
+function round2(value: number): number {
+  return Math.floor(value * 100) / 100;
+}
 
-export class Rectangle implements Figure {}
+export interface Figure {
+  shape: Shape;
+  color: Color;
+  getArea(): number;
+}
 
-export function getInfo(figure): string {
-  return typeof figure;
+export class Triangle implements Figure {
+  shape: Shape = Shape.Triangle;
+
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (
+      this.a + this.b <= this.c ||
+      this.a + this.c <= this.b ||
+      this.b + this.c <= this.a
+    ) {
+      throw new Error('Трикутник з такими сторонами не існує');
+    }
+  }
+
+  getArea(): number {
+    const p = (this.a + this.b + this.c) / 2;
+
+    return round2(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)));
+  }
+}
+
+export class Circle implements Figure {
+  shape: Shape = Shape.Circle;
+
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('Радіус має бути додатним числом');
+    }
+  }
+
+  getArea(): number {
+    return round2(Math.PI * this.radius * this.radius);
+  }
+}
+
+export class Rectangle implements Figure {
+  shape: Shape = Shape.Rectangle;
+
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('Ширина та висота мають бути додатними числами');
+    }
+  }
+
+  getArea(): number {
+    const area = this.width * this.height;
+
+    return round2(area);
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
