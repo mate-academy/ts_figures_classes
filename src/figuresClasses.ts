8f84d97917881ed3021fc0b819d5bb3ab4e2f12b
Oleksii Kyslytsyn <kislitsyn.aleksey@gmail.com>
2025-09-17 17:38:29 +0300

    add task solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,98 @@
-export interface Figure {}
+type Shape = 'triangle' | 'circle' | 'rectangle';
+type Color = 'red' | 'green' | 'blue';
 
-export class Triangle implements Figure {}
+export interface Figure {
+  shape: Shape;
+  color: Color;
 
-export class Circle implements Figure {}
+  getArea(): number;
+}
 
-export class Rectangle implements Figure {}
+export class Triangle implements Figure {
+  constructor(color: Color, a: number, b: number, c: number) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('The lengths of the sides must be positive.');
+    }
 
-export function getInfo(figure): string {
-  return typeof figure;
+    const maxSide = Math.max(a, b, c);
+
+    if (maxSide >= a + b + c - maxSide) {
+      throw new Error(`Sides ${a}, ${b} and ${c} cannot form a triangle.`);
+    }
+
+    this.color = color;
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+
+  shape: Shape = 'triangle';
+
+  color: Color;
+
+  private a: number;
+
+  private b: number;
+
+  private c: number;
+
+  getArea(): number {
+    const p: number = (this.a + this.b + this.c) / 2;
+    const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+
+    return Math.floor(area * 100) / 100;
+  }
+}
+
+export class Circle implements Figure {
+  constructor(color: Color, radius: number) {
+    if (radius <= 0) {
+      throw new Error('The radius must be positive.');
+    }
+
+    this.color = color;
+    this.radius = radius;
+  }
+
+  shape: Shape = 'circle';
+
+  color: Color;
+
+  private radius: number;
+
+  getArea(): number {
+    const area = Math.PI * this.radius ** 2;
+
+    return Math.floor(area * 100) / 100;
+  }
+}
+
+export class Rectangle implements Figure {
+  constructor(color: Color, width: number, height: number) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('Width and height must be positive.');
+    }
+
+    this.color = color;
+    this.width = width;
+    this.height = height;
+  }
+
+  shape: Shape = 'rectangle';
+
+  color: Color;
+
+  private width: number;
+
+  private height: number;
+
+  getArea(): number {
+    const area = this.width * this.height;
+
+    return Math.floor(area * 100) / 100;
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
