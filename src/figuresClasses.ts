d0c910361141f5fcfe87ca400a47391e6069a3bf
Pavlo <marlana08022022@gmail.com>
2025-09-23 22:59:57 +0300

    done

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -7,17 +7,11 @@ export interface Figure {
 export class Triangle implements Figure {
   shape: 'triangle' = 'triangle';
 
-  a: number;
-
-  b: number;
-
-  c: number;
-
   constructor(
     public color: 'red' | 'green' | 'blue',
-    a: number,
-    b: number,
-    c: number,
+    public a: number,
+    public b: number,
+    public c: number,
   ) {
     if (a <= 0 || b <= 0 || c <= 0) {
       throw new Error('Triangle sides must be greater than 0');
@@ -26,10 +20,6 @@ export class Triangle implements Figure {
     if (a + b <= c || a + c <= b || b + c <= a) {
       throw new Error(`Sides ${a}, ${b}, ${c} can't form a triangle`);
     }
-    this.color = color;
-    this.a = a;
-    this.b = b;
-    this.c = c;
   }
 
   getArea(): number {
@@ -51,12 +41,10 @@ export class Circle implements Figure {
     if (radius <= 0) {
       throw new Error('circle radius must be greater than 0');
     }
-    this.color = color;
-    this.radius = radius;
   }
 
   getArea(): number {
-    return Math.floor(Math.PI * this.radius * this.radius * 100) / 100;
+    return Math.round(Math.PI * this.radius * this.radius * 100) / 100;
   }
 }
 
@@ -71,9 +59,6 @@ export class Rectangle implements Figure {
     if (width <= 0 || height <= 0) {
       throw new Error('Rectangle sides must be greater than 0');
     }
-    this.color = color;
-    this.width = width;
-    this.height = height;
   }
 
   getArea(): number {
