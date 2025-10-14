d42f84504b36275170392955c878d8a120241293
Nazar <nazar.shevchuk.121@gmail.com>
2025-10-14 12:19:33 +0300

    sol1

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,97 @@
-export interface Figure {}
+type ShapeType = 'triangle' | 'circle' | 'rectangle';
+type ColorType = 'blue' | 'red' | 'green';
 
-export class Triangle implements Figure {}
+export interface Figure {
+  color: ColorType;
+  shape: ShapeType;
+  getArea(): number;
+}
 
-export class Circle implements Figure {}
+export class Triangle implements Figure {
+  public color: ColorType;
 
-export class Rectangle implements Figure {}
+  public a: number;
 
-export function getInfo(figure): string {
-  return typeof figure;
+  public b: number;
+
+  public c: number;
+
+  public shape: ShapeType = 'triangle';
+
+  constructor(color: ColorType, a: number, b: number, c: number) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    this.color = color;
+
+    if (this.a <= 0 || this.b <= 0 || this.c <= 0) {
+      throw new Error('incorrect data entered');
+    }
+
+    const sides = [this.a, this.b, this.c];
+    const biggestSide = Math.max(...sides);
+    const otherSides = sides.filter((side) => side !== biggestSide);
+
+    if (biggestSide >= otherSides[0] + otherSides[1]) {
+      throw new Error('incorrect data entered');
+    }
+  }
+
+  getArea(): number {
+    const s = (this.a + this.b + this.c) / 2;
+    const area = Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+
+    return Number(area.toFixed(2));
+  }
+}
+
+export class Circle implements Figure {
+  public color: ColorType;
+
+  public radius: number;
+
+  public shape: ShapeType = 'circle';
+
+  constructor(color: ColorType, radius: number) {
+    this.color = color;
+    this.radius = radius;
+
+    if (this.radius <= 0) {
+      throw new Error('incorrect data entered');
+    }
+  }
+
+  getArea(): number {
+    const area = this.radius * this.radius * Math.PI;
+
+    return Math.floor(area * 100) / 100;
+  }
+}
+
+export class Rectangle implements Figure {
+  public color: ColorType;
+
+  public width: number;
+
+  public height: number;
+
+  public shape: ShapeType = 'rectangle';
+
+  constructor(color: ColorType, width: number, height: number) {
+    this.color = color;
+    this.width = width;
+    this.height = height;
+
+    if (this.width <= 0 || this.height <= 0) {
+      throw new Error('incorrect data entered');
+    }
+  }
+
+  getArea(): number {
+    return Number((this.height * this.width).toFixed(2));
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
