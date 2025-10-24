23617f43f490771935e4561c81f96a373592a5ed
Vitor de Freitas Ribeiro <programador.vitor.ribeiro@hotmail.com>
2025-10-24 18:50:10 -0300

    changes made

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -7,17 +7,11 @@ export interface Figure {
 export class Triangle implements Figure {
   public shape: 'triangle' = 'triangle';
 
-  public a: number;
-
-  public b: number;
-
-  public c: number;
-
   constructor(
     public color: 'red' | 'green' | 'blue',
-    a: number,
-    b: number,
-    c: number,
+    public a: number,
+    public b: number,
+    public c: number,
   ) {
     if (a <= 0 || b <= 0 || c <= 0) {
       throw new Error('Sides of a triangle must be positive numbers');
@@ -26,12 +20,6 @@ export class Triangle implements Figure {
     if (a + b <= c || a + c <= b || b + c <= a) {
       throw new Error('Sides of a triangle must form a triangle');
     }
-
-    this.shape = 'triangle';
-    this.color = color;
-    this.a = a;
-    this.b = b;
-    this.c = c;
   }
 
   public getArea(): number {
@@ -48,19 +36,13 @@ export class Triangle implements Figure {
 export class Circle implements Figure {
   public shape: 'circle' = 'circle';
 
-  public r: number;
-
   constructor(
     public color: 'red' | 'green' | 'blue',
-    r: number,
+    public r: number,
   ) {
     if (r <= 0) {
       throw new Error('Radius of a circle must be positive number');
     }
-
-    this.shape = 'circle';
-    this.color = color;
-    this.r = r;
   }
 
   public getArea(): number {
@@ -71,25 +53,16 @@ export class Circle implements Figure {
 export class Rectangle implements Figure {
   public shape: 'rectangle' = 'rectangle';
 
-  public width: number;
-
-  public height: number;
-
   constructor(
     public color: 'red' | 'green' | 'blue',
-    width: number,
-    height: number,
+    public width: number,
+    public height: number,
   ) {
     if (width <= 0 || height <= 0) {
       throw new Error(
         'Width and height of a rectangle must be positive numbers',
       );
     }
-
-    this.shape = 'rectangle';
-    this.color = color;
-    this.width = width;
-    this.height = height;
   }
 
   public getArea(): number {
