806e80662e24592b793079df236e5b0cb989f715
Artem Yakhno <artem.yakhno.ua@gmail.com>
2025-11-20 22:59:16 +0200

    added solution 2

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -17,20 +17,20 @@ export class Triangle implements Figure {
     public c: number,
   ) {
     if (a <= 0 || b <= 0 || c <= 0) {
-      throw new Error('your error message');
+      throw new Error('Triangle sides must be positive numbers');
     }
 
     if (a + b <= c || a + c <= b || b + c <= a) {
-      throw new Error('your error message');
+      throw new Error('Sides do not form a triangle');
     }
   }
 
   public getArea(): number {
     const s: number = (this.a + this.b + this.c) / 2;
 
-    return +Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c)).toFixed(
-      2,
-    );
+    const area = Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+
+    return Math.floor(area * 100) / 100;
   }
 }
 
@@ -42,7 +42,7 @@ export class Circle implements Figure {
     public radius: number,
   ) {
     if (radius <= 0) {
-      throw new Error('your error message');
+      throw new Error('Radius must be a positive number');
     }
   }
 
@@ -62,15 +62,17 @@ export class Rectangle implements Figure {
     public height: number,
   ) {
     if (width <= 0 || height <= 0) {
-      throw new Error('your error message');
+      throw new Error('Width and height must be positive numbers');
     }
   }
 
   public getArea(): number {
-    return +(this.width * this.height).toFixed(2);
+    const area = this.width * this.height;
+
+    return Math.floor(area * 100) / 100;
   }
 }
 
-export function getInfo(figure): string {
+export function getInfo(figure: Figure): string {
   return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
