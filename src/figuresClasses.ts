25c9df8b32d48dfa548fc7cea0efe4c752864f29
gustavo <gustavoalmeida5446@gmail.com>
2025-11-25 09:18:41 -0300

    add solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,74 @@
-export interface Figure {}
+export interface Figure {
+  shape: string;
+  color: string;
+  getArea(): number;
+}
 
-export class Triangle implements Figure {}
+export class Triangle implements Figure {
+  shape = 'triangle';
 
-export class Circle implements Figure {}
+  constructor(
+    public color: string,
+    private a: number,
+    private b: number,
+    private c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('Lados devem ser maiores que zero');
+    }
 
-export class Rectangle implements Figure {}
+    if (a >= b + c || b >= a + c || c >= a + b) {
+      throw new Error('Esses lados não podem formar um triângulo');
+    }
+  }
 
-export function getInfo(figure): string {
-  return typeof figure;
+  getArea(): number {
+    const s = (this.a + this.b + this.c) / 2; // semi-perímetro
+    const area = Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+
+    return Math.floor(area * 100) / 100;
+  }
+}
+
+export class Circle implements Figure {
+  shape = 'circle';
+
+  constructor(
+    public color: string,
+    private radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('O raio deve ser maior que zero');
+    }
+  }
+
+  getArea(): number {
+    const area = Math.PI * (this.radius * this.radius);
+
+    return Math.floor(area * 100) / 100;
+  }
+}
+
+export class Rectangle implements Figure {
+  shape = 'rectangle';
+
+  constructor(
+    public color: string,
+    private width: number,
+    private height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('As dimensões devem ser maiores que zero');
+    }
+  }
+
+  getArea(): number {
+    const area = this.width * this.height;
+
+    return Math.floor(area * 100) / 100;
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
