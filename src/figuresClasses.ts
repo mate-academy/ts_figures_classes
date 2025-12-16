dc14ffb8e4c4f676d270754987c7d74493b3a7df
Sertavr <pampuh.net@gmail.com>
2025-12-16 18:43:06 +0200

    add task solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,80 @@
-export interface Figure {}
+export interface Figure {
+  shape: 'triangle' | 'circle' | 'rectangle';
+  color: 'red' | 'green' | 'blue';
+  getArea(): number;
+  width?: number;
+  height?: number;
+  radius?: number;
+  a?: number;
+  b?: number;
+  c?: number;
+}
 
-export class Triangle implements Figure {}
+export class Triangle implements Figure {
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public a: number,
+    public b: number,
+    public c: number,
+    public shape: 'triangle' = 'triangle',
+  ) {
+    const maxNum = Math.max(a, b, c);
+    const minNum = Math.min(a, b, c);
+    const sum = a + b + c - maxNum;
 
-export class Circle implements Figure {}
+    if (maxNum >= sum || minNum <= 0) {
+      throw new Error('Invalid triangle sides');
+    }
+  }
 
-export class Rectangle implements Figure {}
+  getArea(): number {
+    const semiPerimeter = (this.a + this.b + this.c) / 2;
+    const s = Math.sqrt(
+      semiPerimeter *
+        (semiPerimeter - this.a) *
+        (semiPerimeter - this.b) *
+        (semiPerimeter - this.c),
+    );
 
-export function getInfo(figure): string {
-  return typeof figure;
+    return Math.round(s * 100) / 100;
+  }
+}
+
+export class Circle implements Figure {
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public radius: number,
+    public shape: 'circle' = 'circle',
+  ) {
+    if (radius <= 0) {
+      throw new Error('invalid circle radius');
+    }
+  }
+
+  getArea(): number {
+    return Math.trunc(Math.PI * this.radius ** 2 * 100) / 100;
+  }
+}
+
+export class Rectangle implements Figure {
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public width: number,
+    public height: number,
+    public shape: 'rectangle' = 'rectangle',
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('Invalid triangle sides');
+    }
+  }
+
+  getArea(): number {
+    return Math.round(this.width * this.height * 100) / 100;
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  const { shape, color, getArea } = figure;
+
+  return `A ${color} ${shape} - ${getArea.call(figure)}`;
 }
