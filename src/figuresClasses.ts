69598eb1bc3ac12974b91c1ff8875fbc247335da
Dmytro Popov <lort20178@gmail.com>
2025-12-16 15:15:44 +0200

    Add second solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -9,12 +9,13 @@ export interface Figure {
 
 export class Triangle implements Figure {
   shape: Shape = 'triangle';
-  color: Color;
-  a: number;
-  b: number;
-  c: number;
 
-  constructor(color: Color, a: number, b: number, c: number) {
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
     if (a <= 0 || b <= 0 || c <= 0) {
       throw new Error('Sides must be greater than 0');
     }
@@ -25,18 +26,11 @@ export class Triangle implements Figure {
     if (maxSide >= sumOtherSides) {
       throw new Error(`Sides ${a}, ${b} and ${c} can't form a triangle`);
     }
-
-    this.color = color;
-    this.a = a;
-    this.b = b;
-    this.c = c;
   }
 
   getArea(): number {
     const p = (this.a + this.b + this.c) / 2;
-    const area = Math.sqrt(
-      p * (p - this.a) * (p - this.b) * (p - this.c),
-    );
+    const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
 
     return Math.floor(area * 100) / 100;
   }
@@ -44,16 +38,14 @@ export class Triangle implements Figure {
 
 export class Circle implements Figure {
   shape: Shape = 'circle';
-  color: Color;
-  radius: number;
 
-  constructor(color: Color, radius: number) {
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
     if (radius <= 0) {
       throw new Error('Radius must be greater than 0');
     }
-
-    this.colot = color;
-    this.radius = radius;
   }
 
   getArea(): number {
@@ -63,18 +55,15 @@ export class Circle implements Figure {
 
 export class Rectangle implements Figure {
   shape: Shape = 'rectangle';
-  color: Color;
-  width: number;
-  height: number;
 
-  constructor(color: Color, width: number, height: number) {
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
     if (width <= 0 || height <= 0) {
       throw new Error('Width and height must be greater than 0');
     }
-
-    this.color = color;
-    this.width = width;
-    this.height = height;
   }
 
   getArea(): number {
@@ -83,5 +72,5 @@ export class Rectangle implements Figure {
 }
 
 export function getInfo(figure: Figure): string {
-  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;;
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
