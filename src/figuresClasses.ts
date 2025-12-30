f607c17a1015345c7e3e4b47226c17fcd9638a4a
mpodaniev <mpodanev@gmail.com>
2025-12-30 11:49:34 +0200

    implement `Triangle`, `Circle`, and `Rectangle` classes

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,85 @@
-export interface Figure {}
+export interface Figure {
+  shape: string;
+  color: string;
+  getArea(): number;
+}
 
-export class Triangle implements Figure {}
+export class Triangle implements Figure {
+  shape = 'Triangle';
 
-export class Circle implements Figure {}
+  constructor(
+    public color: string,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    this.color = color;
+    this.a = a;
+    this.b = b;
+    this.c = c;
 
-export class Rectangle implements Figure {}
+    if (this.a < 0 || this.b < 0 || this.c < 0) {
+      throw new Error('Sides cannot be negative');
+    }
 
-export function getInfo(figure): string {
-  return typeof figure;
+    if (
+      this.a + this.b <= this.c ||
+      this.a + this.c <= this.b ||
+      this.b + this.c <= this.a
+    ) {
+      throw new Error('Invalid triangle');
+    }
+  }
+
+  getArea(): number {
+    const p = (this.a + this.b + this.c) / 2;
+
+    return Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+  }
+}
+
+export class Circle implements Figure {
+  shape = 'Circle';
+
+  constructor(
+    public color: string,
+    public radius: number,
+  ) {
+    this.color = color;
+    this.radius = radius;
+
+    if (this.radius < 0) {
+      throw new Error('Radius cannot be negative');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor(Math.PI * this.radius * this.radius * 100) / 100;
+  }
+}
+
+export class Rectangle implements Figure {
+  shape = 'Rectangle';
+
+  constructor(
+    public color: string,
+    public width: number,
+    public height: number,
+  ) {
+    this.color = color;
+    this.width = width;
+    this.height = height;
+
+    if (this.width < 0 || this.height < 0) {
+      throw new Error('Sides cannot be negative');
+    }
+  }
+
+  getArea(): number {
+    return this.width * this.height;
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape.toLowerCase()} - ${Math.round(figure.getArea() * 100) / 100}`;
 }
