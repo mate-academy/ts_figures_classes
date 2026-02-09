3e354d330c1ee20bdc5feebae02d88de28f93818
Mykyta Oliinyk <mykyta.oliinyk0@gmail.com>
2026-02-09 15:04:04 +0100

    add solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,74 @@
-export interface Figure {}
+export interface Figure {
+  color: string;
+  shape: string;
+  getArea: () => number;
+}
 
-export class Triangle implements Figure {}
+export class Triangle implements Figure {
+  shape = 'triangle';
 
-export class Circle implements Figure {}
+  constructor(
+    public color: string,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (
+      a <= 0 ||
+      b <= 0 ||
+      c <= 0 ||
+      Math.max(a, b, c) >= a + b + c - Math.max(a, b, c)
+    ) {
+      throw new Error('sides of triangle cant be 0 or less');
+    }
+  }
 
-export class Rectangle implements Figure {}
+  getArea(): number {
+    const s = (1 / 2) * (this.a + this.b + this.c);
 
-export function getInfo(figure): string {
-  return typeof figure;
+    return (
+      Math.floor(
+        Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c)) * 100,
+      ) / 100
+    );
+  }
+}
+
+export class Circle implements Figure {
+  shape = 'circle';
+
+  constructor(
+    public color: string,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('radius cant be less than 0');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor(Math.PI * this.radius ** 2 * 100) / 100;
+  }
+}
+
+export class Rectangle implements Figure {
+  shape = 'rectangle';
+
+  constructor(
+    public color: string,
+    public width: number,
+    public height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('side of rectangle cant be 0 or less');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor(this.width * this.height * 100) / 100;
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
