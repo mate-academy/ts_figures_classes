6d8506073e1661387202cd8f93754f89e518a0c8
Alex Kh. <sanchez.primal@gmail.com>
2026-04-02 19:19:10 +0200

    fix rounding of areas

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -29,7 +29,7 @@ export class Triangle implements Figure {
     const s = (this.a + this.b + this.c) / 2;
 
     return (
-      Math.round(
+      Math.floor(
         Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c)) * 100,
       ) / 100
     );
@@ -67,7 +67,7 @@ export class Rectangle implements Figure {
   }
 
   getArea(): number {
-    return +(this.width * this.height).toFixed(2);
+    return Math.floor(this.width * this.height * 100) / 100;
   }
 }
 
