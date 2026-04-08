3c8dd4618fdb92d5175781476154f72b79716ede
Serhii Shimko <shimkosergij2010@gmail.com>
2026-04-08 15:24:16 +0300

    completed the task

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,79 @@
-export interface Figure {}
+export interface Figure {
+  color: string;
+  shape: string;
+  getArea(): number;
+}
 
-export class Triangle implements Figure {}
+export class Triangle implements Figure {
+  shape: string = 'triangle';
 
-export class Circle implements Figure {}
+  constructor(
+    public color: string,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (
+      this.a <= 0 ||
+      this.b <= 0 ||
+      this.c <= 0 ||
+      this.a + this.b <= this.c ||
+      this.b + this.c <= this.a ||
+      this.a + this.c <= this.b
+    ) {
+      throw new Error('your error message');
+    }
+  }
 
-export class Rectangle implements Figure {}
+  getArea(): number {
+    const p: number = (this.a + this.b + this.c) / 2;
+    const s: number = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
 
-export function getInfo(figure): string {
-  return typeof figure;
+    return +s.toFixed(2);
+  }
+}
+
+export class Circle implements Figure {
+  shape: string = 'circle';
+
+  constructor(
+    public color: string,
+    public radius: number,
+  ) {
+    if (this.radius <= 0) {
+      throw new Error('your error message');
+    }
+  }
+
+  getArea(): number {
+    const s: number = Math.PI * this.radius ** 2;
+
+    return +(Math.floor(s * 100) / 100).toFixed(2);
+  }
+}
+
+export class Rectangle implements Figure {
+  shape: string = 'rectangle';
+
+  constructor(
+    public color: string,
+    public width: number,
+    public height: number,
+  ) {
+    if (this.width <= 0 || this.height <= 0) {
+      throw new Error('your error message');
+    }
+  }
+
+  getArea(): number {
+    const s: number = this.width * this.height;
+
+    return +s.toFixed(2);
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  const s: number = figure.getArea();
+
+  return `A ${figure.color} ${figure.shape} - ${s}`;
 }
