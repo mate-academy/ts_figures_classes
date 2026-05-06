3611a5ce068aab3567d94eff321f82200a8abcf8
Alternate001 <artem.kushko@publizer.de>
2026-05-06 11:34:03 +0200

    solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,82 @@
-export interface Figure {}
+enum Color {
+  Red = 'red',
+  Green = 'green',
+  Blue = 'blue',
+}
+enum Shape {
+  Triangle = 'triangle',
+  Circle = 'circle',
+  Rectangle = 'rectangle',
+}
 
-export class Triangle implements Figure {}
+export interface Figure {
+  shape: Shape;
+  color: Color;
+}
 
-export class Circle implements Figure {}
+export class Triangle implements Figure {
+  constructor(
+    public color: Color,
+    protected a: number,
+    protected b: number,
+    protected c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0 || a + b <= c || c + b <= a || a + c <= b) {
+      throw new Error('Triangle error');
+    }
+  }
 
-export class Rectangle implements Figure {}
+  get shape(): Shape {
+    return Shape.Triangle;
+  }
 
-export function getInfo(figure): string {
-  return typeof figure;
+  getArea(): number {
+    const s = (this.a + this.b + this.c) / 2;
+    const area = Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+
+    return area;
+  }
+}
+
+export class Circle implements Figure {
+  constructor(
+    public color: Color,
+    protected radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('Circle error');
+    }
+  }
+
+  get shape(): Shape {
+    return Shape.Circle;
+  }
+
+  getArea(): number {
+    return Math.floor(this.radius * this.radius * Math.PI * 100) / 100;
+  }
+}
+
+export class Rectangle implements Figure {
+  constructor(
+    public color: Color,
+    protected width: number,
+    protected height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('Rectangle error');
+    }
+  }
+
+  get shape(): Shape {
+    return Shape.Rectangle;
+  }
+
+  getArea(): number {
+    return this.width * this.height;
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${Math.floor(figure.getArea() * 100 + 0.01) / 100}`;
 }
