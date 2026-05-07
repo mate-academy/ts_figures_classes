cb9fb30b8acfb2153948f7f733d9b7a396febe23
Yurii-Leniv <supusman32@gmail.com>
2026-05-07 21:12:30 +0300

    fewopgnwiegbj2wbg

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -13,14 +13,20 @@ export class Triangle implements Figure {
     public b: number,
     public c: number,
   ) {
-    if (a <= 0 || b <= 0 || c <= 0 || Math.max(a,b,c) >= a+b+c - Math.max(a,b,c)) {
-      throw new Error('your error message');
+    if (
+      a <= 0 ||
+      b <= 0 ||
+      c <= 0 ||
+      Math.max(a, b, c) >= a + b + c - Math.max(a, b, c)
+    ) {
+      throw new Error('Triangle sides cannot form a valid shape');
     }
   }
 
   getArea(): number {
     const p = (this.a + this.b + this.c) / 2;
     const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+
     return Math.floor(area * 100) / 100;
   }
 }
@@ -32,13 +38,14 @@ export class Circle implements Figure {
     public color: 'red' | 'green' | 'blue',
     public radius: number,
   ) {
-    if (radius <= 0 ) {
-      throw new Error('your error message');
+    if (radius <= 0) {
+      throw new Error('Length must be greater than 0');
     }
   }
 
   getArea(): number {
     const area = this.radius * this.radius * Math.PI;
+
     return Math.floor(area * 100) / 100;
   }
 }
@@ -49,19 +56,20 @@ export class Rectangle implements Figure {
   constructor(
     public color: 'red' | 'green' | 'blue',
     public width: number,
-    public height: number
+    public height: number,
   ) {
     if (width <= 0 || height <= 0) {
-      throw new Error('your error message');
+      throw new Error('Length must be greater than 0');
     }
   }
 
   getArea(): number {
     const area = this.width * this.height;
+
     return Math.floor(area * 100) / 100;
   }
 }
 
 export function getInfo(figure: Figure): string {
-  return `Shape: ${figure.shape}, color: ${figure.color}, area: ${figure.getArea()}`;
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
