dc31d645661f495b6fe34569ee465c09aace761d
Yurii-Leniv <supusman32@gmail.com>
2026-05-07 21:04:17 +0300

    fpewgjipqeghoiuq

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,67 @@
-export interface Figure {}
+export interface Figure {
+  shape: 'triangle' | 'circle' | 'rectangle';
+  color: 'red' | 'green' | 'blue';
+  getArea: () => number;
+}
 
-export class Triangle implements Figure {}
+export class Triangle implements Figure {
+  shape: 'triangle' | 'circle' | 'rectangle' = 'triangle';
 
-export class Circle implements Figure {}
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0 || Math.max(a,b,c) >= a+b+c - Math.max(a,b,c)) {
+      throw new Error('your error message');
+    }
+  }
 
-export class Rectangle implements Figure {}
+  getArea(): number {
+    const p = (this.a + this.b + this.c) / 2;
+    const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+    return Math.floor(area * 100) / 100;
+  }
+}
 
-export function getInfo(figure): string {
-  return typeof figure;
+export class Circle implements Figure {
+  shape: 'triangle' | 'circle' | 'rectangle' = 'circle';
+
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public radius: number,
+  ) {
+    if (radius <= 0 ) {
+      throw new Error('your error message');
+    }
+  }
+
+  getArea(): number {
+    const area = this.radius * this.radius * Math.PI;
+    return Math.floor(area * 100) / 100;
+  }
+}
+
+export class Rectangle implements Figure {
+  shape: 'triangle' | 'circle' | 'rectangle' = 'rectangle';
+
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public width: number,
+    public height: number
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('your error message');
+    }
+  }
+
+  getArea(): number {
+    const area = this.width * this.height;
+    return Math.floor(area * 100) / 100;
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `Shape: ${figure.shape}, color: ${figure.color}, area: ${figure.getArea()}`;
 }
