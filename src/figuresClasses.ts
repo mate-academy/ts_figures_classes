0fd74d4ae970aafda5489877bd034482a28c058c
Alla Karakaliuk <alla.uriivna@gmail.com>
2026-05-17 00:13:55 +0400

    add task solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,69 @@
-export interface Figure {}
+export interface Figure {
+  shape: 'triangle' | 'circle' | 'rectangle';
+  color: 'red' | 'green' | 'blue';
+  getArea(): number;
+}
 
-export class Triangle implements Figure {}
+export class Triangle implements Figure {
+  shape: 'triangle' = 'triangle';
 
-export class Circle implements Figure {}
+  constructor(
+    public color: Figure['color'],
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (!(a + b > c && a + c > b && b + c > a) || a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('Invalid triangle sides');
+    }
+  }
 
-export class Rectangle implements Figure {}
+  getArea(): number {
+    const s = (this.a + this.b + this.c) / 2;
+    const area = Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
 
-export function getInfo(figure): string {
-  return typeof figure;
+    return Math.floor(area * 100) / 100;
+  }
+}
+
+export class Circle implements Figure {
+  shape: 'circle' = 'circle';
+
+  constructor(
+    public color: Figure['color'],
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('radius must be greater than 0');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor(Math.PI * this.radius * this.radius * 100) / 100;
+  }
+}
+
+export class Rectangle implements Figure {
+  readonly shape: 'rectangle' = 'rectangle';
+
+  constructor(
+    public color: Figure['color'],
+    public width: number,
+    public height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('Width and height must be greater than 0');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor(this.width * this.height * 100) / 100;
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  const area = figure.getArea();
+  const areaStr = area % 1 === 0 ? area.toString() : area.toFixed(2);
+
+  return `A ${figure.color} ${figure.shape} - ${areaStr}`;
 }
