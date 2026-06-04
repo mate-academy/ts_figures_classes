e2b509525f8f7d67d9a9c1ac366d5adf93e7deae
Roman <yamshchikovr11@gmail.com>
2026-06-04 03:41:51 +0300

    add task solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -90,7 +90,7 @@ export class Rectangle implements Figure {
   }
 
   getArea(): number {
-    return this.width * this.height;
+    return Math.floor(this.width * this.height * 100) / 100;
   }
 }
 
