f64d1e67a813741f0070ee1458fd1a9130efd8fe
PavloMolytovnyk <pavlomolytovny@icloud.com>
2026-06-12 21:43:20 +0300

    Fixed texts and logic

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -14,11 +14,13 @@ export class Triangle implements Figure {
     public c: number,
   ) {
     if (this.a <= 0 || this.b <= 0 || this.c <= 0) {
-      throw new Error('Error number');
+      throw new Error('One of numbers have unexcepted value');
     }
 
-    if (this.c >= a + b) {
-      throw new Error('Error number');
+    if ((this.c >= this.a + this.b)
+      || (this.b >= this.a + this.c)
+      || (this.a >= this.b + this.c)) {
+      throw new Error('Biggest number are less or equal the sum of rest numbers');
     }
   }
 
@@ -26,9 +28,9 @@ export class Triangle implements Figure {
     const halfPerimetr: number = (this.a + this.b + this.c) / 2;
     const square = Math.sqrt(
       halfPerimetr *
-        (halfPerimetr - this.a) *
-        (halfPerimetr - this.b) *
-        (halfPerimetr - this.c),
+      (halfPerimetr - this.a) *
+      (halfPerimetr - this.b) *
+      (halfPerimetr - this.c),
     );
 
     return Math.floor(square * 100) / 100;
@@ -43,7 +45,7 @@ export class Circle implements Figure {
     public radius: number,
   ) {
     if (this.radius <= 0) {
-      throw new Error('Error number');
+      throw new Error('Radius have unexcepted number');
     }
   }
 
@@ -61,7 +63,7 @@ export class Rectangle implements Figure {
     public height: number,
   ) {
     if (this.width <= 0 || this.height <= 0) {
-      throw new Error('Error numbers');
+      throw new Error('One of sides of rectangle have less or equal to 0');
     }
   }
 
