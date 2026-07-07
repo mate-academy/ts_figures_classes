9b48cdc8ebfc494637bdafe5bd50f0412e4db5de
Olena <simonaolena6@gmail.com>
2026-07-07 19:29:12 +0300

    I did Figure, Triangle, Circle, Rectangle

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,10 +1,66 @@
-export interface Figure {}
+export interface Figure {
+  shape: 'triangle' | 'circle' | 'rectangle';
+  color: 'red' | 'green' | 'blue';
+  getArea(): number;
+}
 
-export class Triangle implements Figure {}
+export class Triangle implements Figure {
+  shape: 'triangle' = 'triangle';
+  constructor(public a: number, public b: number, public c: number, public color: 'red' | 'green' | 'blue') {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('Invalid triangle');
+    }
 
-export class Circle implements Figure {}
+    const max = Math.max(a, b, c);
+    const sum = (a + b + c) - max;
 
-export class Rectangle implements Figure {}
+    if (max >= sum) {
+      throw new Error('Error!');
+    }
+  }
+
+  getArea(): number {
+    const s = (this.a + this.b + this.c) / 2;
+    const area = Math.sqrt((s * (s - this.a) * (s - this.b) * (s - this.c)));
+    const floor = Math.floor(area * 100) / 100;
+
+    return floor;
+  }
+}
+
+
+export class Circle implements Figure {
+  shape: 'circle' = 'circle';
+  constructor(public color: 'red' | 'green' | 'blue', public radius: number) {
+    if (radius <= 0) {
+      throw new Error('Error!');
+
+    }
+  }
+
+  getArea(): number {
+    const pi = Math.PI * this.radius * this.radius;
+    const floor = Math.floor(pi * 100) / 100;
+
+    return floor;
+  }
+}
+
+export class Rectangle implements Figure {
+  shape: 'rectangle' = 'rectangle';
+  constructor(public width: number, public height: number, public color: 'red' | 'green' | 'blue') {
+    if (width <= 0 || height <= 0) {
+      throw new Error('Error!');
+    }
+  }
+
+  getArea(): number {
+      const s = this.width * this.height;
+      const floor = Math.floor(s * 100) / 100;
+
+      return floor;
+    }
+}
 
 export function getInfo(figure): string {
   return typeof figure;
