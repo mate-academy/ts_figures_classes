22639094b52326acabd0bb3536aa68731983e519
Anik <anikferreira8@gmail.com>
2026-07-13 21:00:02 -0300

    Implement figures classes

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,86 @@
-export interface Figure {}
+export type Shape = 'triangle' | 'circle' | 'rectangle';
+export type Color = 'red' | 'green' | 'blue';
 
-export class Triangle implements Figure {}
+export interface Figure {
+  shape: Shape;
+  color: Color;
+  getArea(): number;
+}
 
-export class Circle implements Figure {}
+export class Triangle implements Figure {
+  public shape: Shape = 'triangle';
 
-export class Rectangle implements Figure {}
+  constructor(
+    public color: Color,
+    private a: number,
+    private b: number,
+    private c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('Side length must be greater than 0');
+    }
 
-export function getInfo(figure): string {
-  return typeof figure;
+    const longest: number = Math.max(a, b, c);
+    const otherSides: number = a + b + c - longest;
+
+    if (longest >= otherSides) {
+      throw new Error("Sides can't form a triangle");
+    }
+  }
+
+  public getArea(): number {
+    const halfPerimeter: number = (this.a + this.b + this.c) / 2;
+
+    const area: number = Math.sqrt(
+      halfPerimeter *
+        (halfPerimeter - this.a) *
+        (halfPerimeter - this.b) *
+        (halfPerimeter - this.c),
+    );
+
+    return Math.floor(area * 100) / 100;
+  }
+}
+
+export class Circle implements Figure {
+  public shape: Shape = 'circle';
+
+  constructor(
+    public color: Color,
+    private radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('Radius must be greater than 0');
+    }
+  }
+
+  public getArea(): number {
+    const area: number = Math.PI * this.radius ** 2;
+
+    return Math.floor(area * 100) / 100;
+  }
+}
+
+export class Rectangle implements Figure {
+  public shape: Shape = 'rectangle';
+
+  constructor(
+    public color: Color,
+    private width: number,
+    private height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('Width and height must be greater than 0');
+    }
+  }
+
+  public getArea(): number {
+    const area: number = this.width * this.height;
+
+    return Math.floor(area * 100) / 100;
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
