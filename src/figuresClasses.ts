fc1dd90af8a99d97dd7e712640183575f53891a1
Ranierij <95530644+Ranierij@users.noreply.github.com>
2026-07-20 16:20:14 -0300

    Update figuresClasses.ts

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,97 +1,92 @@
 export interface Figure {}
-type Color = 'red' | 'green' | 'blue';
-type Shape = 'triangle' | 'circle' | 'rectangle';
-
-export class Triangle implements Figure {}
 export interface Figure {
-  color: Color;
-  shape: Shape;
-  getArea(): number;
+  shape: 'triangle' | 'circle' | 'rectangle';
+  color: 'red' | 'green' | 'blue';
+  getArea(): number | string;Expand commentComment on line R4
 }
 
 export class Triangle implements Figure {
-  shape: Shape = 'triangle';
+  readonly shape = 'triangle';
 
-  color: Color;
-
-  a: number;
-
-  b: number;
-
-  c: number;
-
-  constructor(color: Color, a: number, b: number, c: number) {
-    this.color = color;
-    this.a = a;
-    this.b = b;
-    this.c = c;
-
-    if (a <= 0 || b <= 0 || c <= 0) {
-      throw new Error('Sides must be greater than 0');
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public a = 0,
+    public b = 0,
+    public c = 0,
+  ) {
+    if (this.a <= 0 || this.b <= 0 || this.c <= 0) {
+      throw new Error('A side must be greater than 0');
     }
 
-    if (a + b <= c || a + c <= b || b + c <= a) {
-      throw new Error('Sides a, b and c cannot form a triangle');
+    if (
+      this.a + this.b <= this.c ||
+      this.a + this.c <= this.b ||
+      this.b + this.c <= this.a
+    ) {
+      throw new Error(
+        'The longest side must be shorter than the other two combined',
+      );
     }
   }
 
-  getArea(): number {
-    const s = (this.a + this.b + this.c) / 2;
-    const rawArea = Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+  getArea(): number | string {
+    const sum = (this.a + this.b + this.c) / 2;
+    const result = Math.sqrt(
+      sum * (sum - this.a) * (sum - this.b) * (sum - this.c),
+    );
 
-    return Math.floor(rawArea * 100) / 100;
+    if (Number.isInteger(result)) {
+      return result;
+    } else {
+      return Number(result.toFixed(2));
+    }Expand commentComment on lines R31 to R41
   }
 }
 
+export class Triangle implements Figure {}
 export class Circle implements Figure {
-  shape: Shape = 'circle';
-
-  radius: number;
+  readonly shape = 'circle';
 
 export class Circle implements Figure {}
-  color: Color;
-
-export class Rectangle implements Figure {}
-  constructor(color: Color, radius: number) {
-    this.radius = radius;
-    this.color = color;
-
-    if (radius <= 0) {
-      throw new Error('Radius must be greater than 0');
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public radius = 0,
+  ) {
+    if (this.radius <= 0) {
+      throw new Error('The radius must be greater than 0');
     }
   }
 
-  getArea(): number {
-    const likeArea = Math.PI * this.radius ** 2;
+export class Rectangle implements Figure {}
+  getArea(): number | string {Expand commentComment on line R57
+    const result = Math.PI * this.radius ** 2;
 
-    return Math.floor(likeArea * 100) / 100;
+    return Math.floor(result * 100) / 100;Expand commentComment on lines R57 to R60
   }
 }
 
 export class Rectangle implements Figure {
-  shape: Shape = 'rectangle';
+  readonly shape = 'rectangle';
 
-  width: number;
-
-  height: number;
-
-  color: Color;
-
-  constructor(color: Color, width: number, height: number) {
-    this.width = width;
-    this.height = height;
-    this.color = color;
-
-    if (width <= 0 || height <= 0) {
-      throw new Error('width and height must be greater than 0');
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public width = 0,
+    public height = 0,
+  ) {
+    if (this.width <= 0 || this.height <= 0) {
+      throw new Error('A side must be greater than 0');
     }
   }
 
-  getArea(): number {
-    const rawArea = this.width * this.height;
+  getArea(): number | string {
+    const result = this.width * this.height;
 
-    return Math.floor(rawArea * 100) / 100;
-  }
+    if (Number.isInteger(result)) {
+      return result;
+    } else {
+      return Number(result.toFixed(2));
+    }
+  }Expand commentComment on lines R77 to R85
 }
 
 export function getInfo(figure): string {
