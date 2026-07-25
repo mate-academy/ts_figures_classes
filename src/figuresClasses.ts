2ce48cb355a6939560ba99e94bf3f6c8a334e4a8
ILLYA SMYK <smykillya@gmail.com>
2026-07-25 23:27:20 +0200

    add task solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,82 @@
-export interface Figure {}
+type Shape = 'triangle' | 'circle' | 'rectangle';
+type Color = 'red' | 'green' | 'blue';
 
-export class Triangle implements Figure {}
+export interface Figure {
+  shape: Shape;
+  color: Color;
 
-export class Circle implements Figure {}
+  getArea(): number;
+}
 
-export class Rectangle implements Figure {}
+export class Triangle implements Figure {
+  constructor(
+    public color: Color,
+    public sideA: number,
+    public sideB: number,
+    public sideC: number,
+  ) {
+    if (sideA <= 0 || sideB <= 0 || sideC <= 0) {
+      throw new Error();
+    }
 
-export function getInfo(figure): string {
-  return typeof figure;
+    if (
+      sideA + sideB <= sideC ||
+      sideB + sideC <= sideA ||
+      sideA + sideC <= sideB
+    ) {
+      throw new Error();
+    }
+  }
+
+  shape: Shape = 'triangle';
+
+  getArea(): number {
+    const p: number = (this.sideA + this.sideB + this.sideC) / 2;
+
+    return (
+      Math.floor(
+        Math.sqrt(p * (p - this.sideA) * (p - this.sideB) * (p - this.sideC)) *
+          100,
+      ) / 100
+    );
+  }
+}
+
+export class Circle implements Figure {
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error();
+    }
+  }
+
+  shape: Shape = 'circle';
+
+  getArea(): number {
+    return Math.floor(Math.PI * this.radius ** 2 * 100) / 100;
+  }
+}
+
+export class Rectangle implements Figure {
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
+    if (height <= 0 || width <= 0) {
+      throw new Error();
+    }
+  }
+
+  shape: Shape = 'rectangle';
+
+  getArea(): number {
+    return this.width * this.height;
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
