b5751f664e3f7e2609a18bf0ad11c9259a8b2d46
Vlad CHuikov <koras4uikov@gmail.com>
2026-09-05 20:20:29 +0300

    all tests are passed

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,86 @@
-export interface Figure {}
+export interface Figure {
+  shape: 'triangle' | 'circle' | 'rectangle';
+  color: 'red' | 'green' | 'blue';
+  getArea(): number;
+}
 
-export class Triangle implements Figure {}
+export class Triangle implements Figure {
+  shape: 'triangle';
 
-export class Circle implements Figure {}
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public a: number = 0,
+    public b: number = 0,
+    public c: number = 0,
+  ) {
+    this.shape = 'triangle';
 
-export class Rectangle implements Figure {}
+    if (a < 0 || b < 0 || c < 0) {
+      throw new Error('Invalid values of triangle sides');
+    }
 
-export function getInfo(figure): string {
-  return typeof figure;
+    const sum = a + b + c;
+
+    if (sum - Math.max(a, b, c) <= Math.max(a, b, c)) {
+      throw new Error('This figure isn`t a triangle');
+    }
+  }
+
+  getArea(): number {
+    const s: number = (this.a + this.b + this.c) / 2;
+    const area: number =
+      Math.floor(
+        Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c)) * 100,
+      ) / 100;
+
+    return area;
+  }
+}
+
+export class Circle implements Figure {
+  shape: 'circle';
+
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public radius: number = 0,
+  ) {
+    this.shape = 'circle';
+
+    if (radius < 0) {
+      throw new Error('Invalid value of circle radius');
+    }
+  }
+
+  getArea(): number {
+    const area: number =
+      Math.floor(Math.PI * this.radius * this.radius * 100) / 100;
+
+    return area;
+  }
+}
+
+export class Rectangle implements Figure {
+  shape: 'rectangle';
+
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public width: number,
+    public lenght: number,
+  ) {
+    this.shape = 'rectangle';
+
+    if (width < 0 || lenght < 0) {
+      throw new Error('Invalid values for rectangle sides');
+    }
+  }
+
+  getArea(): number {
+    const area: number = Math.floor(this.width * this.lenght * 100) / 100;
+
+    return area;
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
