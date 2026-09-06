f35bc44254e7f93a06b9b08ee9d0df0deccdcb71
sofiatsebenko <sofialtd50@gmail.com>
2026-09-06 22:29:03 +0300

    done

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -7,14 +7,6 @@ export interface Figure {
 export class Triangle implements Figure {
   shape: 'triangle';
 
-  color: 'red' | 'blue' | 'green';
-
-  a: number;
-
-  b: number;
-
-  c: number;
-
   getArea(): number {
     const s = (this.a + this.b + this.c) / 2;
     const area = Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
@@ -23,32 +15,27 @@ export class Triangle implements Figure {
   }
 
   constructor(
-    color: 'red' | 'blue' | 'green',
-    a: number,
-    b: number,
-    c: number,
+    public color: 'red' | 'blue' | 'green',
+    public a: number,
+    public b: number,
+    public c: number,
   ) {
     if (a <= 0 || b <= 0 || c <= 0) {
       throw new Error('Sides must be greater than 0');
     }
 
     if (a >= b + c || b >= a + c || c >= a + b) {
-      throw new Error('Invalid triangle');
+      throw new Error(
+        'The longest side must be less than the sum of the other two sides',
+      );
     }
+
     this.shape = 'triangle';
-    this.color = color;
-    this.a = a;
-    this.b = b;
-    this.c = c;
   }
 }
 
 export class Circle implements Figure {
   shape: 'circle';
-
-  color: 'red' | 'blue' | 'green';
-
-  radius: number;
 
   getArea(): number {
     const area = Math.PI * this.radius ** 2;
@@ -56,24 +43,20 @@ export class Circle implements Figure {
     return Math.floor(area * 100) / 100;
   }
 
-  constructor(color: 'red' | 'blue' | 'green', r: number) {
-    if (r <= 0) {
+  constructor(
+    public color: 'red' | 'blue' | 'green',
+    public radius: number,
+  ) {
+    if (radius <= 0) {
       throw new Error('Radius must be greater than 0');
     }
+
     this.shape = 'circle';
-    this.color = color;
-    this.radius = r;
   }
 }
 
 export class Rectangle implements Figure {
   shape: 'rectangle';
-
-  color: 'red' | 'blue' | 'green';
-
-  height: number;
-
-  width: number;
 
   getArea(): number {
     const area = this.height * this.width;
@@ -81,14 +64,16 @@ export class Rectangle implements Figure {
     return Math.floor(area * 100) / 100;
   }
 
-  constructor(color: 'red' | 'blue' | 'green', height: number, width: number) {
+  constructor(
+    public color: 'red' | 'blue' | 'green',
+    public height: number,
+    public width: number,
+  ) {
     if (height <= 0 || width <= 0) {
       throw new Error('Width and height must be greater than 0');
     }
+
     this.shape = 'rectangle';
-    this.color = color;
-    this.height = height;
-    this.width = width;
   }
 }
 
