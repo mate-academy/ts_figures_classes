95fc6b586f77d9a7003a85d044e6e3d882423f3a
Khrystyna Yelyseieva <42446716+KhrystynaYelyseyeva@users.noreply.github.com>
2026-09-10 15:56:22 +0300

    Enhance Figure interface and implement area calculations

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,80 @@
-export interface Figure {}
+export interface Figure {
+  shape: 'triangle' | 'circle' | 'rectangle';
+  color: 'red' | 'green' | 'blue';
+  getArea(): number; 
+}
 
-export class Triangle implements Figure {}
+export class Triangle implements Figure {
+  constructor(
+    public a: number,
+    public b: number,
+    public c: number,
+    public area: number
+  ) {
+    if (!this.isValid()) {
+      throw new Error('Invalid triangle sides');
+    }
+  }
 
-export class Circle implements Figure {}
+  isValid() {
+    return (
+      this.a > 0 &&
+      this.b > 0 &&
+      this.c > 0 &&
+      this.a + this.b > this.c &&
+      this.a + this.c > this.b &&
+      this.c + this.b > this.a
+    );
+  }
 
-export class Rectangle implements Figure {}
+  getArea() {
+    const pp = (a + b + c / 2);
+    this.area = Math.floor(Math.sqrt(pp * (pp - a) * (pp - b) * (pp - c)) * 100) / 100;
+    return this.area;
+  }
+}
+
+export class Circle implements Figure {
+  constructor(
+    public radius: number,
+    public area: number
+  ) {
+    if (!this.isValid()) {
+      throw new Error('Invalid circle radius');
+    }
+  }
+
+  isValid() {
+    return this.radius > 0;
+  }
+
+  getArea() {
+    this.area = Math.floor((Math.PI * this.radius ** 2) * 100) / 100;
+    return this.area;
+  }
+}
+
+export class Rectangle implements Figure {
+  constructor(
+    public width: number,
+    public height: number,
+    public area: number
+  ) {
+    if (!this.isValid()) {
+      throw new Error('Invalid rectangle sides');
+    }
+  }
+
+  isValid() {
+    return this.width > 0 && this.height > 0;
+  }
+
+  getArea() {
+    this.area = Math.floor(this.width * this. height * 100) / 100;
+    return this.area;
+  }
+}
 
 export function getInfo(figure): string {
-  return typeof figure;
+  return `A ${figure.color} ${figure.shape} - ${figure.area}`;
 }
